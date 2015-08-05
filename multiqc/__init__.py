83ea15a17c804d81fb6f29b778f7e21f2f9c7d0c
Phil Ewels <phil.ewels@scilifelab.se>
2015-08-05 17:23:32 +0200

    Got a table of basic stats from FastQC reports to work.

--- multiqc/__init__.py
+++ multiqc/__init__.py
@@ -1,1 +1,30 @@
+#!/usr/bin/env python
+
 """ MultiQC modules """
+
+class BaseMultiqcModule(object):
+
+    def __init__(self):
+        pass
+
+    def dict_to_table(self, d, colheaders=None, tclasses=['table'], sort_rows=False):
+        """ Takes a 2D dictionary and creates a HTML table.
+        1st set of keys = row headers
+        2nd set of keys = column headers
+        """
+        t = '<table class="{}">'.format(' '.join(tclasses))
+        thead = False
+        for rh, cols in d.iteritems():
+            if not thead:
+                t += '<tr><td></td>'
+                for c in cols.keys():
+                    if colheaders is not None and c in colheaders:
+                        t += '<th>{}</th>'.format(colheaders[c])
+                    else:
+                        t += '<th>{}</th>'.format(c)
+                t += '</tr>'
+                thead = True
+
+            t += '<tr><th>{}</th><td>{}</td></tr>'.format(rh, '</td><td>'.join(cols.values()))
+        t += '</table>'
+        return t
--- multiqc/fastqc/__init__.py
+++ multiqc/fastqc/__init__.py
@@ -3,15 +3,21 @@
 """ MultiQC module to parse output from FastQC
 """
 
+import collections
 import logging
 import os
+import re
 import shutil
 import zipfile
 
+import multiqc
 
-class MultiqcModule(object):
+class MultiqcModule(multiqc.BaseMultiqcModule):
 
     def __init__(self, analysis_dir, output_dir):
+
+        # Initialise the parent object
+        super(MultiqcModule, self).__init__()
 
         # Static variables
         self.name = "FastQC"
@@ -19,7 +25,7 @@ class MultiqcModule(object):
         self.output_dir = output_dir
 
         # Find and load any FastQC reports
-        fastqc_data = {}
+        fastqc_raw_data = {}
         for root, dirnames, filenames in os.walk(analysis_dir):
             # Extracted FastQC directory
             if root[-7:] == '_fastqc' and 'fastqc_data.txt' in filenames:
@@ -27,7 +33,7 @@ class MultiqcModule(object):
                 s_name = s_name[:-7]
                 d_path = os.path.join(root, 'fastqc_data.txt')
                 with open (d_path, "r") as f:
-                    fastqc_data[s_name] = f.read()
+                    fastqc_raw_data[s_name] = f.read()
 
             # Zipped FastQC report
             for f in filenames:
@@ -35,15 +41,64 @@ class MultiqcModule(object):
                     s_name = f[:-11]
                     d_name = f[:-4]
                     fqc_zip = zipfile.ZipFile(os.path.join(root, f))
-                    with fqc_zip.open(os.path.join(d_name, 'fastqc_data.txt')) as f:
-                        fastqc_data[s_name] = f.read()
+                    try:
+                        with fqc_zip.open(os.path.join(d_name, 'fastqc_data.txt')) as f:
+                            fastqc_raw_data[s_name] = f.read()
+                    except KeyError:
+                        pass # Can't find fastqc_raw_data.txt in the zip file
 
-        if len(fastqc_data) == 0:
+        if len(fastqc_raw_data) == 0:
             logging.debug("Could not find any FastQC reports in {}".format(analysis_dir))
-        else:
-            logging.info("Found {} FastQC reports".format(len(fastqc_data)))
-            # Copy across the required module files (CSS / Javascript etc)
-            self.init_modfiles()
+            return
+
+        logging.info("Found {} FastQC reports".format(len(fastqc_raw_data)))
+
+        self.sections = ['fubar!']
+
+        # Section 1 - Basic Stats
+        parsed_stats = self.fastqc_basic_stats(fastqc_raw_data)
+        parsed_stats = collections.OrderedDict(sorted(parsed_stats.items()))
+        stats_table_headers = {
+            'percent_duplicates': '% Sequence Duplication',
+            'sequence_length': 'Sequence Length (bp)',
+            'percent_gc': '% GC',
+            'total_sequences_m': 'Total Sequences (millions)'
+        }
+        self.sections.append({
+            'name': 'Basic Stats',
+            'content': self.dict_to_table(parsed_stats, colheaders=stats_table_headers, sort_rows=True)
+        })
+
+
+        # Copy across the required module files (CSS / Javascript etc)
+        self.init_modfiles()
+
+    def fastqc_basic_stats(self, fastqc_raw_data):
+        """ Parse the contents of multiple fastqc_raw_data.txt files.
+        Returns a 2D dict with basic stats, sample names as first keys,
+        then statistic type as second key. """
+        parsed_data = {}
+        for s, data in fastqc_raw_data.iteritems():
+            parsed_data[s] = {}
+
+            dups = re.search("#Total Deduplicated Percentage\s+([\d\.]+)", data)
+            if dups:
+                parsed_data[s]['percent_duplicates'] = "{0:.2f}%".format(float(dups.group(1)))
+
+            seqlen = re.search("Sequence length\s+(\d+)", data)
+            if seqlen:
+                parsed_data[s]['sequence_length'] = seqlen.group(1)
+
+            gc = re.search("%GC\s+(\d+)", data)
+            if gc:
+                parsed_data[s]['percent_gc'] = "{}%".format(gc.group(1))
+
+            numseq = re.search("Total Sequences\s+(\d+)", data)
+            if numseq:
+                parsed_data[s]['total_sequences_m'] = "{0:.1f}".format(float(numseq.group(1))/1000000)
+
+        return parsed_data
+
 
     def init_modfiles(self):
         """ Copy the required assets into the output directory.
