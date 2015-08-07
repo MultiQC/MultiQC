fc85cff5335f3489ae58e18e134c49dcb4d62e8b
Phil Ewels <phil.ewels@scilifelab.se>
2015-08-07 16:49:45 +0200

    Made a slightly crappy vertical nav for the default template.

--- multiqc/fastqc/assets/js/multiqc_fastqc.js
+++ multiqc/fastqc/assets/js/multiqc_fastqc.js
@@ -16,36 +16,36 @@ function fastqc_seq_content_heatmap(data) {
     $("#fastqc_seq_heatmap").prop("width", c_width);
     $("#fastqc_seq_heatmap").prop("height", c_height+1);
     var canvas = document.getElementById("fastqc_seq_heatmap");
-    if (canvas.getContext) {{
+    if (canvas.getContext) {
         var ctx = canvas.getContext("2d");
         ctx.font = "12px Arial";
         ctx.strokeStyle = "#333333";
         // First, do labels and get max base pairs
-        $.each(data, function(name, s){{
+        $.each(data, function(name, s){
             var txt_w = ctx.measureText(name).width;
-            if(txt_w > labels_width) {{
+            if(txt_w > labels_width) {
                 labels_width = txt_w;
-            }}
+            }
             ctx.fillText(name, 0, ypos+14);
             ypos += s_height;
-            $.each(s, function(bp, v){{
+            $.each(s, function(bp, v){
                 bp = parseInt(bp);
-                if(bp > max_bp){{
+                if(bp > max_bp){
                     max_bp = bp;
-                }}
-            }});
-        }});
+                }
+            });
+        });
         labels_width += 5;
         ypos = 0;
         xpos_start = labels_width;
         c_width -= labels_width;
-        $.each(data, function(name, s){{
-            if (num_data == undefined){{
+        $.each(data, function(name, s){
+            if (num_data == undefined){
                 var s_width = c_width / Object.keys(s).length;
-            }}
+            }
             var xpos = xpos_start;
             var last_bp = 0;
-            $.each(s, function(bp, v){{
+            $.each(s, function(bp, v){
                 bp = parseInt(bp);
                 var this_width = (bp - last_bp) * (c_width / max_bp);
                 last_bp = bp;
@@ -55,23 +55,23 @@ function fastqc_seq_content_heatmap(data) {
                 ctx.fillStyle = chroma(r,g,b).css();
                 ctx.fillRect (xpos, ypos, this_width, s_height-1);
                 xpos += this_width;
-            }});
+            });
             // Draw a line under this row
             ctx.beginPath();
             ctx.moveTo(0, ypos);
             ctx.lineTo(c_width+labels_width, ypos);
             ctx.stroke();
             ypos += s_height;
-        }});
+        });
         // Final line under row
         ctx.beginPath();
         ctx.moveTo(0, ypos);
         ctx.lineTo(c_width+labels_width, ypos);
         ctx.stroke();
-    }}
+    }
 
     // http://stackoverflow.com/questions/6735470/get-pixel-color-from-canvas-on-mouseover
-    $("#fastqc_seq_heatmap").mousemove(function(e) {{
+    $("#fastqc_seq_heatmap").mousemove(function(e) {
         var pos = findPos(this);
         var x = e.pageX - pos.x;
         var y = e.pageY - pos.y;
@@ -90,5 +90,5 @@ function fastqc_seq_content_heatmap(data) {
         $("#fastqc_seq_heatmap_key_colourbar_a span").css("margin-left", seq_a);
         $("#fastqc_seq_heatmap_key_colourbar_t span").css("margin-left", seq_t);
         $("#fastqc_seq_heatmap_key_colourbar_c span").css("margin-left", seq_c);
-    }});
+    });
 }
