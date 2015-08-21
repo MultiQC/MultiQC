0b338c261f9947fe25a0ea7a9c1eaae13bf7496f
Phil Ewels <phil.ewels@scilifelab.se>
2015-08-21 17:49:11 +0200

    Pretty much rewrote the cutadapt module to handle multiple concatenated logs in a single file. Now much more awesome.

--- multiqc/cutadapt/__init__.py
+++ multiqc/cutadapt/__init__.py
@@ -2,6 +2,7 @@
 
 """ MultiQC module to parse output from Cutadapt """
 
+from __future__ import print_function
 import json
 import logging
 import mmap
@@ -27,122 +28,121 @@ class MultiqcModule(multiqc.BaseMultiqcModule):
         self.output_dir = report['output_dir']
 
         # Find and load any Cutadapt reports
-        cutadapt_raw_data = {}
+        self.cutadapt_data = dict()
         for root, dirnames, filenames in os.walk(self.analysis_dir):
             for fn in filenames:
                 if fn.endswith('.txt') and os.path.getsize(os.path.join(root,fn)) < 50000:
                     try:
                         with open (os.path.join(root,fn), "r") as f:
                             s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-                            if s.find('This is cutadapt') != -1:
-                                fn_search = re.search(r"^Input filename:\s+(.+)$", s, re.MULTILINE)
-                                if fn_search:
-                                    s_name = fn_search.group(1)
-                                else:
-                                    s_name = fn
-                                s_name = s_name.split(".txt",1)[0]
-                                s_name = s_name.split("_trimming_report",1)[0]
-                                s_name = s_name.split(".gz",1)[0]
-                                s_name = s_name.split(".fastq",1)[0]
-                                s_name = s_name.split(".fq",1)[0]
-
-                                cutadapt_raw_data[s_name] = f.read()
+                            self.parse_cutadapt_logs(s)
                     except ValueError:
                         logging.warn("Couldn't read file when looking for cutadapt output: {}".format(fn))
 
-        if len(cutadapt_raw_data) == 0:
+        if len(self.cutadapt_data) == 0:
             logging.debug("Could not find any Cutadapt reports in {}".format(self.analysis_dir))
             raise UserWarning
 
-        logging.info("Found {} Cutadapt reports".format(len(cutadapt_raw_data)))
+        logging.info("Found {} Cutadapt reports".format(len(self.cutadapt_data)))
+
+        # Write parsed report data to a file
+        # Only the summary stats - skip the length data (t_lengths)
+        with open (os.path.join(self.output_dir, 'report_data', 'multiqc_cutadapt_data.txt'), "w") as f:
+            print( self.dict_to_csv( { k: { j: x for j, x in v.iteritems() if j != 't_lengths'} for k, v in self.cutadapt_data.iteritems() } ), file=f)
 
         self.sections = list()
 
         # Basic Stats Table
         # Report table is immutable, so just updating it works
-        parsed_stats = self.cutadapt_general_stats(cutadapt_raw_data)
-        self.cutadapt_general_stats_table(parsed_stats, report)
+        self.cutadapt_general_stats_table(report)
 
         # Trimming Length Profiles
         # Only one section, so add to the intro
-        length_trimmed = self.cutadapt_length_trimmed(cutadapt_raw_data)
-        self.intro += self.cutadapt_length_trimmed_plot(length_trimmed)
+        self.intro += self.cutadapt_length_trimmed_plot()
 
 
-    def cutadapt_general_stats(self, cutadapt_raw_data):
-        """ Parse the single-digit stats for each sample from the Cutadapt report. """
-        parsed_stats = {}
-        for s, data in cutadapt_raw_data.iteritems():
-            parsed_stats[s] = {}
+    def parse_cutadapt_logs(self, s):
+        i = s.find('Input filename', 0)
+        while i >= 0:
+            s.seek(i)
 
-            bp_processed = re.search("Total basepairs processed:\s*([\d,]+) bp", data)
-            if bp_processed:
-                parsed_stats[s]['bp_processed'] = int(bp_processed.group(1).replace(',', ''))
+            fn_search = re.search(r"^Input filename:\s+(.+)$", s.readline())
+            while fn_search is None:
+                l = s.readline()
+                if not l: return # end of file
+                fn_search = re.search(r"^Input filename:\s+(.+)$", l)
+            s_name = fn_search.group(1)
+            s_name = s_name.split(".txt",1)[0]
+            s_name = s_name.split("_trimming_report",1)[0]
+            s_name = s_name.split(".gz",1)[0]
+            s_name = s_name.split(".fastq",1)[0]
+            s_name = s_name.split(".fq",1)[0]
 
-            bp_written = re.search("Total written \(filtered\):\s*([\d,]+) bp", data)
-            if bp_written:
-                parsed_stats[s]['bp_written'] = int(bp_written.group(1).replace(',', ''))
+            self.cutadapt_data[s_name] = dict()
+            regexes = {
+                'bp_processed': "Total basepairs processed:\s*([\d,]+) bp",
+                'bp_written': "Total written \(filtered\):\s*([\d,]+) bp",
+                'quality_trimmed': "Quality-trimmed:\s*([\d,]+) bp",
+                'r_processed': "Total reads processed:\s*([\d,]+)",
+                'r_with_adapters': "Reads with adapters:\s*([\d,]+)"
+            }
 
-            if bp_processed and bp_written:
-                parsed_stats[s]['percent_trimmed'] = (float(parsed_stats[s]['bp_processed'] - parsed_stats[s]['bp_written']) / parsed_stats[s]['bp_processed']) * 100
+            l = s.readline()
+            while l:
+                for k, r in regexes.iteritems():
+                    match = re.search(r, l)
+                    if match:
+                        self.cutadapt_data[s_name][k] = int(match.group(1).replace(',', ''))
 
-            quality_trimmed = re.search("Quality-trimmed:\s*([\d,]+) bp", data)
-            if quality_trimmed:
-                parsed_stats[s]['quality_trimmed'] = int(quality_trimmed.group(1).replace(',', ''))
+                if l.find('length') > -1 and l.find('max.err') > -1:
+                    l = s.readline()
+                    r_seqs = re.search(r"^(\d+)\s+(\d+)\s+([\d\.]+)", l)
+                    self.cutadapt_data[s_name]['t_lengths'] = {}
+                    while r_seqs:
+                        a_len = int(r_seqs.group(1))
+                        self.cutadapt_data[s_name]['t_lengths'][a_len] = {
+                            'count': int(r_seqs.group(2)),
+                            'expect': float(r_seqs.group(3))
+                        }
+                        if float(r_seqs.group(3)) > 0:
+                            self.cutadapt_data[s_name]['t_lengths'][a_len]['obs_exp'] = float(r_seqs.group(2)) / float(r_seqs.group(3))
+                        else:
+                            # Cheating, I know. Infinity is difficult to plot.
+                            self.cutadapt_data[s_name]['t_lengths'][a_len]['obs_exp'] = float(r_seqs.group(2))
 
-            r_processed = re.search("Total reads processed:\s*([\d,]+)", data)
-            if r_processed:
-                parsed_stats[s]['r_processed'] = int(r_processed.group(1).replace(',', ''))
+                        l = s.readline() # Push on to next line for regex
+                        r_seqs = re.search(r"^(\d+)\s+(\d+)\s+([\d\.]+)", l)
 
-            r_with_adapters = re.search("Reads with adapters:\s*([\d,]+)", data)
-            if r_with_adapters:
-                parsed_stats[s]['r_with_adapters'] = int(r_with_adapters.group(1).replace(',', ''))
+                if len(self.cutadapt_data[s_name]) == len(regexes) + 1: break # Found everything we need
+                if l.find('Input filename') > -1: break # Didn't find everything, but come across another sample
 
-        return parsed_stats
+                l = s.readline() # Next line for while loop
 
-    def cutadapt_general_stats_table(self, parsed_stats, report):
+            if 'bp_processed' in self.cutadapt_data[s_name] and 'bp_written' in self.cutadapt_data[s_name]:
+                self.cutadapt_data[s_name]['percent_trimmed'] = (float(self.cutadapt_data[s_name]['bp_processed'] - self.cutadapt_data[s_name]['bp_written']) / self.cutadapt_data[s_name]['bp_processed']) * 100
+
+            # Look for the next cutadapt output in this file
+            i = s.find('This is cutadapt', i + 1)
+
+
+    def cutadapt_general_stats_table(self, report):
         """ Take the parsed stats from the Cutadapt report and add it to the
         basic stats table at the top of the report """
 
         report['general_stats']['headers']['bp_trimmed'] = '<th class="chroma-col" data-chroma-scale="OrRd" data-chroma-max="100" data-chroma-min="0"><span data-toggle="tooltip" title="% Total Base Pairs trimmed by Cutadapt">Trimmed</span></th>'
-        for samp, vals in parsed_stats.iteritems():
+        for samp, vals in self.cutadapt_data.iteritems():
             report['general_stats']['rows'][samp]['bp_trimmed'] = '<td class="text-right">{:.1f}%</td>'.format(vals['percent_trimmed'])
 
-    def cutadapt_length_trimmed(self, cutadapt_raw_data):
-        """ Parse the counts of adapter lengths that have been trimmed """
-
-        parsed_data = {}
-        for s, data in cutadapt_raw_data.iteritems():
-            parsed_data[s] = {}
-            in_section = False
-            for l in data.splitlines():
-                if l == "length	count	expect	max.err	error counts":
-                    in_section = True
-                elif in_section:
-                    r_seqs = re.search(r"^(\d+)\s+(\d+)\s+([\d\.]+)", l)
-                    if r_seqs:
-                        a_len = int(r_seqs.group(1))
-                        parsed_data[s][a_len] = {}
-                        parsed_data[s][a_len]['count'] = int(r_seqs.group(2))
-                        parsed_data[s][a_len]['expect'] = float(r_seqs.group(3))
-                        if float(r_seqs.group(3)) > 0:
-                            parsed_data[s][a_len]['obs_exp'] = float(r_seqs.group(2)) / float(r_seqs.group(3))
-                        else:
-                            parsed_data[s][a_len]['obs_exp'] = float(r_seqs.group(2)) # Cheating, I know. Infinity is difficult to plot.
-                    else:
-                        break
-        return parsed_data
-
-    def cutadapt_length_trimmed_plot (self, parsed_data):
+    def cutadapt_length_trimmed_plot (self):
 
         counts = list()
         obsexp = list()
-        for s in sorted(parsed_data):
+        for s in sorted(self.cutadapt_data):
             counts_pairs = list()
             obsexp_pairs = list()
-            for l, p in iter(sorted(parsed_data[s].iteritems())):
-                counts_pairs.append([l, parsed_data[s][l]['count']])
-                obsexp_pairs.append([l, parsed_data[s][l]['obs_exp']])
+            for l, p in iter(sorted(self.cutadapt_data[s]['t_lengths'].iteritems())):
+                counts_pairs.append([l, p['count']])
+                obsexp_pairs.append([l, p['obs_exp']])
             counts.append({
                 'name': s,
                 'data': counts_pairs
