2309806cc99255c9a778c1a73fff70ee1c6e3563
moonso <monsunas@gmail.com>
2015-09-24 16:40:22 +0200

    Moved check osx app to multiqc/utils

--- multiqc/__init__.py
+++ multiqc/__init__.py
@@ -4,3 +4,15 @@ logger = logging.getLogger(__name__)
 
 from . import config
 from . base_module import BaseMultiqcModule
+
+
+# Order tha modules should appear in report. Try to list in order of analysis,
+# eg. FastQC is usually the first step, so should be last in this list.
+module_order = [
+    # Post-alignment analysis results
+    'qualimap', 'featureCounts', 'picard',
+    # Alignment tool stats
+    'bismark', 'star', 'tophat', 'bowtie2', 'bowtie1',
+    # Pre-alignment QC
+    'cutadapt', 'fastq_screen', 'fastqc'
+]
---
+++ multiqc/utils/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import
+
+from .check_app import check_app
