77f156b1848583c425465c5ca9b05153dff08e3a
Phil Ewels <phil.ewels@scilifelab.se>
2015-10-14 18:20:13 +0200

    WIP: UNTESTED: Moved helper scripts into utils directory.

--- multiqc/__init__.py
+++ multiqc/__init__.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 __version__ = get_distribution("multiqc").version
 
-from . import config
-from . base_module import BaseMultiqcModule
+from utils import config
+from modules.base_module import BaseMultiqcModule
 
 config.version = __version__
