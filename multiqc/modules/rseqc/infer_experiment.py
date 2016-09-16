620e6a7f902c5459a20f95a0f8489735fe539c41
Hammarn <rickard.hammaren@scilifelab.se>
2016-09-16 16:49:51 +0200

    Cleanup and added missing files to PR

--- multiqc/modules/rseqc/infer_experiment.py
+++ multiqc/modules/rseqc/infer_experiment.py
@@ -18,10 +18,7 @@ def parse_reports(self):
     
     # Set up vars 
     self.infer_exp = dict()
-    first_regexes = {
-        'Paired_single': r"This is ([A-Za-z]*) Data"
-    }
-    second_regexes = {
+    regexes = {
         'pe_sense': r"\"1\+\+,1--,2\+-,2-\+\": (\d\.\d*)",
         'pe_antisense': r"\"1\+-,1-\+,2\+\+,2--\": (\d\.\d*)",
         'se_sense': r"\"\+\+,--\": (\d\.\d*)",
@@ -32,30 +29,11 @@ def parse_reports(self):
     # Go through files and parse data using regexes
     for f in self.find_log_files(config.sp['rseqc']['infer_experiment']):
         d = dict()
-        for k, r in first_regexes.items():
-            r_search = re.search(r, f['f'], re.MULTILINE)
-            if r_search:
-                d[k] = str(r_search.group(1))
-
-        for k, r in second_regexes.items():
+        for k, r in regexes.items():
             r_search = re.search(r, f['f'], re.MULTILINE)
             if r_search:
                 d[k] = float(r_search.group(1))
                 
-               # d['{}_total_bases'.format(k)] = int(r_search.group(1))
-               # d['{}_tag_count'.format(k)] = int(r_search.group(2))
-               # d['{}_tags_kb'.format(k)] = float(r_search.group(2))
-        
-        # Calculate some percentages for parsed file
-        #if 'total_tags' in d:
-        #    t = float(d['total_tags'])
-        #    pcts = dict()
-        #    for k in d:
-        #        if k.endswith('_tag_count'):
-        #            pk = '{}_tag_pct'.format(k[:-10])
-        #            pcts[pk] = (float(d[k]) / t)*100.0
-        #    d.update(pcts)
-        
         if len(d) > 0:
             if f['s_name'] in self.infer_exp:
                 log.debug("Duplicate sample name found! Overwriting: {}".format(f['s_name']))
@@ -69,29 +47,27 @@ def parse_reports(self):
         
         # Plot bar graph of groups
         keys = OrderedDict()
-        keys['pe_sense'] = {'name': "PairedEnd Sense"}
-        keys['pe_antisense'] = {'name': "PairedEnd Antisense"}
-        keys['se_sense'] = {'name': "SingleEnd Sense"}
-        keys['se_antisense'] = {'name': "SingleEnd antisense"}
+        keys['pe_sense'] = {'name': "Paired End Sense"}
+        keys['pe_antisense'] = {'name': "Paired End Antisense"}
+        keys['se_sense'] = {'name': "Single End Sense"}
+        keys['se_antisense'] = {'name': "Single End Antisense"}
         keys['failed'] = {'name': "Undetermined"}
-        # Config for the plot   ----  redo
+        # Config for the plot
         pconfig = {
             'id': 'rseqc_infer_experiment_plot',
-            'title': 'RSeQC: Read Distribution',
+            'title': 'RSeQC: Infer experiment',
             'ylab': '# Tags',
             'cpswitch': False,
             'cpswitch_counts_label': 'Number of Tags',
-            'cpswitch_c_active': False
         }
         
         p_link = '<a href="http://rseqc.sourceforge.net/#infer-experiment-py" target="_blank">Infer experiment</a>'
         self.sections.append({
             'name': 'Infer experiment',
             'anchor': 'rseqc-infer_experiment',
-            'content': "<p>"+p_link+" Infer experiment is used to 'guess' how RNA-seq sequencing were configured,in particulary how reads were stranded for strand-specific RNA-seq data, through comparing the 'strandness of reads with the 'strandness of transcripts'.</p>" + 
+            'content': "<p>"+p_link+" counts how reads and read pairs match the strandedness of overlapping transcripts. It can be used to infer whether RNA-seq library preps are stranded (sense or antisense) .</p>" + 
                 plots.bargraph.plot(self.infer_exp, keys, pconfig)
         })
-    print self.infer_exp 
     # Return number of samples found
     return len(self.infer_exp)
     
