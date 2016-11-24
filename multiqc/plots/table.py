25d753bd1b5c1dbb48a9be66c9c41438d1d13940
Tim Booth <tim.booth@ed.ac.uk>
2016-11-24 16:05:38 +0000

    Prepare for allowing re-ordering of table columns by minor code refactoring.

--- multiqc/plots/table.py
+++ multiqc/plots/table.py
@@ -20,16 +20,15 @@ def plot (data, headers=[], pconfig={}):
     :param headers: list of optional dicts with column config in key:value pairs.
     :return: HTML ready to be inserted into the page
     """
-    
     # Make a datatable object
     dt = table_object.datatable(data, headers, pconfig)
-    
+
     # Collect unique sample names
     s_names = set()
     for d in dt.data:
         for s_name in d.keys():
             s_names.add(s_name)
-    
+
     # Make a beeswarm plot if we have lots of samples
     if len(s_names) >= config.max_table_rows and pconfig.get('no_beeswarm') is not True:
         logger.debug('Plotting beeswarm instead of table, {} samples'.format(len(s_names)))
@@ -47,7 +46,7 @@ def make_table (dt):
     Build the HTML needed for a MultiQC table.
     :param data: MultiQC datatable object
     """
-    
+
     table_id = dt.pconfig.get('id', 'table_{}'.format(''.join(random.sample(letters, 4))) )
     t_headers = OrderedDict()
     t_modal_headers = OrderedDict()
@@ -55,114 +54,113 @@ def make_table (dt):
     dt.raw_vals = defaultdict(lambda: dict())
     empty_cells = dict()
     hidden_cols = 1
-    
-    for idx, hs in enumerate(dt.headers):
-        for k, header in hs.items():
-            
-            rid = header['rid']
-            
-            # Build the table header cell
-            shared_key = ''
-            if header.get('shared_key', None) is not None:
-                shared_key = ' data-shared-key={}'.format(header['shared_key'])
-                
-            hide = ''
-            muted = ''
-            checked = ' checked="checked"'
-            if header.get('hidden', False) is True:
-                hide = 'hidden'
-                muted = ' text-muted'
-                checked = ''
-                hidden_cols += 1
-            
-            c_col = '' if header['scale'] == False else 'chroma-col'
-            
-            data_attr = 'data-chroma-scale="{}" data-chroma-max="{}" data-chroma-min="{}" {}' \
-                .format(header['scale'], header['dmax'], header['dmin'], shared_key)
-            
-            cell_contents = '<span data-toggle="tooltip" title="{}: {}">{}</span>' \
-                .format(header['namespace'], header['description'], header['title'])
-            
-            t_headers[rid] = '<th id="header_{rid}" class="{cc} {rid} {h}" {d}>{c}</th>' \
-                .format(rid=rid, d=data_attr, h=hide, c=cell_contents, cc=c_col)
-            
-            empty_cells[rid] = '<td class="data-coloured {rid} {h}"></td>'.format(rid=rid, h=hide)
-            
-            # Build the modal table row
-            t_modal_headers[rid] = """
-            <tr class="{rid}{muted}" style="background-color: rgba({col}, 0.15);">
-              <td class="sorthandle ui-sortable-handle">||</span></td>
-              <td style="text-align:center;">
-                <input class="mqc_table_col_visible" type="checkbox" {checked} value="{rid}" data-target="#{tid}">
-              </td>
-              <td>{name}</td>
-              <td>{title}</td>
-              <td>{desc}</td>
-              <td>{col_id}</td>
-              <td>{sk}</td>
-            </tr>""".format(
-                    rid = rid,
-                    muted = muted,
-                    checked = checked,
-                    tid = table_id,
-                    col = header['colour'],
-                    name = header['namespace'],
-                    title = header['title'],
-                    desc = header['description'],
-                    col_id = '<code>{}</code>'.format(k),
-                    sk = header.get('shared_key', '')
-                )
-            
-            # Add the data table cells
-            for (s_name, samp) in dt.data[idx].items():
-                if k in samp:
-                    val = samp[k]
-                    kname = '{}_{}'.format(header['namespace'], rid[5:]) # trucate 'kxhe_' random chars from rid
-                    dt.raw_vals[s_name][kname] = val
-                    
-                    if 'modify' in header and callable(header['modify']):
-                        val = header['modify'](val)
-                    
+
+    for idx, k, header in dt.get_headers_in_order():
+
+        rid = header['rid']
+
+        # Build the table header cell
+        shared_key = ''
+        if header.get('shared_key', None) is not None:
+            shared_key = ' data-shared-key={}'.format(header['shared_key'])
+
+        hide = ''
+        muted = ''
+        checked = ' checked="checked"'
+        if header.get('hidden', False) is True:
+            hide = 'hidden'
+            muted = ' text-muted'
+            checked = ''
+            hidden_cols += 1
+
+        c_col = '' if header['scale'] == False else 'chroma-col'
+
+        data_attr = 'data-chroma-scale="{}" data-chroma-max="{}" data-chroma-min="{}" {}' \
+            .format(header['scale'], header['dmax'], header['dmin'], shared_key)
+
+        cell_contents = '<span data-toggle="tooltip" title="{}: {}">{}</span>' \
+            .format(header['namespace'], header['description'], header['title'])
+
+        t_headers[rid] = '<th id="header_{rid}" class="{cc} {rid} {h}" {d}>{c}</th>' \
+            .format(rid=rid, d=data_attr, h=hide, c=cell_contents, cc=c_col)
+
+        empty_cells[rid] = '<td class="data-coloured {rid} {h}"></td>'.format(rid=rid, h=hide)
+
+        # Build the modal table row
+        t_modal_headers[rid] = """
+        <tr class="{rid}{muted}" style="background-color: rgba({col}, 0.15);">
+          <td class="sorthandle ui-sortable-handle">||</span></td>
+          <td style="text-align:center;">
+            <input class="mqc_table_col_visible" type="checkbox" {checked} value="{rid}" data-target="#{tid}">
+          </td>
+          <td>{name}</td>
+          <td>{title}</td>
+          <td>{desc}</td>
+          <td>{col_id}</td>
+          <td>{sk}</td>
+        </tr>""".format(
+                rid = rid,
+                muted = muted,
+                checked = checked,
+                tid = table_id,
+                col = header['colour'],
+                name = header['namespace'],
+                title = header['title'],
+                desc = header['description'],
+                col_id = '<code>{}</code>'.format(k),
+                sk = header.get('shared_key', '')
+            )
+
+        # Add the data table cells
+        for (s_name, samp) in dt.data[idx].items():
+            if k in samp:
+                val = samp[k]
+                kname = '{}_{}'.format(header['namespace'], rid[5:]) # trucate 'kxhe_' random chars from rid
+                dt.raw_vals[s_name][kname] = val
+
+                if 'modify' in header and callable(header['modify']):
+                    val = header['modify'](val)
+
+                try:
+                    dmin = header['dmin']
+                    dmax = header['dmax']
+                    percentage = ((float(val) - dmin) / (dmax - dmin)) * 100;
+                    percentage = min(percentage, 100)
+                    percentage = max(percentage, 0)
+                except (ZeroDivisionError,ValueError):
+                    percentage = 0
+
+                try:
+                    val = header['format'].format(val)
+                except ValueError:
                     try:
-                        dmin = header['dmin']
-                        dmax = header['dmax']
-                        percentage = ((float(val) - dmin) / (dmax - dmin)) * 100;
-                        percentage = min(percentage, 100)
-                        percentage = max(percentage, 0)
-                    except (ZeroDivisionError,ValueError):
-                        percentage = 0
-                    
-                    try:
-                        val = header['format'].format(val)
+                        val = header['format'].format(float(samp[k]))
                     except ValueError:
-                        try:
-                            val = header['format'].format(float(samp[k]))
-                        except ValueError:
-                            val = samp[k]
-                    except:
                         val = samp[k]
-                    
-                    # Build HTML
-                    if not header['scale']:
-                        t_rows[s_name][rid] = '<td class="{rid} {h}">{v}</td>'.format(rid=rid, h=hide, v=val)
-                    else:
-                        bar_html = '<span class="bar" style="width:{}%;"></span>'.format(percentage)
-                        val_html = '<span class="val">{}</span>'.format(val)
-                        wrapper_html = '<div class="wrapper">{}{}</div>'.format(bar_html, val_html)
-                        
-                        t_rows[s_name][rid] = \
-                            '<td class="data-coloured {rid} {h}">{c}</td>'.format(rid=rid, h=hide, c=wrapper_html)
-            
-            # Remove header if we don't have any filled cells for it
-            if sum([len(rows) for rows in t_rows.values()]) == 0:
-                t_headers.pop(rid, None)
-                t_modal_headers.pop(rid, None)
-                logger.debug('Removing header {} from general stats table, as no data'.format(k))
-    
+                except:
+                    val = samp[k]
+
+                # Build HTML
+                if not header['scale']:
+                    t_rows[s_name][rid] = '<td class="{rid} {h}">{v}</td>'.format(rid=rid, h=hide, v=val)
+                else:
+                    bar_html = '<span class="bar" style="width:{}%;"></span>'.format(percentage)
+                    val_html = '<span class="val">{}</span>'.format(val)
+                    wrapper_html = '<div class="wrapper">{}{}</div>'.format(bar_html, val_html)
+
+                    t_rows[s_name][rid] = \
+                        '<td class="data-coloured {rid} {h}">{c}</td>'.format(rid=rid, h=hide, c=wrapper_html)
+
+        # Remove header if we don't have any filled cells for it
+        if sum([len(rows) for rows in t_rows.values()]) == 0:
+            t_headers.pop(rid, None)
+            t_modal_headers.pop(rid, None)
+            logger.debug('Removing header {} from general stats table, as no data'.format(k))
+
     #
     # Put everything together
     #
-    
+
     # Buttons above the table
     html = ''
     if not config.simple_output:
@@ -178,17 +176,17 @@ def make_table (dt):
         </button>
         <small id="{tid}_numrows_text" class="mqc_table_numrows_text">Showing <sup id="{tid}_numrows" class="mqc_table_numrows">{nrows}</sup>/<sub>{nrows}</sub> rows and <sup id="{tid}_numcols" class="mqc_table_numcols">{ncols_vis}</sup>/<sub>{ncols}</sub> columns.</small>
     """.format(tid=table_id, nrows=len(t_rows), ncols_vis = len(t_headers)-hidden_cols, ncols=len(t_headers))
-    
+
     # Build the table itself
     html += """
         <div id="{tid}_container" class="mqc_table_container">
             <div class="table-responsive">
                 <table id="{tid}" class="table table-condensed mqc_table">
         """.format(tid=table_id)
-    
+
     # Build the header row
     html += '<thead><tr><th class="rowheader">Sample Name</th>{}</tr></thead>'.format(''.join(t_headers.values()))
-    
+
     # Build the table body
     html += '<tbody>'
     for s_name in sorted(t_rows.keys()):
@@ -199,7 +197,7 @@ def make_table (dt):
             html += t_rows[s_name].get(k, empty_cells[k])
         html += '</tr>'
     html += '</tbody></table></div></div>'
-    
+
     # Build the bootstrap modal to customise columns and order
     if not config.simple_output:
         html += """
@@ -236,17 +234,12 @@ def make_table (dt):
         </div>
         <div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> </div>
     </div> </div> </div>""".format( tid=table_id, title=dt.pconfig.get('table_title', table_id), trows=''.join(t_modal_headers.values()) )
-    
+
     # Save the raw values to a file if requested
     if dt.pconfig.get('save_file') is True:
         fn = dt.pconfig.get('raw_data_fn', 'multiqc_{}'.format(table_id) )
         util_functions.write_data_file(dt.raw_vals, fn )
         report.saved_raw_data[fn] = dt.raw_vals
-    
+
     return html
-    
-    
-    
-    
-    
-    
+
--- multiqc/plots/table_object.py
+++ multiqc/plots/table_object.py
@@ -15,29 +15,29 @@ letters = 'abcdefghijklmnopqrstuvwxyz'
 class datatable (object):
     """ Data table class. Prepares and holds data and configuration
     for either a table or a beeswarm plot. """
-    
+
     def __init__ (self, data, headers=[], pconfig={}):
         """ Prepare data for use in a table or plot """
-        
+
         # Given one dataset - turn it into a list
         if type(data) is not list:
             data = [data]
         if type(headers) is not list:
             headers = [headers]
-        
+
         sectcols = ['55,126,184', '77,175,74', '152,78,163', '255,127,0', '228,26,28', '255,255,51', '166,86,40', '247,129,191', '153,153,153']
         shared_keys = defaultdict(lambda: dict())
-        
+
         # Go through each table section
         for idx, d in enumerate(data):
-            
+
             # Get the header keys
             try:
                 keys = headers[idx].keys()
                 assert len(keys) > 0
             except (IndexError, AssertionError):
                 keys = d.keys()
-            
+
             # Check that we have some data in each column
             empties = list()
             for k in keys:
@@ -50,11 +50,11 @@ class datatable (object):
             for k in empties:
                 keys = [j for j in keys if j != k]
                 del headers[idx][k]
-            
+
             for k in keys:
                 # Unique id to avoid overwriting by other datasets
                 headers[idx][k]['rid'] = '{}_{}'.format(''.join(random.sample(letters, 4)), k)
-                
+
                 # Use defaults / data keys if headers not given
                 headers[idx][k]['namespace']   = headers[idx][k].get('namespace', pconfig.get('namespace', ''))
                 headers[idx][k]['title']       = headers[idx][k].get('title', k)
@@ -67,20 +67,20 @@ class datatable (object):
                 headers[idx][k]['min']         = headers[idx][k].get('min', pconfig.get('min', None))
                 headers[idx][k]['shared_key']  = headers[idx][k].get('shared_key', pconfig.get('shared_key', None))
                 headers[idx][k]['modify']      = headers[idx][k].get('modify', pconfig.get('modify', None))
-                
+
                 if headers[idx][k]['colour'] is None:
                     cidx = idx
                     while cidx >= len(sectcols):
                         cidx -= len(sectcols)
                     headers[idx][k]['colour'] = sectcols[cidx]
-                
+
                 # Overwrite hidden if set in user config
                 try:
                     # Config has True = visibile, False = Hidden. Here we're setting "hidden" which is inverse
                     headers[idx][k]['hidden'] = not config.table_columns_visible[ headers[idx][k]['namespace'] ][k]
                 except KeyError:
                     pass
-                
+
                 # Work out max and min value if not given
                 setdmax = False
                 setdmin = False
@@ -89,13 +89,13 @@ class datatable (object):
                 except TypeError:
                     headers[idx][k]['dmax'] = float("-inf")
                     setdmax = True
-                
+
                 try:
                     headers[idx][k]['dmin'] = float(headers[idx][k]['min'])
                 except TypeError:
                     headers[idx][k]['dmin'] = float("inf")
                     setdmin = True
-                
+
                 # Figure out the min / max if not supplied
                 if setdmax or setdmin:
                     for s_name, samp in data[idx].items():
@@ -111,7 +111,7 @@ class datatable (object):
                             val = samp[k] # couldn't convert to float - keep as a string
                         except KeyError:
                             pass # missing data - skip
-        
+
         # Collect settings for shared keys
         shared_keys = defaultdict(lambda: dict())
         for idx, hs in enumerate(headers):
@@ -121,7 +121,7 @@ class datatable (object):
                     shared_keys[sk]['scale'] = headers[idx][k]['scale']
                     shared_keys[sk]['dmax']  = max(headers[idx][k]['dmax'], shared_keys[sk].get('dmax', headers[idx][k]['dmax']))
                     shared_keys[sk]['dmin']  = max(headers[idx][k]['dmin'], shared_keys[sk].get('dmin', headers[idx][k]['dmin']))
-        
+
         # Overwrite shared key settings
         for idx, hs in enumerate(headers):
             for k in hs.keys():
@@ -130,8 +130,18 @@ class datatable (object):
                     headers[idx][k]['scale'] = shared_keys[sk]['scale']
                     headers[idx][k]['dmax'] = shared_keys[sk]['dmax']
                     headers[idx][k]['dmin'] = shared_keys[sk]['dmin']
-        
+
         # Assign to class
         self.data = data
         self.headers = headers
         self.pconfig = pconfig
+
+    def get_headers_in_order(self):
+        """Gets the headers in the order they want to be displayed.
+           Returns an iterable of triplets: (idx, key, header_info)
+        """
+
+        #Initial refactoring - just traverse them in the order we have them
+        for idx, hs in enumerate(self.headers):
+            for k, header in hs.items():
+                yield (idx, k, header)
