556c81962ec997b1b48f8634772da694d8ae8da3
Phil Ewels <phil.ewels@scilifelab.se>
2016-12-06 14:48:30 +0100

    Get git commit hash for version. Closes #320

--- multiqc/__init__.py
+++ multiqc/__init__.py
@@ -1,11 +1,8 @@
 import logging
-from pkg_resources import get_distribution
 
 logger = logging.getLogger(__name__)
-
-__version__ = get_distribution("multiqc").version
 
 from multiqc.utils import config
 from multiqc.modules.base_module import BaseMultiqcModule
 
-config.version = __version__
+__version__ = config.version
