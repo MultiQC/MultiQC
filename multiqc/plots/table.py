e36d432b6ce48241fb121230460fa606bf43fb06
Tim Booth <tim.booth@ed.ac.uk>
2017-01-04 12:24:48 +0000

    Remove unecessary randomness by using the standard id() function to get a unique number.

--- multiqc/plots/table.py
+++ multiqc/plots/table.py
@@ -117,7 +117,8 @@ def make_table (dt):
             for (s_name, samp) in dt.data[idx].items():
                 if k in samp:
                     val = samp[k]
-                    kname = '{}_{}'.format(header['namespace'], rid[5:]) # trucate 'kxhe_' random chars from rid
+                    # truncate '12345_' random prefix from rid
+                    kname = '{}_{}'.format(header['namespace'], rid.split('_',1)[1])
                     dt.raw_vals[s_name][kname] = val
 
                     if 'modify' in header and callable(header['modify']):
