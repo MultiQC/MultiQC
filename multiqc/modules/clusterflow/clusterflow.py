0b4b038f666926fc4769b05218987c9eff18947b
Phil Ewels <phil.ewels@scilifelab.se>
2017-03-04 01:12:50 +0100

    Cluster Flow: commands organised by pipeline.
    See #287.

--- multiqc/modules/clusterflow/clusterflow.py
+++ multiqc/modules/clusterflow/clusterflow.py
@@ -5,6 +5,7 @@
 from __future__ import print_function
 from collections import OrderedDict
 import logging
+import re
 
 from multiqc import config
 from multiqc.plots import table
@@ -26,7 +27,7 @@ class MultiqcModule(BaseMultiqcModule):
         info="is a simple and flexible bioinformatics pipeline tool.")
 
         # Find and load any Cluster Flow reports
-        self.clusterflow_commands = list()
+        self.clusterflow_commands = dict()
 
         for f in self.find_log_files(config.sp['clusterflow'], filehandles=True):
             self.parse_clusterflow_logs(f)
@@ -36,7 +37,7 @@ class MultiqcModule(BaseMultiqcModule):
             log.debug("Could not find any reports in {}".format(config.analysis_dir))
             raise UserWarning
 
-        log.info("Found {} commands".format(len(self.clusterflow_commands)))
+        log.info("Found {} pipelines".format(len(self.clusterflow_commands)))
 
         self.sections = list()
 
@@ -50,10 +51,30 @@ class MultiqcModule(BaseMultiqcModule):
 
     def parse_clusterflow_logs(self, f):
         """ Parse Clusterflow logs """
+        module = None
+        job_id = None
+        pipeline_id = None
         for l in f['f']:
+
+            # Get pipeline ID
+            module_r = re.match(r'Module:\s+(.+)$', l)
+            if module_r:
+                module = module_r.group(1)
+            job_id_r = re.match(r'Job ID:\s+(.+)$', l)
+            if job_id_r:
+                job_id = job_id_r.group(1)
+                if module is not None:
+                    pipeline_r = re.match(r"(cf_.+)_"+re.escape(module)+r"_\d+$", job_id)
+                    if pipeline_r:
+                        pipeline_id = pipeline_r.group(1)
+            
             # Get commands that have been run
             if l.startswith('###CFCMD'):
-                self.clusterflow_commands.append(l[8:])
+                if pipeline_id is None:
+                    pipeline_id = 'unknown'
+                if pipeline_id not in self.clusterflow_commands.keys():
+                    self.clusterflow_commands[pipeline_id] = list()
+                self.clusterflow_commands[pipeline_id].append(l[8:])
 
 
     def clusterflow_commands_table (self):
@@ -67,26 +88,34 @@ class MultiqcModule(BaseMultiqcModule):
             calls are replaced with <code>[variable]</code>
             <em>(typically input and ouput filenames)</em>.</p>'''
 
-        # Try to find consensus commands
-        self.var_html = '<span style="background-color:#dedede; color:#999;">[variable]</span>'
-        tool_cmd_parts = OrderedDict()
-        for cmd in self.clusterflow_commands:
-            s = cmd.split()
-            if s[0] not in tool_cmd_parts.keys():
-                tool_cmd_parts[s[0]] = list()
-            tool_cmd_parts[s[0]].append(s)
-
+        # Loop through pipelines
         tool_cmds = OrderedDict()
-        for tool, cmds in tool_cmd_parts.items():
-            cons_cmd = self._replace_variable_chunks(cmds)
-            # Try again with first two blocks if all variable
-            variable_count = cons_cmd.count(self.var_html)
-            if variable_count == len(cmds[0]) - 1 and len(cmds[0]) > 2:
-                for subcmd in set([s[1] for s in cmds]):
-                    sub_cons_cmd = self._replace_variable_chunks([cmd for cmd in cmds if cmd[1] == subcmd])
-                    tool_cmds["{} {}".format(tool, subcmd)] = {'Command': '<code style="white-space:nowrap;">{}</code>'.format(" ".join(sub_cons_cmd) ) }
-            else:
-                tool_cmds[tool] = {'Command': '<code style="white-space:nowrap;">{}</code>'.format(" ".join(cons_cmd) ) }
+        for pipeline_id, commands in self.clusterflow_commands.items():
+
+            self.var_html = '<span style="background-color:#dedede; color:#999;">[variable]</span>'
+            tool_cmd_parts = OrderedDict()
+            for cmd in commands:
+                s = cmd.split()
+                if s[0] not in tool_cmd_parts.keys():
+                    tool_cmd_parts[s[0]] = list()
+                tool_cmd_parts[s[0]].append(s)
+
+            
+            for tool, cmds in tool_cmd_parts.items():
+                cons_cmd = self._replace_variable_chunks(cmds)
+                # Try again with first two blocks if all variable
+                variable_count = cons_cmd.count(self.var_html)
+                if variable_count == len(cmds[0]) - 1 and len(cmds[0]) > 2:
+                    for subcmd in set([s[1] for s in cmds]):
+                        sub_cons_cmd = self._replace_variable_chunks([cmd for cmd in cmds if cmd[1] == subcmd])
+                        tool = "{} {}".format(tool, subcmd)
+                        if tool not in tool_cmds:
+                            tool_cmds[tool] = dict()
+                        tool_cmds[tool][pipeline_id] = '<code style="white-space:nowrap;">{}</code>'.format(" ".join(sub_cons_cmd) )
+                else:
+                    if tool not in tool_cmds:
+                        tool_cmds[tool] = dict()
+                    tool_cmds[tool][pipeline_id] = '<code style="white-space:nowrap;">{}</code>'.format(" ".join(cons_cmd) )
         
         table_config = {
             'id': 'clusterflow-commands',
@@ -95,7 +124,7 @@ class MultiqcModule(BaseMultiqcModule):
             'sortRows': False,
             'no_beeswarm': True
         }
-        return html + table.plot(tool_cmds, {'Command': {'scale': False}}, table_config)
+        return html + table.plot(tool_cmds, None, table_config)
 
 
     def _replace_variable_chunks(self, cmds):
