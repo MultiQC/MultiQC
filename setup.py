7b014db5ee6fcd2fb4bc78704099f293762d6929
Tim Booth <tim.booth@ed.ac.uk>
2017-07-11 14:52:03 +0100

    bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ MultiQC was written by Phil Ewels (http://phil.ewels.co.uk) at SciLifeLab Sweden
 
 from setuptools import setup, find_packages
 
-version = '1.0dev'
+version = '1.0dev1'
 dl_version = 'master' if 'dev' in version else 'v{}'.format(version)
 
 print("""-----------------------------------
