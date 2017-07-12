44c6cb2acad85d2195fdb7f3506b42cc2fcc4166
Phil Ewels <phil.ewels@scilifelab.se>
2017-07-12 11:31:58 +0200

    MACS2: Additional stats in report, new plot. See #476.

--- multiqc/modules/macs2/macs2.py
+++ multiqc/modules/macs2/macs2.py
@@ -8,6 +8,7 @@ import logging
 import re
 
 from multiqc import config
+from multiqc.plots import bargraph
 from multiqc.modules.base_module import BaseMultiqcModule
 
 # Initialise the logger
@@ -37,25 +38,8 @@ class MultiqcModule(BaseMultiqcModule):
         log.info("Found {} logs".format(len(self.macs_data)))
         self.write_data_file(self.macs_data, 'multiqc_macs')
 
-        # Add drop rate to the general stats table
-        headers = OrderedDict()
-        headers['treatment_redundant_rate'] = {
-            'title': 'Treatment Redundancy',
-            'description': 'Redundant rate in treatment',
-            'max': 1,
-            'min': 0,
-            'format': '{:,.2f}',
-            'scale': 'RdYlBu-rev'
-        }
-        headers['control_redundant_rate'] = {
-            'title': 'Control Redundancy',
-            'description': 'Redundant rate in control',
-            'max': 1,
-            'min': 0,
-            'format': '{:,.2f}',
-            'scale': 'RdYlBu-rev'
-        }
-        self.general_stats_addcols(self.macs_data, headers)
+        self.macs_general_stats()
+        self.macs_filtered_reads_plot()
 
     def parse_macs(self, f):
         regexes = {
@@ -88,3 +72,59 @@ class MultiqcModule(BaseMultiqcModule):
                 log.debug("Duplicate sample name found! Overwriting: {}".format(s_name))
             self.macs_data[s_name] = parsed_data
 
+    def macs_general_stats(self):
+        """ Add columns to General Statistics table """
+        headers = OrderedDict()
+        headers['d'] = {
+            'title': 'Fragment Length',
+            'min': 0,
+            'format': '{:,.0f}'
+        }
+        headers['treatment_redundant_rate'] = {
+            'title': 'Treatment Redundancy',
+            'description': 'Redundant rate in treatment',
+            'max': 1,
+            'min': 0,
+            'format': '{:,.2f}',
+            'scale': 'RdYlBu-rev'
+        }
+        headers['control_redundant_rate'] = {
+            'title': 'Control Redundancy',
+            'description': 'Redundant rate in control',
+            'max': 1,
+            'min': 0,
+            'format': '{:,.2f}',
+            'scale': 'RdYlBu-rev'
+        }
+        self.general_stats_addcols(self.macs_data, headers)
+
+    def macs_filtered_reads_plot(self):
+        """ Plot of filtered reads for control and treatment samples """
+        data = dict()
+        req_cats = ['control_fragments_total', 'control_fragments_after_filtering', 'treatment_fragments_total', 'treatment_fragments_after_filtering']
+        for s_name, d in self.macs_data.items():
+            if all([c in d for c in req_cats]):
+                data['{}: Control'.format(s_name)] = dict()
+                data['{}: Treatment'.format(s_name)] = dict()
+                data['{}: Control'.format(s_name)]['fragments_filtered'] = d['control_fragments_total'] - d['control_fragments_after_filtering']
+                data['{}: Control'.format(s_name)]['fragments_not_filtered'] = d['control_fragments_after_filtering']
+                data['{}: Treatment'.format(s_name)]['fragments_filtered'] = d['treatment_fragments_total'] - d['treatment_fragments_after_filtering']
+                data['{}: Treatment'.format(s_name)]['fragments_not_filtered'] = d['treatment_fragments_after_filtering']
+
+        # Specify the order of the different possible categories
+        keys = OrderedDict()
+        keys['fragments_not_filtered'] = { 'color': '#437BB1', 'name': 'Remaining fragments' }
+        keys['fragments_filtered'] =     { 'color': '#B1084C', 'name': 'Filtered fragments' }
+
+        # Config for the plot
+        pconfig = {
+            'id': 'macs2_filtered',
+            'title': 'MACS2: Filtered Fragments',
+            'ylab': '# Fragments',
+            'cpswitch_counts_label': 'Number of Fragments',
+            'hide_zero_cats': False
+        }
+
+        self.add_section(
+            plot = bargraph.plot(data, keys, pconfig)
+        )
