c1c07315e7736f638ad21d6081e0b532fa32437c
Lorena Pantano <lorena.pantano@gmail.com>
2017-08-04 10:14:33 -0400

    Add Atropos tool

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
         'multiqc.modules.v1': [
             'adapterRemoval = multiqc.modules.adapterRemoval:MultiqcModule',
             'afterqc = multiqc.modules.afterqc:MultiqcModule',
+            'atropos = multiqc.modules.atropos:MultiqcModule',
             'bamtools = multiqc.modules.bamtools:MultiqcModule',
             'bcftools = multiqc.modules.bcftools:MultiqcModule',
             'bcl2fastq = multiqc.modules.bcl2fastq:MultiqcModule',
