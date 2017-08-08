73646f8090d81b667571b76e6955b3bef123ddf2
Phil Ewels <phil.ewels@scilifelab.se>
2017-08-08 11:30:31 +0200

    bcl2fastq: Tidying, base modifiers, new lane statistics table.

--- multiqc/modules/bcl2fastq/bcl2fastq.py
+++ multiqc/modules/bcl2fastq/bcl2fastq.py
@@ -4,7 +4,7 @@ import os
 import json
 from collections import OrderedDict
 from multiqc import config
-from multiqc.plots import bargraph
+from multiqc.plots import bargraph, table
 
 log = logging.getLogger(__name__)
 
@@ -44,6 +44,14 @@ class MultiqcModule(BaseMultiqcModule):
         self.write_data_file({str(k): self.bcl2fastq_bylane[k] for k in self.bcl2fastq_bylane.keys()}, 'multiqc_bcl2fastq_bylane')
         self.write_data_file(self.bcl2fastq_bysample, 'multiqc_bcl2fastq_bysample')
 
+        # Add section for summary stats per flow cell
+        self.add_section (
+            name = 'Lane Statistics',
+            anchor = 'bcl2fastq-lanestats',
+            description = 'Statistics about each lane for each flowcell',
+            plot = self.lane_stats_table()
+        )
+
         # Add section for counts by lane
         cats = OrderedDict()
         cats["perfect"] = {'name': 'Perfect Index Reads'}
@@ -53,7 +61,9 @@ class MultiqcModule(BaseMultiqcModule):
             name = 'Clusters by lane',
             anchor = 'bcl2fastq-bylane',
             description = 'Number of reads per lane (with number of perfect index reads)',
-            helptext = "Perfect index reads are those that do not have a single mismatch. All samples of a lane are combined. Undetermined reads are treated as a third category. To avoid conflicts the run ID is prepended.",
+            helptext = """Perfect index reads are those that do not have a single mismatch.
+                All samples of a lane are combined. Undetermined reads are treated as a third category.
+                To avoid conflicts the run ID is prepended.""",
             plot = bargraph.plot(self.get_bar_data_from_counts(self.bcl2fastq_bylane), cats)
         )
 
@@ -62,7 +72,10 @@ class MultiqcModule(BaseMultiqcModule):
             name = 'Clusters by sample',
             anchor = 'bcl2fastq-bysample',
             description = 'Number of reads per sample (with number of perfect index reads)',
-            helptext = "Perfect index reads are those that do not have a single mismatch. All samples are aggregated across lanes combinned. Undetermined reads are ignored. Undetermined reads are treated as a separate sample. To avoid conflicts the runId is prepended.",
+            helptext = """Perfect index reads are those that do not have a single mismatch.
+                All samples are aggregated across lanes combinned. Undetermined reads are ignored.
+                Undetermined reads are treated as a separate sample.
+                To avoid conflicts the runId is prepended.""",
             plot = bargraph.plot(self.get_bar_data_from_counts(self.bcl2fastq_bysample), cats)
         )
 
@@ -80,48 +93,108 @@ class MultiqcModule(BaseMultiqcModule):
             lane = 'L{}'.format(conversionResult["LaneNumber"])
             if lane in run_data:
                 log.debug("Duplicate runId/lane combination found! Overwriting: {}".format(self.prepend_runid(runId, lane)))
-            run_data[lane] = {"total": 0, "perfectIndex": 0, "samples": dict(), "yieldQ30": 0}
+            run_data[lane] = {
+                "total": 0,
+                "total_yield": 0,
+                "perfectIndex": 0,
+                "samples": dict(),
+                "yieldQ30": 0,
+                "qscore_sum": 0
+            }
             for demuxResult in conversionResult["DemuxResults"]:
                 sample = demuxResult["SampleName"]
                 if sample in run_data[lane]["samples"]:
                     log.debug("Duplicate runId/lane/sample combination found! Overwriting: {}, {}".format(self.prepend_runid(runId, lane),sample))
-                run_data[lane]["samples"][sample] = {"total": 0, "perfectIndex": 0, "filename": os.path.join(myfile['root'],myfile["fn"]), "yieldQ30": 0}
+                run_data[lane]["samples"][sample] = {
+                    "total": 0,
+                    "total_yield": 0,
+                    "perfectIndex": 0,
+                    "filename": os.path.join(myfile['root'],myfile["fn"]),
+                    "yieldQ30": 0,
+                    "qscore_sum": 0
+                }
                 run_data[lane]["total"] += demuxResult["NumberReads"]
+                run_data[lane]["total_yield"] += demuxResult["Yield"]
                 run_data[lane]["samples"][sample]["total"] += demuxResult["NumberReads"]
+                run_data[lane]["samples"][sample]["total_yield"] += demuxResult["Yield"]
                 for indexMetric in demuxResult["IndexMetrics"]:
                     run_data[lane]["perfectIndex"] += indexMetric["MismatchCounts"]["0"]
                     run_data[lane]["samples"][sample]["perfectIndex"] += indexMetric["MismatchCounts"]["0"]
                 for readMetric in demuxResult["ReadMetrics"]:
                     run_data[lane]["yieldQ30"] += readMetric["YieldQ30"]
+                    run_data[lane]["qscore_sum"] += readMetric["QualityScoreSum"]
                     run_data[lane]["samples"][sample]["yieldQ30"] += readMetric["YieldQ30"]
+                    run_data[lane]["samples"][sample]["qscore_sum"] += readMetric["QualityScoreSum"]
             undeterminedYieldQ30 = 0
+            undeterminedQscoreSum = 0
             for readMetric in conversionResult["Undetermined"]["ReadMetrics"]:
                 undeterminedYieldQ30 += readMetric["YieldQ30"]
-            run_data[lane]["samples"]["undetermined"] = {"total": conversionResult["Undetermined"]["NumberReads"], "perfectIndex": 0, "yieldQ30": undeterminedYieldQ30}
+                undeterminedQscoreSum += readMetric["QualityScoreSum"]
+            run_data[lane]["samples"]["undetermined"] = {
+                "total": conversionResult["Undetermined"]["NumberReads"],
+                "total_yield": conversionResult["Undetermined"]["Yield"],
+                "perfectIndex": 0,
+                "yieldQ30": undeterminedYieldQ30,
+                "qscore_sum": undeterminedQscoreSum
+            }
+
+        # Calculate Percents and averages
+        for lane in run_data:
+            run_data[lane]["percent_Q30"] = (float(run_data[lane]["yieldQ30"]) / float(run_data[lane]["total_yield"])) * 100.0
+            run_data[lane]["percent_perfectIndex"] = (float(run_data[lane]["perfectIndex"]) / float(run_data[lane]["total"])) * 100.0
+            run_data[lane]["mean_qscore"] = float(run_data[lane]["qscore_sum"]) / float(run_data[lane]["total_yield"])
+            for sample, d in run_data[lane]["samples"].items():
+                run_data[lane]["samples"][sample]["percent_Q30"] = (float(d["yieldQ30"]) / float(d["total_yield"])) * 100.0
+                run_data[lane]["samples"][sample]["percent_perfectIndex"] = (float(d["perfectIndex"]) / float(d["total"])) * 100.0
+                run_data[lane]["samples"][sample]["mean_qscore"] = float(d["qscore_sum"]) / float(d["total_yield"])
 
     def split_data_by_lane_and_sample(self):
         for runId in self.bcl2fastq_data.keys():
             for lane in self.bcl2fastq_data[runId].keys():
                 uniqLaneName = self.prepend_runid(runId, lane)
                 self.bcl2fastq_bylane[uniqLaneName] = {
-                "total": self.bcl2fastq_data[runId][lane]["total"],
-                "perfectIndex": self.bcl2fastq_data[runId][lane]["perfectIndex"],
-                "undetermined": self.bcl2fastq_data[runId][lane]["samples"]["undetermined"]["total"],
-                "yieldQ30": self.bcl2fastq_data[runId][lane]["yieldQ30"]
+                    "total": self.bcl2fastq_data[runId][lane]["total"],
+                    "total_yield": self.bcl2fastq_data[runId][lane]["total_yield"],
+                    "perfectIndex": self.bcl2fastq_data[runId][lane]["perfectIndex"],
+                    "undetermined": self.bcl2fastq_data[runId][lane]["samples"]["undetermined"]["total"],
+                    "yieldQ30": self.bcl2fastq_data[runId][lane]["yieldQ30"],
+                    "qscore_sum": self.bcl2fastq_data[runId][lane]["qscore_sum"],
+                    "percent_Q30": self.bcl2fastq_data[runId][lane]["percent_Q30"],
+                    "percent_perfectIndex": self.bcl2fastq_data[runId][lane]["percent_perfectIndex"],
+                    "mean_qscore": self.bcl2fastq_data[runId][lane]["mean_qscore"]
                 }
                 for sample in self.bcl2fastq_data[runId][lane]["samples"].keys():
                     if not sample in self.bcl2fastq_bysample:
-                        self.bcl2fastq_bysample[sample] = {"total": 0, "perfectIndex": 0, "yieldQ30": 0}
+                        self.bcl2fastq_bysample[sample] = {
+                            "total": 0,
+                            "total_yield": 0,
+                            "perfectIndex": 0,
+                            "yieldQ30": 0,
+                            "qscore_sum": 0
+                        }
                     self.bcl2fastq_bysample[sample]["total"] += self.bcl2fastq_data[runId][lane]["samples"][sample]["total"]
+                    self.bcl2fastq_bysample[sample]["total_yield"] += self.bcl2fastq_data[runId][lane]["samples"][sample]["total_yield"]
                     self.bcl2fastq_bysample[sample]["perfectIndex"] += self.bcl2fastq_data[runId][lane]["samples"][sample]["perfectIndex"]
                     self.bcl2fastq_bysample[sample]["yieldQ30"] += self.bcl2fastq_data[runId][lane]["samples"][sample]["yieldQ30"]
+                    self.bcl2fastq_bysample[sample]["qscore_sum"] += self.bcl2fastq_data[runId][lane]["samples"][sample]["qscore_sum"]
+                    self.bcl2fastq_bysample[sample]["percent_Q30"] = (float(self.bcl2fastq_bysample[sample]["yieldQ30"]) / float(self.bcl2fastq_bysample[sample]["total_yield"])) * 100.0
+                    self.bcl2fastq_bysample[sample]["percent_perfectIndex"] = (float(self.bcl2fastq_bysample[sample]["perfectIndex"]) / float(self.bcl2fastq_bysample[sample]["total"])) * 100.0
+                    self.bcl2fastq_bysample[sample]["mean_qscore"] = float(self.bcl2fastq_bysample[sample]["qscore_sum"]) / float(self.bcl2fastq_bysample[sample]["total_yield"])
                     if sample != "undetermined":
                         if not sample in self.source_files:
                             self.source_files[sample] = []
                         self.source_files[sample].append(self.bcl2fastq_data[runId][lane]["samples"][sample]["filename"])
 
     def add_general_stats(self):
-        data = {key: {"yieldQ30": self.bcl2fastq_bysample[key]["yieldQ30"], "total": self.bcl2fastq_bysample[key]["total"], "perfectPercent": '{0:.1f}'.format(float(100.0*self.bcl2fastq_bysample[key]["perfectIndex"]/self.bcl2fastq_bysample[key]["total"]))} for key in self.bcl2fastq_bysample.keys()}
+        data = {
+            key: {
+                "yieldQ30": self.bcl2fastq_bysample[key]["yieldQ30"],
+                "total": self.bcl2fastq_bysample[key]["total"],
+                "perfectPercent": '{0:.1f}'.format(
+                    float( 100.0 * self.bcl2fastq_bysample[key]["perfectIndex"] / self.bcl2fastq_bysample[key]["total"] )
+                )
+            } for key in self.bcl2fastq_bysample.keys()
+        }
         headers = OrderedDict()
         headers['total'] = {
             'title': '{} Total Reads'.format(config.read_count_prefix),
@@ -131,21 +204,73 @@ class MultiqcModule(BaseMultiqcModule):
             'modify': lambda x: x * config.read_count_multiplier,
             'shared_key': 'read_count'
         }
+        headers['yieldQ30'] = {
+            'title': '{} Yield &ge; Q30'.format(config.base_count_prefix),
+            'description': 'Number of bases with a Phred score of 30 or higher ({})'.format(config.base_count_desc),
+            'min': 0,
+            'scale': 'Greens',
+            'modify': lambda x: x * config.base_count_multiplier,
+            'shared_key': 'base_count'
+        }
         headers['perfectPercent'] = {
-            'title': 'Perfect Index Read Percentage',
+            'title': '% Perfect Index',
             'description': 'Percent of reads with perfect index (0 mismatches)',
             'max': 100,
             'min': 0,
             'scale': 'RdYlGn',
             'suffix': '%'
         }
-        headers['yieldQ30'] = {
-            'title': 'Yield Q30',
-            'description': 'Number of bases with a Phred score of 30 or higher',
-            'min': 0,
-            'scale': 'Greens'
-        }
         self.general_stats_addcols(data, headers)
+
+    def lane_stats_table(self):
+        """ Return a table with overview stats for each bcl2fastq lane for a single flow cell """
+        headers = OrderedDict()
+        headers['total_yield'] = {
+            'title': '{} Total Yield'.format(config.base_count_prefix),
+            'description': 'Number of bases ({})'.format(config.base_count_desc),
+            'min': 0,
+            'scale': 'Greens',
+            'modify': lambda x: x * config.base_count_multiplier,
+            'shared_key': 'base_count'
+        }
+        headers['total'] = {
+            'title': '{} Total Clusters'.format(config.read_count_prefix),
+            'description': 'Total number of clusters for this lane ({})'.format(config.read_count_desc),
+            'min': 0,
+            'scale': 'Blues',
+            'modify': lambda x: x * config.read_count_multiplier,
+            'shared_key': 'read_count'
+        }
+        headers['percent_Q30'] = {
+            'title': '% bases &ge; Q30',
+            'description': 'Percentage of bases with greater than or equal to Q30 quality score',
+            'suffix': '%',
+            'max': 100,
+            'min': 0,
+            'scale': 'RdYlGn'
+        }
+        headers['mean_qscore'] = {
+            'title': 'Mean Quality',
+            'description': 'Average phred qualty score',
+            'min': 0,
+            'scale': 'Spectral'
+        }
+        headers['percent_perfectIndex'] = {
+            'title': '% Perfect Index',
+            'description': 'Percent of reads with perfect index (0 mismatches)',
+            'max': 100,
+            'min': 0,
+            'scale': 'RdYlGn',
+            'suffix': '%'
+        }
+        table_config = {
+            'namespace': 'bcl2fastq',
+            'id': 'bcl2fastq-lane-stats-table',
+            'table_title': 'bcl2fastq Lane Statistics',
+            'col1_header': 'Run ID - Lane',
+            'no_beeswarm': True
+        }
+        return table.plot(self.bcl2fastq_bylane, headers, table_config)
 
     def prepend_runid(self, runId, rest):
         return str(runId)+" - "+str(rest)
