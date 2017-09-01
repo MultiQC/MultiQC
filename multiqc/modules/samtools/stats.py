6e7a185e0c6fbdde0388e7c61820f63f30c5059a
vladsaveliev <vladsaveliev@me.com>
2017-09-02 02:33:46 +0300

    Samtools-stats helptext formatting

--- multiqc/modules/samtools/stats.py
+++ multiqc/modules/samtools/stats.py
@@ -153,18 +153,17 @@ class StatsReportMixin():
             name = 'Percent Mapped',
             anchor = 'samtools-stats-alignment',
             description = "Alignment metrics from <code>samtools stats</code>; mapped vs. unmapped reads.",
-            helptext="""For a set of samples that have come from the same multiplexed library, 
-        similar numbers of reads for each sample are expected. Large differences in numbers might 
-        indicate issues during the library preparation process. Whilst large differences in read 
-        numbers may be controlled for in downstream processings (e.g. read count normalisation), 
-        you may wish to consider whether the read depths achieved have fallen below recommended 
-        levels depending on the applications (e.g. ~ 10 million reads for differential gene
-        expression analysis.)</p>
-        <p>A high-quality library should have ~95% of reads align. Low alignment rates could
-        indicate contamination of samples (e.g. adapter sequences), low sequencing quality or 
-        other artefacts. These can be further investigated in the sequence level QC (e.g. from 
-        FastQC). If the percentage is not 100%, the lack is reflective of read pairs where one 
-        mate didn't align.""",
+            helptext = """
+            For a set of samples that have come from the same multiplexed library, 
+            similar numbers of reads for each sample are expected. Large differences in numbers might 
+            indicate issues during the library preparation process. Whilst large differences in read 
+            numbers may be controlled for in downstream processings (e.g. read count normalisation), 
+            you may wish to consider whether the read depths achieved have fallen below recommended 
+            levels depending on the applications.
+            
+            Low alignment rates could indicate contamination of samples (e.g. adapter sequences), 
+            low sequencing quality or other artefacts. These can be further investigated in the 
+            sequence level QC (e.g. from FastQC).""",
             plot = alignment_chart(bedgraph_data)
         )
 
