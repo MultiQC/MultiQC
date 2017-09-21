539247fad18cb4b5a75309cee865e75610e1addd
dpryan79 <dpryan79@gmail.com>
2017-09-21 14:15:42 +0200

    Support all known SNPsplit output types

--- multiqc/modules/snpsplit/snpsplit.py
+++ multiqc/modules/snpsplit/snpsplit.py
@@ -34,28 +34,47 @@ class MultiqcModule(BaseMultiqcModule):
                     log.warning("Replacing duplicate sample {}".format(parsed[0]))
                 d[parsed[0]] = parsed[1]
                 self.add_data_source(f, section='SNPsplit')
-                for k, v in parsed[1].items():
-                    log.info("{}: {}".format(k, v))
 
         if len(d) > 0:
-            # Allele-tagging report
+            # Allele-tagging report, the order matches that in the text file
             cats = OrderedDict()
-            #cats['processed'] = dict(name='Total alignments processed')
             cats['skipped'] = dict(name='Unaligned reads')
             cats['unassignable'] = dict(name='Reads that could not be assigned to a genome')
             cats['genome1'] = dict(name='Reads assigned to genome 1')
             cats['genome2'] = dict(name='Reads assigned to genome 2')
-            cats['conflicting'] = dict(name='Reads containing conflicting SNPs')
-            cats['ambiguous'] = dict(name='Reads did not match either genome')
-            # Subtract ambiguous from unassignable
+            # Subtract ambiguous and C->T from unassignable
+            CT = False
             for k, v in d.items():
                 if 'unassignable' and 'ambiguous' in v:
                     v['unassignable'] -= v['ambiguous']
+                if 'unassignable' and 'unassignableCT' in v:
+                    v['unassignable'] -= v['unassignableCT']
+                    CT = True
+            if CT:
+                cats['unassignableCT'] = dict(name='Reads unassigned due to a C->T SNP')
+            cats['ambiguous'] = dict(name='Reads did not match either genome')
+            cats['conflictingSNPs'] = dict(name='Reads containing conflicting SNPs')
+
             self.add_section(name="Allele-tagging report",
+                             description="Per-sample metrics of how many reads were assigned to each genome.",
+                             helptext="Possible reasons for not assigning reads to a genome are that they aren't aligned ('Unaligned reads'), they don't overlap a SNP ('Reads that could not be assigned to a genome'), they overlap informative SNPs, but don't contain the expected nucleotide for either genome ('Reads did not match either genome'), or the reads overlapped multiple informative SNPs, but there was a conflict in support for assignment to one genome over the other between the SNPs ('Reads contain conflicting SNPs'). Additionally, for bisulfite sequencing data reads overlapping C->T SNPs may be unassigned ('Reads unassigned due to a C->T SNP').",
                              anchor="SNPsplit",
                              plot=bargraph.plot(d, cats))
 
-            # SNP-coverage report
+            # Allele-specific sorting report
+            cats = OrderedDict()
+            cats['unassignableReads'] = dict(name='Reads that could not be assigned to a genome')
+            cats['genome1Reads'] = dict(name='Reads assigned to genome 1')
+            cats['genome2Reads'] = dict(name='Reads assigned to genome 2')
+            cats['G1UA'] = dict(name='PE Reads with one mate assigned to genome 1 and the other unassignable')
+            cats['G2UA'] = dict(name='PE Reads with one mate assigned to genome 2 and the other unassignable')
+            cats['G1G2'] = dict(name='PE Reads with mates assigned to different genomes')
+            cats['conflictingReads'] = dict(name='Reads containing conflicting SNPs')
+            self.add_section(name="Allele-specific sorting",
+                             description="Per-sample metrics of how reads and pairs of reads were sorted into each genome.",
+                             helptext="Note that metrics here may differ from those in the allele-tagging report. This occurs when paired-end reads are used, since 'tagging' only one read in a pair as arising from one genome can suffice in both reads being sorted there. Similarly, if two reads in a pair are tagged as arising from different genomes then the pair becomes unassignable.",
+                             anchor="SNPsplit",
+                             plot=bargraph.plot(d, cats))
 
         return len(d)
 
@@ -74,10 +93,6 @@ class MultiqcModule(BaseMultiqcModule):
                     continue
 
                 # Allele-tagging report
-                match = re.match(r"Processed (\d+) read alignments in total", line)
-                if match:
-                    d["processed"] = int(match.group(1))
-                    continue
                 match = re.match(r"Reads were unaligned and hence skipped: (\d+)", line)
                 if match:
                     d["skipped"] = int(match.group(1))
@@ -108,72 +123,44 @@ class MultiqcModule(BaseMultiqcModule):
                     d["conflictingSNPs"] = int(match.group(1))
                     continue
 
-                # SNP coverage report
-                match = re.match(r"SNPs stored in total:\W+(\d+)", line) 
-                if match:
-                    d["totalStoredSNPs"] = int(match.group(1))
+                # Allele-specific sorting report
+                if line.startswith("Reads were unassignable"):
+                    d["unassignableReads"] = int(line.split("\t")[-1].split()[0])
                     continue
-                match = re.match(r"N-containing reads:\W+(\d+)", line) 
-                if match:
-                    d["Nreads"] = int(match.group(1))
+                if line.startswith("Read pairs were unassignable (UA/UA):"):
+                    d["unassignableReads"] = int(line.split("\t")[-1].split()[0])
                     continue
-                match = re.match(r"non-N:\W+(\d+)", line) 
-                if match:
-                    d["nonNreads"] = int(match.group(1))
+                if line.startswith("Reads were specific for genome 1"):
+                    d["genome1Reads"] = int(line.split("\t")[-1].split()[0])
                     continue
-                match = re.match(r"total:\W+(\d+)", line) 
-                if match:
-                    d["totalReads"] = int(match.group(1))
+                if line.startswith("Read pairs were specific for genome 1 (G1/G1)"):
+                    d["genome1Reads"] = int(line.split("\t")[-1].split()[0])
                     continue
-                if line.startswith("Reads had a deletion of the N-masked position (and were thus dropped)"):
-                    d["deletedNReads"] = int(line.split("\t")[1].split()[0])
+                if line.startswith("Reads were specific for genome 2"):
+                    d["genome2Reads"] = int(line.split("\t")[-1].split()[0])
                     continue
-                match = re.match(r"Of which had multiple deletions of N-masked positions within the same read:	(\d+)", line)
-                if match:
-                    d["multiDeletedNReads"] = int(match.group(1))
+                if line.startswith("Read pairs were specific for genome 2 (G2/G2)"):
+                    d["genome2Reads"] = int(line.split("\t")[-1].split()[0])
                     continue
-
-                # Of valid N-containing reads
-                match = re.match(r"N was present in the list of known SNPs:\W+(\d+)", line)
-                if match:
-                    d["NIsKnownSNP"] = int(match.group(1))
+                if line.startswith("Read pairs were a mix of G1 and UA"):
+                    d["G1UA"] = int(line.split("\t")[-1].split()[0])
                     continue
-                # Generally not present
-                match = re.match(r"Positions were skipped since they involved C>T SNPs:\W+(\d+)", line)
-                if match:
-                    d["skippedCTSNPs"] = int(match.group(1))
+                if line.startswith("Read pairs were a mix of G2 and UA"):
+                    d["G2UA"] = int(line.split("\t")[-1].split()[0])
                     continue
-                match = re.match(r"N was not present in the list of SNPs:\W+(\d+)", line)
-                if match:
-                    d["NIsUnknownSNP"] = int(match.group(1))
-                    continue
-
-                # Allele-specific paired-end sorting report
-                match = re.match(r"Read pairs/singletons processed in total:\W+(\d+)", line)
-                if match:
-                    d["totalReadProcessed"] = int(match.group(1))
-                    continue
-                if line.startswith("Reads were unassignable (not overlapping SNPs):		"):
-                    d["unassignableReads"] = int(line.split("\t")[2].split()[0])
-                    continue
-                match = re.match(r"Reads were specific for genome 1:\W+(\d+)", line)
-                if match:
-                    d["genome1Reads"] = int(match.group(1))
-                    continue
-                match = re.match(r"Reads were specific for genome 2:\W+(\d+)", line)
-                if match:
-                    d["genome2Reads"] = int(match.group(1))
+                if line.startswith("Read pairs were a mix of G1 and G2"):
+                    d["G1G2"] = int(line.split("\t")[-1].split()[0])
                     continue
                 match = re.match(r"Reads contained conflicting SNP information:\W+(\d+)", line)
                 if match:
                     d["conflictingReads"] = int(match.group(1))
                     continue
+                if line.startswith("Read pairs contained conflicting SNP information"):
+                    d["conflictingReads"] = int(line.split("\t")[-1].split()[0])
+                    continue
         except:
             log.warning("{} was initially flagged as the output from SNPsplit, but that seems to not be the case. Skipping...".format(fname))
             return []
-
-        # check the number of entries in d (see SNPsplit source code)
-        log.info(len(d.keys()))
         l.append(d)
 
         return l
