9d06079dc33921907db8a09d2f5c58a2c2b7e98f
Abhishek S P <abhisheksp1993@gmail.com>
2017-11-04 22:19:31 -0400

    plot gc bias

--- multiqc/modules/salmon/salmon.py
+++ multiqc/modules/salmon/salmon.py
@@ -3,14 +3,17 @@
 """ MultiQC module to parse output from Salmon """
 
 from __future__ import print_function
-from collections import OrderedDict
+
 import json
 import logging
 import os
+from collections import OrderedDict
 
-from multiqc import config
-from multiqc.plots import linegraph
+import numpy
+
 from multiqc.modules.base_module import BaseMultiqcModule
+from multiqc.modules.salmon.gcmodel import GCModel
+from multiqc.plots import linegraph
 
 # Initialise the logger
 log = logging.getLogger(__name__)
@@ -33,11 +36,13 @@ class MultiqcModule(BaseMultiqcModule):
             self.salmon_meta[s_name] = json.loads(f['f'])
         # Parse Fragment Length Distribution logs
         self.salmon_fld = dict()
+        self.salmon_gc = []
         for f in self.find_log_files('salmon/fld'):
             # Get the s_name from the parent directory
             if os.path.basename(f['root']) == 'libParams':
                 s_name = os.path.basename(os.path.dirname(f['root']))
                 s_name = self.clean_s_name(s_name, f['root'])
+                self.parse_gc_bias(f['root'])
                 parsed = OrderedDict()
                 for i, v in enumerate(f['f'].split()):
                     parsed[i] = float(v)
@@ -46,14 +51,12 @@ class MultiqcModule(BaseMultiqcModule):
                         log.debug("Duplicate sample name found! Overwriting: {}".format(s_name))
                     self.add_data_source(f, s_name)
                     self.salmon_fld[s_name] = parsed
-
         # Filter to strip out ignored sample names
         self.salmon_meta = self.ignore_samples(self.salmon_meta)
         self.salmon_fld = self.ignore_samples(self.salmon_fld)
 
         if len(self.salmon_meta) == 0 and len(self.salmon_fld) == 0:
             raise UserWarning
-
         if len(self.salmon_meta) > 0:
             log.info("Found {} meta reports".format(len(self.salmon_meta)))
             self.write_data_file(self.salmon_meta, 'multiqc_salmon')
@@ -92,3 +95,32 @@ class MultiqcModule(BaseMultiqcModule):
             'tt_label': '<b>{point.x:,.0f} bp</b>: {point.y:,.0f}',
         }
         self.add_section(plot=linegraph.plot(self.salmon_fld, pconfig))
+        self.plot_gc_bias()
+
+    def plot_gc_bias(self):
+        pconfig = {
+            'smooth_points': 500,
+            'id': 'salmon_gc_plot',
+            'title': 'GC Bias',
+            'ylab': 'Obs/Exp ratio',
+            'xlab': 'bins',
+            'ymin': 0,
+            'xmin': 0,
+            'tt_label': '<b>{point.x:,.0f} bp</b>: {point.y:,.0f}',
+        }
+        for sample_gc in self.salmon_gc:
+            ratio = numpy.divide(sample_gc.obs_, sample_gc.exp_)
+            plot_details = {
+                0: OrderedDict(enumerate(ratio[0])),
+                1: OrderedDict(enumerate(ratio[1])),
+                2: OrderedDict(enumerate(ratio[2]))
+            }
+            self.add_section(plot=linegraph.plot(plot_details, pconfig))
+
+    def parse_gc_bias(self, f_root):
+        is_exp_gc_exists = os.path.exists(os.path.join(os.path.dirname(f_root), 'aux_info', 'exp_gc.gz'))
+        is_obs_gc_exists = os.path.exists(os.path.join(os.path.dirname(f_root), 'aux_info', 'obs_gc.gz'))
+        if is_exp_gc_exists and is_obs_gc_exists:
+            gc = GCModel()
+            gc.from_file(os.path.dirname(f_root))
+            self.salmon_gc.append(gc)
