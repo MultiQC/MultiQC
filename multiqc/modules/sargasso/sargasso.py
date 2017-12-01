c8b9f68245f4096d3e8e6c32d71d0bb2a5ded8e8
hxin <hxin87@gmail.com>
2017-12-01 16:40:27 +0000

    first implementation. Showing a bar plot together with a general statistic table for sargasso log.

--- multiqc/modules/sargasso/sargasso.py
+++ multiqc/modules/sargasso/sargasso.py
@@ -25,117 +25,136 @@ class MultiqcModule(BaseMultiqcModule):
         info="is a tool to separate mixed-species RNA-seq reads"\
              "according to their species of origin.")
 
-    #     # Find and load any featureCounts reports
-    #     self.featurecounts_data = dict()
-    #     self.featurecounts_keys = list()
-    #     for f in self.find_log_files('featurecounts'):
-    #         self.parse_featurecounts_report(f)
-    #
-    #     # Filter to strip out ignored sample names
-    #     self.featurecounts_data = self.ignore_samples(self.featurecounts_data)
-    #
-    #     if len(self.featurecounts_data) == 0:
-    #         raise UserWarning
-    #
-    #     log.info("Found {} reports".format(len(self.featurecounts_data)))
-    #
-    #     # Write parsed report data to a file
-    #     self.write_data_file(self.featurecounts_data, 'multiqc_featureCounts')
-    #
-    #     # Basic Stats Table
-    #     self.featurecounts_stats_table()
-    #
-    #     # Assignment bar plot
-    #     self.add_section( plot = self.featureCounts_chart() )
-    #
-    #
-    # def parse_featurecounts_report (self, f):
-    #     """ Parse the featureCounts log file. """
-    #
-    #     file_names = list()
-    #     parsed_data = dict()
-    #     for l in f['f'].splitlines():
-    #         thisrow = list()
-    #         s = l.split("\t")
-    #         if len(s) < 2:
-    #             continue
-    #         if s[0] == 'Status':
-    #             for f_name in s[1:]:
-    #                 file_names.append(f_name)
-    #         else:
-    #             k = s[0]
-    #             if k not in self.featurecounts_keys:
-    #                 self.featurecounts_keys.append(k)
-    #             for val in s[1:]:
-    #                 try:
-    #                     thisrow.append(int(val))
-    #                 except ValueError:
-    #                     pass
-    #         if len(thisrow) > 0:
-    #             parsed_data[k] = thisrow
-    #     # Check that this actually is a featureCounts file, as format and parsing is quite general
-    #     if 'Assigned' not in parsed_data.keys():
-    #         return None
-    #     for idx, f_name in enumerate(file_names):
-    #
-    #         # Clean up sample name
-    #         s_name = self.clean_s_name(f_name, f['root'])
-    #
-    #         # Reorganised parsed data for this sample
-    #         # Collect total count number
-    #         data = dict()
-    #         data['Total'] = 0
-    #         for k in parsed_data:
-    #             data[k] = parsed_data[k][idx]
-    #             data['Total'] += parsed_data[k][idx]
-    #
-    #         # Calculate the percent aligned if we can
-    #         try:
-    #             data['percent_assigned'] = (float(data['Assigned'])/float(data['Total'])) * 100.0
-    #         except (KeyError, ZeroDivisionError):
-    #             pass
-    #
-    #         # Add to the main dictionary
-    #         if len(data) > 1:
-    #             if s_name in self.featurecounts_data:
-    #                 log.debug("Duplicate sample name found! Overwriting: {}".format(s_name))
-    #             self.add_data_source(f, s_name)
-    #             self.featurecounts_data[s_name] = data
-    #
-    #
-    # def featurecounts_stats_table(self):
-    #     """ Take the parsed stats from the featureCounts report and add them to the
-    #     basic stats table at the top of the report """
-    #
-    #     headers = OrderedDict()
-    #     headers['percent_assigned'] = {
-    #         'title': '% Assigned',
-    #         'description': '% Assigned reads',
-    #         'max': 100,
-    #         'min': 0,
-    #         'suffix': '%',
-    #         'scale': 'RdYlGn'
-    #     }
-    #     headers['Assigned'] = {
-    #         'title': '{} Assigned'.format(config.read_count_prefix),
-    #         'description': 'Assigned reads ({})'.format(config.read_count_desc),
-    #         'min': 0,
-    #         'scale': 'PuBu',
-    #         'modify': lambda x: float(x) * config.read_count_multiplier,
-    #         'shared_key': 'read_count'
-    #     }
-    #     self.general_stats_addcols(self.featurecounts_data, headers)
-    #
-    #
-    # def featureCounts_chart (self):
-    #     """ Make the featureCounts assignment rates plot """
-    #
-    #     # Config for the plot
-    #     config = {
-    #         'id': 'featureCounts_assignment_plot',
-    #         'title': 'featureCounts: Assignments',
-    #         'ylab': '# Reads',
-    #         'cpswitch_counts_label': 'Number of Reads'
-    #     }
-    #
-    #     return bargraph.plot(self.featurecounts_data, self.featurecounts_keys, config)
+         # Find and load any Sargasso reports
+        self.sargasso_data = dict()
+        self.sargasso_keys = list()
+        for f in self.find_log_files('sargasso'):
+            self.parse_sargasso_logs(f)
+            # self.sargasso_data[f['s_name'].split(' | ')[-2]] = self.parse_sargasso_logs(f['f'])
+            # self.sargasso_data[f['s_name']] = self.parse_sargasso_logs(f['f'])
+
+        log.info('Parsing log file...')
+        log.info(self.sargasso_data)
+
+        #sample name has been changed, think again
+        log.info('Removing innored samples...')
+        self.sargasso_data = self.ignore_samples(self.sargasso_data)
+
+        if len(self.sargasso_data) == 0:
+            raise UserWarning
+
+        log.info("Found {} reports".format(len(self.sargasso_data)))
+
+        # Write parsed report data to a file
+        self.write_data_file(self.sargasso_data, 'multiqc_sargasso')
+
+        # Basic Stats Table
+        self.sargasso_stats_table()
+
+        # Assignment bar plot
+        self.add_section( plot = self.sargasso_chart() )
+
+        log.info('done')
+
+
+    def _chunks(l, n):
+        n = max(1, n)
+        return (l[i:i+n] for i in xrange(0, len(l), n))
+
+    def parse_sargasso_logs(self, f):
+        """ Parse the sargasso log file. """
+        # file_names = list()
+        parsed_data = dict()
+
+        species_name = list()
+        items = list()
+        header = list()
+        for l in f['f'].splitlines():
+            thisrow = list()
+            s = l.split(",")
+            if len(s) < 7:
+                continue
+            if s[0] == 'Sample':
+                header = s
+                for i in header[1:]:
+                    #find out what species included
+                    sname = i.split('-')[-1]
+                    if sname not in species_name:
+                        species_name.append(sname)
+                    #find out what is being counted
+                    kname = ("-".join(i.split('-')[-3:-1]))
+                    if kname not in items:
+                        items.append(kname)
+            else:
+                #start sample lines.
+                sample_name = s.pop(0)
+                chunk_by_species = [s[i:i + len(items)] for i in xrange(0, len(s), len(items))];
+                for idx,v in enumerate(chunk_by_species):
+                    new_sample_name = '-'.join([sample_name, f['s_name'].split(' | ')[-2], species_name[idx]])
+
+                    if new_sample_name in self.sargasso_data.keys():
+                        log.debug("Duplicate sample name found! Overwriting: {}".format(new_sample_name))
+
+                    try:
+                        self.sargasso_data[new_sample_name] = dict(zip(items,map(int, v)))
+                    except ValueError:
+                        pass
+
+
+        # Check that this actually is a featureCounts file
+        if 'Filtered-Hits' not in items:
+            return None
+
+        self.sargasso_keys = items
+
+        for idx, f_name in enumerate(self.sargasso_data.keys()):
+
+            # Clean up sample name
+            s_name = self.clean_s_name(f_name, f['root'])
+
+            # Reorganised parsed data for this sample
+            # Collect total count number
+            self.sargasso_data[f_name]['Total'] = sum (self.sargasso_data[f_name].values())
+            # Calculate the percent aligned if we can
+            try:
+                self.sargasso_data[f_name]['sargasso_percent_assigned'] = (float(self.sargasso_data[f_name]['Filtered-Reads'])/float(self.sargasso_data[f_name]['Total'])) * 100.0
+            except (KeyError, ZeroDivisionError):
+                pass
+
+
+    def sargasso_stats_table(self):
+        """ Take the parsed stats from the featureCounts report and add them to the
+        basic stats table at the top of the report """
+
+        headers = OrderedDict()
+        headers['sargasso_percent_assigned'] = {
+            'title': 'sargasso % Assigned',
+            'description': 'sargasso % Assigned reads',
+            'max': 100,
+            'min': 0,
+            'suffix': '%',
+            'scale': 'RdYlGn'
+        }
+        headers['Filtered-Reads'] = {
+            'title': 'sargasso {} Assigned'.format(config.read_count_prefix),
+            'description': 'sargasso Assigned reads ({})'.format(config.read_count_desc),
+            'min': 0,
+            'scale': 'PuBu',
+            'modify': lambda x: float(x) * config.read_count_multiplier,
+            'shared_key': 'read_count'
+        }
+        self.general_stats_addcols(self.sargasso_data, headers)
+
+
+    def sargasso_chart (self):
+        """ Make the featureCounts assignment rates plot """
+
+        # Config for the plot
+        config = {
+            'id': 'sargasso_assignment_plot',
+            'title': 'Sargasso:',
+            'ylab': '# Reads',
+            'cpswitch_counts_label': 'Number of Reads'
+        }
+
+        return bargraph.plot(self.sargasso_data, self.sargasso_keys, config)
