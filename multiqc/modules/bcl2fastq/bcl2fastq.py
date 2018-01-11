1da00e155eacc1645bfc67807b99c5ad0a61da95
aledj2 <aledjones@nhs.net>
2018-01-11 15:16:56 +0000

    try fix bcl2fastq issue

--- multiqc/modules/bcl2fastq/bcl2fastq.py
+++ multiqc/modules/bcl2fastq/bcl2fastq.py
@@ -181,7 +181,7 @@ class MultiqcModule(BaseMultiqcModule):
             except:
                 run_data[lane]["mean_qscore"] = "NA"
             for sample, d in run_data[lane]["samples"].items():
-                try
+                try:
                     run_data[lane]["samples"][sample]["percent_Q30"] = (float(d["yieldQ30"]) / float(d["total_yield"])) * 100.0
                 except:
                     run_data[lane]["samples"][sample]["percent_Q30"] = "NA"
@@ -226,9 +226,22 @@ class MultiqcModule(BaseMultiqcModule):
                     self.bcl2fastq_bysample[sample]["perfectIndex"] += self.bcl2fastq_data[runId][lane]["samples"][sample]["perfectIndex"]
                     self.bcl2fastq_bysample[sample]["yieldQ30"] += self.bcl2fastq_data[runId][lane]["samples"][sample]["yieldQ30"]
                     self.bcl2fastq_bysample[sample]["qscore_sum"] += self.bcl2fastq_data[runId][lane]["samples"][sample]["qscore_sum"]
-                    self.bcl2fastq_bysample[sample]["percent_Q30"] = (float(self.bcl2fastq_bysample[sample]["yieldQ30"]) / float(self.bcl2fastq_bysample[sample]["total_yield"])) * 100.0
-                    self.bcl2fastq_bysample[sample]["percent_perfectIndex"] = (float(self.bcl2fastq_bysample[sample]["perfectIndex"]) / float(self.bcl2fastq_bysample[sample]["total"])) * 100.0
-                    self.bcl2fastq_bysample[sample]["mean_qscore"] = float(self.bcl2fastq_bysample[sample]["qscore_sum"]) / float(self.bcl2fastq_bysample[sample]["total_yield"])
+                    try:
+                        self.bcl2fastq_bysample[sample]["percent_Q30"] = (float(self.bcl2fastq_bysample[sample]["yieldQ30"]) / float(self.bcl2fastq_bysample[sample]["total_yield"])) * 100.0
+                    except:
+                        self.bcl2fastq_bysample[sample]["percent_Q30"] = "NA"
+                    try:
+                        self.bcl2fastq_bysample[sample]["percent_perfectIndex"] = (float(self.bcl2fastq_bysample[sample]["perfectIndex"]) / float(self.bcl2fastq_bysample[sample]["total"])) * 100.0
+                    except:
+                        self.bcl2fastq_bysample[sample]["percent_perfectIndex"] = "NA"
+                    try:
+                        self.bcl2fastq_bysample[sample]["percent_perfectIndex"] = (float(self.bcl2fastq_bysample[sample]["perfectIndex"]) / float(self.bcl2fastq_bysample[sample]["total"])) * 100.0
+                    except:
+                        self.bcl2fastq_bysample[sample]["percent_perfectIndex"] = "NA"
+                    try:
+                        self.bcl2fastq_bysample[sample]["mean_qscore"] = float(self.bcl2fastq_bysample[sample]["qscore_sum"]) / float(self.bcl2fastq_bysample[sample]["total_yield"])
+                    except:
+                        self.bcl2fastq_bysample[sample]["mean_qscore"] = "NA"
                     if sample != "undetermined":
                         if not sample in self.source_files:
                             self.source_files[sample] = []
