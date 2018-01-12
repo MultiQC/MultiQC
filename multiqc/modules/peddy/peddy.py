583ea58e858a23e012ecb24a159d1f867a591b72
aledj2 <aledjones@nhs.net>
2018-01-12 13:01:17 +0000

    change the way peddy sex check is reported

--- multiqc/modules/peddy/peddy.py
+++ multiqc/modules/peddy/peddy.py
@@ -70,13 +70,13 @@ class MultiqcModule(BaseMultiqcModule):
         self.peddy_general_stats_table()
 
         # PCA plot
-        #self.peddy_pca_plot()
+        self.peddy_pca_plot()
 
         # Relatedness plot
-        #self.peddy_relatedness_plot()
+        self.peddy_relatedness_plot()
         
         # hetcheck plot
-        #self.peddy_het_check_plot()
+        self.peddy_het_check_plot()
 
 
     def parse_peddy_summary(self, f):
@@ -122,12 +122,10 @@ class MultiqcModule(BaseMultiqcModule):
                 parsed_data[s_name] = dict()
                 for i, v in enumerate(s):
                     if i not in s_name_idx:
-                        if headers[i] == "error":
+                        if headers[i] == "error" and pattern == "sex_check":
                             if v == "True":
-                                #log.debug(v)
                                 v = "False"
                             elif v == "False":
-                                #log.debug(v)
                                 v = "True"
                         try:
                             # add the pattern as a suffix to key
