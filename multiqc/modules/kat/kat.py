c1b151729d0bfc7e61efbb8aac04ca6b617f2727
Daniel Mapleson <daniel.mapleson@earlham.ac.uk>
2018-04-24 14:09:17 +0100

    Addressing @ewels comments

--- multiqc/modules/kat/kat.py
+++ multiqc/modules/kat/kat.py
@@ -15,14 +15,14 @@ log = logging.getLogger(__name__)
 class MultiqcModule(BaseMultiqcModule):
     def __init__(self):
         # Initialise the parent object
-        super(MultiqcModule, self).__init__(name='KAT', anchor='kat',
+        super(MultiqcModule, self).__init__(name='K-mer Analysis Toolkit', anchor='kat',
                                             href="https://github.com/TGAC/KAT",
                                             info="is an toolkit for analysing sequencing data via its k-mer spectra.")
 
         # Find and load any KAT dist analysis reports
         self.kat_data = dict()
         for c_file in self.find_log_files('kat'):
-            s_name = self.clean_s_name(c_file['s_name'][:-14], c_file['root'])
+            s_name = self.clean_s_name(c_file['s_name'].replace(".dist_analysis", ""), c_file['root'])
             content = json.loads(c_file['f'])
             self.kat_data[s_name] = self.parse_kat_report(content)
 
@@ -41,34 +41,31 @@ class MultiqcModule(BaseMultiqcModule):
         headers["kmer_peaks"] = {
             'title': '# of Kmer Peaks',
             'description': "Number of peaks identified in the K-mer spectra",
-            'format': '{:,.0f}',
-            'custom_style': 'width=10%'
+            'scale': False,
+            'format': '{:,.0f}'
         }
         headers["gc_peaks"] = {
             'title': '# of GC Peaks',
             'description': "Number of peaks identified in the GC distribution",
-            'format': '{:,.0f}',
-            'custom_style': 'width=10%'
+            'scale': False,
+            'format': '{:,.0f}'
         }
         headers["est_genome_size"] = {
             'title': 'Est. genome Size',
             'description': "Estimated Genome Size based on K-mer spectra",
-            'format': '{:,.0f}',
-            'custom_style': 'width=20%'
+            'scale': "BuPu",
+            'format': '{:,.0f}'
         }
         headers["mean_kmer_freq"] = {
             'title': 'Mean K-mer Freq.',
             'description': "Mean K-mer Frequency, provides an estimate of sequencing coverage",
+            'scale': "Greens",
             'format': '{:,.0f}',
-            'custom_style': 'width=20%',
             'suffix': 'x'
         }
 
         kat_config = {
             'namespace': 'KAT',
-            'scale': 'RdYlGn',
-            'save_file': True,
-            'raw_data_fn': 'mqc_kat_table'
         }
 
         # Basic Stats Table
@@ -101,6 +98,6 @@ class MultiqcModule(BaseMultiqcModule):
             table_data['est_genome_size'] = content['est_genome_size']
             table_data['gc_peaks'] = 0
         else:
-            raise ValueError("Unexpected JSON configuration")
+            log.error("Unexpected JSON configuration")
 
         return table_data
