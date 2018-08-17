f7e83382e94fbcc2898a7eb241f08620cf280403
Chuan Wang <chuan.wang@scilifelab.se>
2018-08-17 10:47:15 +0200

    Add function for plotting strand correlation for phantompeakqualtools

--- multiqc/modules/phantompeakqualtools/phantompeakqualtools.py
+++ multiqc/modules/phantompeakqualtools/phantompeakqualtools.py
@@ -7,9 +7,11 @@ from __future__ import print_function
 from collections import OrderedDict
 import logging
 import re
+import csv
 
 from multiqc import config
 from multiqc.modules.base_module import BaseMultiqcModule
+from multiqc.plots import linegraph
 
 # Initialise the logger
 log = logging.getLogger(__name__)
@@ -25,23 +27,43 @@ class MultiqcModule(BaseMultiqcModule):
 
         # Parse logs
         self.phantompeakqualtools_data = dict()
-        for f in self.find_log_files('phantompeakqualtools', filehandles=False):
+        for f in self.find_log_files('phantompeakqualtools/out', filehandles=False):
             self.parse_phantompeakqualtools(f)
 
-        # Filter to strip out ignored sample names
-        self.phantompeakqualtools_data = self.ignore_samples(self.phantompeakqualtools_data)
+        # Parse csv for strand correlation plot
+        self.correlation_data = dict()
+        for f in self.find_log_files('phantompeakqualtools/csv'):
+            self.parse_correlation(f)
 
-        if len(self.phantompeakqualtools_data) == 0:
+        # Filter to strip out ignored sample names
+        self.phantompeakqualtools_data  =  self.ignore_samples(self.phantompeakqualtools_data)
+        self.correlation_data           =  self.ignore_samples(self.correlation_data)
+
+        # Warning when no files are found
+        if max(len(self.phantompeakqualtools_data), len(self.correlation_data)) == 0:
             raise UserWarning
 
+        # Log
         log.info("Found {} logs".format(len(self.phantompeakqualtools_data)))
-        self.write_data_file(self.phantompeakqualtools_data, 'multiqc_phantompeakqualtools')
+        log.info("Found {} csv file for strand correlation".format(len(self.correlation_data)))
 
+        # Write parsed data to a file
+        self.write_data_file(self.phantompeakqualtools_data, 'multiqc_phantompeakqualtools')
+        self.write_data_file(self.correlation_data, 'multiqc_correlationdata')
+
+        # Report section
         self.phantompeakqualtools_general_stats()
 
+        if len(self.correlation_data) > 0:
+            self.add_section (
+                name = 'Strand Shift Correlation Plot',
+                anchor = 'strand_shift_correlation',
+                plot = self.strand_shift_correlation_plot()
+            )
 
+    # Parse spp.out file from phantompeakqualtools
     def parse_phantompeakqualtools(self, f):
-        s_name = f['s_name']
+        s_name = self.clean_s_name(f['s_name'], f['root'])
         parsed_data = {}
         lines = f['f'].splitlines()
         for l in lines:
@@ -52,9 +74,10 @@ class MultiqcModule(BaseMultiqcModule):
         if len(parsed_data) > 0:
             if s_name in self.phantompeakqualtools_data:
                 log.debug("Duplicate sample name found! Overwriting: {}".format(s_name))
+            self.add_data_source(f, s_name)
             self.phantompeakqualtools_data[s_name] = parsed_data
 
-
+    # Report fragment length, NSC and RSC in general stat table
     def phantompeakqualtools_general_stats(self):
         """ Add columns to General Statistics table """
         headers = OrderedDict()
@@ -81,3 +104,45 @@ class MultiqcModule(BaseMultiqcModule):
             'scale': 'RdYlBu-rev'
         }
         self.general_stats_addcols(self.phantompeakqualtools_data, headers)
+
+    # Parse spp.csv file from phantompeakqualtools
+    def parse_correlation(self, f):
+        s_name = self.clean_s_name(f['s_name'], f['root'])
+        parsed_data = {}
+        reader = csv.DictReader(f['f'])
+        for row in reader:
+            parsed_data['strand−shift'] = int(row['x'])
+            parsed_data['cross-correlation'] = float(row['y'])
+            parsed_data['peak_category'] = int(row['x'])
+        if len(parsed_data) > 0:
+            if s_name in self.correlation_data:
+                log.debug("Duplicate sample name found! Overwriting: {}".format(s_name))
+            self.add_data_source(f, s_name)
+            self.correlation_data[s_name] = parsed_data
+
+    # Strand shift correlation plot
+    def strand_shift_correlation_plot(self):
+        """ Generate the strand shift correlation plot"""
+
+        data = dict()
+        for s_name in self.complexity_data:
+            try:
+                data[s_name] = {int(self.complexity_data[s_name][d]) : int(d) for d in self.complexity_data[s_name]}
+            except KeyError:
+                pass
+        if len(data) == 0:
+            log.debug('No valid data for miRNA complexity')
+            return None
+
+        config = {
+            'id': 'strand_shift_correlation_plot',
+            'title': 'phantompeakqualtools: Strand Shift Correlation Plot',
+            'ylab': 'Cross-correlation',
+            'xlab': 'Strand shift (bp)',
+            'ymin': 0,
+            'xmin': 1,
+            'xDecimals': False,
+            'tt_label': '<b>Strand shift (bp) {point.x}</b>: {point.y} Cross-correlation',
+        }
+
+        return linegraph.plot(data, config)
