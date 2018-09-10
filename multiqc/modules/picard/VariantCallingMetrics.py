96c68f89f007de72bb83d60deed38aeaa90c965e
cpavanrun <C.P.A.vanrun@prinsesmaximacentrum.nl>
2018-09-10 11:29:40 +0200

    Fine-tune Picard VariantCallingMetrics

--- multiqc/modules/picard/VariantCallingMetrics.py
+++ multiqc/modules/picard/VariantCallingMetrics.py
@@ -23,6 +23,8 @@ def parse_reports(parent_module):
     parent_module.picard_variantCalling_data = data
 
     if len(data) > 0:
+        derive_data(data)
+
         # Write parsed data to a file
         parent_module.write_data_file(data, 'multiqc_picard_varientCalling')
 
@@ -47,7 +49,8 @@ def parse_reports(parent_module):
             'description': "The Insertion / Deletion ratio of the passing bi-allelic SNP calls made at SNP-database sites.",
             'min': 0,
             'scale': 'Greens',
-            'shared_key': 'indel_ratio'
+            'shared_key': 'indel_ratio',
+            'hidden': True,
         }
 
         parent_module.general_stats_headers['NOVEL_INS_DEL_RATIO'] = {
@@ -55,7 +58,27 @@ def parse_reports(parent_module):
             'description': "The Insertion / Deletion ratio of the passing bi-allelic SNP calls made at non-SNP-database sites.",
             'min': 0,
             'scale': 'Greens',
-            'shared_key': 'indel_ratio'
+            'shared_key': 'indel_ratio',
+            'hidden': True,
+        }
+
+
+        parent_module.general_stats_headers['total_called_variants_known'] = {
+            'title': 'Called Variants (known)',
+            'description': "Total counts of variants in SNP-database sites.",
+            'shared_key': 'variant_count',
+            'min': 0,
+            'format': '{0:,.0f}',
+            'hidden': True,
+        }
+
+        parent_module.general_stats_headers['total_called_variants_novel'] = {
+            'title': 'Called Variants (novel)',
+            'description': "Total counts of variants in non-SNP-database sites.",
+            'shared_key': 'variant_count',
+            'min': 0,
+            'format': '{0:,.0f}',
+            'hidden': True,
         }
 
         for s_name in data:
@@ -63,16 +86,31 @@ def parse_reports(parent_module):
                 parent_module.general_stats_data[s_name] = dict()
             parent_module.general_stats_data[s_name].update(data[s_name])
 
-        # Variant Counts plot
+
+        # Variant Counts Bargraph
         parent_module.add_section(
-            name='Variant Counts',
+            name='Variant Types',
             anchor='picard-count-variants',
-            description='Variants that are being called',
-            helptext="Only passing variants are shown (i.e. non-filtered). SNPs are bi-allelic. Complex indels are both an insertation and a deletion.",
+            description='Variants that have been called, looking at variant types. Optionally filtered on label.',
+            helptext="""
+            Only passing variants are shown (i.e. non-filtered).\n
+            SNPs are bi-allelic.\n
+            Complex InDels are both an insertion and a deletion.
+            """,
             plot=count_variants_barplot(data)
         )
 
-        derived_data = derive_data(data)
+        # Variant Counts Table
+        parent_module.add_section(
+            name='Variant Labels',
+            anchor='picard-count-variants',
+            description='Variants that have been called, comparing with known variant sites.',
+            helptext="""
+            Only passing variants are shown (i.e. non-filtered).\n
+            Variants contain bi-allelic SNPs, multi-allelic SNPs, simple and complex inserts and deletions.
+            """,
+            plot=compare_variants_plot(data)
+        )
 
     return len(data)
 
@@ -117,30 +155,24 @@ def table_in(filehandle, pre_header_string):
 def derive_data(data):
     """ Based on the data derive additional data """
 
-    extra_data = dict()
     for s_name, values in data.items():
         # setup holding variable
-        extra_data[s_name] = OrderedDict()
 
         # Sum all variants that have been called
         total_called_variants = 0
         for value_name in ['TOTAL_SNPS', 'TOTAL_COMPLEX_INDELS', 'TOTAL_MULTIALLELIC_SNPS', 'TOTAL_INDELS']:
             total_called_variants = total_called_variants + int(values[value_name])
-        extra_data[s_name]['total_called_variants'] = total_called_variants
+        values['total_called_variants'] = total_called_variants
 
         # Sum all variants that have been called and are known
         total_called_variants_known = 0
         for value_name in ['NUM_IN_DB_SNP', 'NUM_IN_DB_SNP_COMPLEX_INDELS', 'NUM_IN_DB_SNP_MULTIALLELIC']:
             total_called_variants_known = total_called_variants_known + int(values[value_name])
         total_called_variants_known = total_called_variants_known + int(values['TOTAL_INDELS']) - int(values['NOVEL_INDELS'])
-        extra_data[s_name]['total_called_variants_known'] = total_called_variants_known
+        values['total_called_variants_known'] = total_called_variants_known
 
         # Extrapolate the total novel variants
-        extra_data[s_name]['total_called_variants_novel'] = total_called_variants - total_called_variants_known
-
-        # Calculate the fraction
-        extra_data[s_name]['fraction_called_variants_that_are_known'] = float(total_called_variants_known) / float(total_called_variants)
-    return extra_data
+        values['total_called_variants_novel'] = total_called_variants - total_called_variants_known
 
 
 def stripped(iterator):
@@ -184,16 +216,37 @@ def count_variants_barplot(data):
 
     plot_conf = {
         'id': 'picard_variantCallingMetrics_variant_plot',
-        'title': 'Picard: VariantCallingMetrics',
-        'ylab': '# Variants',
-        'cpswitch_counts_label': 'Number of Variants',
+        'title': 'Picard: Variants Called',
+        'ylab': 'Counts of Variants',
         'hide_zero_cats': False,
         'data_labels': [
-            {'name': 'Total', 'ylab': '# Variants'},
-            {'name': 'Known', 'ylab': '# Known Variants'},
-            {'name': 'Novel', 'ylab': '# Novel Variants'}
+            {'name': 'Total'},
+            {'name': 'Known'},
+            {'name': 'Novel'}
         ],
     }
     return bargraph.plot(data=[total_variants, known_variants, novel_variants],
                          cats=[keys, keys, keys],
                          pconfig=plot_conf)
+
+
+def compare_variants_plot(data):
+    """ Return HTML for the Compare variants plot"""
+    keys = OrderedDict()
+
+    keys['total_called_variants_known'] = {'name': 'Known Variants'}
+    keys['total_called_variants_novel'] = {'name': 'Novel Variants'}
+
+    pconfig = {
+        'id': 'picard_variantCallingMetrics_compare_table',
+        'title': 'Picard: Variants Called',
+        'ylab': 'Counts of Variants',
+     }
+
+    return bargraph.plot(data, cats=keys, pconfig=pconfig)
+
+
+def merge_two_dicts(x, y):
+    z = x.copy()   # start with x's keys and values
+    z.update(y)    # modifies z with y's keys and values & returns None
+    return z
