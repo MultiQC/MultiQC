fa011670b8aa2a70c9530c13ecd8ddff6e6ad6ed
Alexander Peltzer <alex.peltzer@gmail.com>
2018-12-18 23:54:29 +0100

    Add 3pGtoA parsing

--- multiqc/modules/damageprofiler/damageprofiler.py
+++ multiqc/modules/damageprofiler/damageprofiler.py
@@ -5,7 +5,7 @@
 from __future__ import print_function
 from collections import OrderedDict
 import logging
-import json
+import yaml
 
 from multiqc import config
 from multiqc.plots import bargraph, linegraph
@@ -28,30 +28,65 @@ class MultiqcModule(BaseMultiqcModule):
             self.parse3pG(f)
         
         # Find and load 5pCtoTFreq Files
-        self.5pCtoTfreq_data = dict() 
-        for f in self.find_log_files('damageprofiler/fiveprime'):
-            self.parse5pC(f)
+        #self.5pCtoTfreq_data = dict() 
+        #for f in self.find_log_files('damageprofiler/fiveprime'):
+        #    self.parse5pC(f)
         
         # Find and load lgdist forward Files
-        self.lgdist_fw_data = dict() 
-        for f in self.find_log_files('damageprofiler/lgdistfw'):
-            self.lgdistfw(f)
+        #self.lgdist_fw_data = dict() 
+        #for f in self.find_log_files('damageprofiler/lgdistfw'):
+        #    self.lgdistfw(f)
 
         # Find and load lgdist reverse Files
-        self.lgdist_rv_data = dict() 
-        for f in self.find_log_files('damageprofiler/lgdistrv'):
-            self.lgdistrv(f)
+        #self.lgdist_rv_data = dict() 
+        #for f in self.find_log_files('damageprofiler/lgdistrv'):
+        #    self.lgdistrv(f)
 
         # Filter to strip out ignored sample names
         self.3pGtoAfreq_data         =   self.ignore_samples(self.3pGtoAfreq_data)
-        self.5pCtoTfreq_data          =   self.ignore_samples(self.5pCtoTfreq_data)
-        self.lgdist_fw_data   =   self.ignore_samples(self.lgdist_fw_data)
-        self.lgdist_rv_data      =   self.ignore_samples(self.lgdist_rv_data)
+        #self.5pCtoTfreq_data          =   self.ignore_samples(self.5pCtoTfreq_data)
+        #self.lgdist_fw_data   =   self.ignore_samples(self.lgdist_fw_data)
+        #self.lgdist_rv_data      =   self.ignore_samples(self.lgdist_rv_data)
 
         # Warning when no files are found
         if max(len(self.3pGtoAfreq_data), len(self.5pCtoTfreq_data), len(self.lgdist_fw_data), len(self.lgdist_rv_data)) == 0:
             raise UserWarning
         
+        # Write parsed data to a file
+        self.write_data_file(self.3pGtoAfreq_data, 'multiqc_damageprofiler_3pGtoAfreq')
+        #self.write_data_file(self.5pCtoTfreq_data, 'multiqc_damageprofiler_5pCtoTfreq')
+        #self.write_data_file(self.lgdist_fw_data, 'multiqc_damageprofiler_lgdist_fw')
+        #self.write_data_file(self.lgdist_rv_data, 'multiqc_damageprofiler_lgdist_rv')
+    
+    #Parse a 3pGtoAfreq file
+    def parse3pG(self, f):
+
+        try:
+            # Parsing as OrderedDict is slightly messier with YAML
+            # http://stackoverflow.com/a/21048064/713980
+            # Copied over from custom_content.py - thanks @ewels!
+            def dict_constructor(loader, node):
+                return OrderedDict(loader.construct_pairs(node))
+            yaml.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, dict_constructor)
+            parsed_data = yaml.load(f['f'])
+        except Exception as e:
+            log.warning("Error parsing YAML file '{}' (probably invalid YAML)".format(f['fn']))
+            log.warning("YAML error: {}".format(e))
+            return None
         
+        #Sample name is always the only key in the YAML
+        if parsed_data is not None:
+            key = list(parsed_data.keys())[0] 
+            s_name = self.clean_s_name(list(parsed_data.keys())[0],f[root])
+
+            if s_name in self.summary_data:
+                log.debug("Duplicate sample name found! Overwriting: {}".format(s_name))
+        
+            self.3pGtoAfreq_data[s_name] = parsed_data.get(key)
+        else: 
+            log.debug('No valid data {} in 3pGtoA report'..format(f['fn']))
+            return None
+
+
         
         
