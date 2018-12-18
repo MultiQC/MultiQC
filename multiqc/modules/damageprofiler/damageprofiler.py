605767964365641847b3de459f18146dcc36aae3
Alexander Peltzer <alex.peltzer@gmail.com>
2018-12-19 00:15:48 +0100

    Some more debugging choices

--- multiqc/modules/damageprofiler/damageprofiler.py
+++ multiqc/modules/damageprofiler/damageprofiler.py
@@ -6,32 +6,38 @@ from __future__ import print_function
 from collections import OrderedDict
 import logging
 import yaml
+import pprint
 
 from multiqc import config
-from multiqc.plots import bargraph, linegraph
+from multiqc.plots import  linegraph
 from multiqc.modules.base_module import BaseMultiqcModule
 
-class MultiqcModule(BaseMultiqcModule):
-    """
-    damageprofiler module class
-    """
+# Initialise the logger
+log = logging.getLogger(__name__)
 
-    # Initialise the parent object
+class MultiqcModule(BaseMultiqcModule):
+
+    def __init__(self):
+        """
+        damageprofiler module class
+        """
+
+        # Initialise the parent object
         super(MultiqcModule, self).__init__(name='damageprofiler',
         anchor='damageprofiler',
         href='https://github.com/Integrative-Transcriptomics/DamageProfiler',
         info="A Java based tool to determine damage patterns on ancient DNA as a replacement for mapDamage.")
 
         # Find and load 3pGtoAFreq Files
-        self.3pGtoAfreq_data = dict() 
+        self.threepGtoAfreq_data = dict() 
         for f in self.find_log_files('damageprofiler/threeprime'):
             self.parse3pG(f)
-        
+
         # Find and load 5pCtoTFreq Files
         #self.5pCtoTfreq_data = dict() 
         #for f in self.find_log_files('damageprofiler/fiveprime'):
         #    self.parse5pC(f)
-        
+
         # Find and load lgdist forward Files
         #self.lgdist_fw_data = dict() 
         #for f in self.find_log_files('damageprofiler/lgdistfw'):
@@ -43,17 +49,17 @@ class MultiqcModule(BaseMultiqcModule):
         #    self.lgdistrv(f)
 
         # Filter to strip out ignored sample names
-        self.3pGtoAfreq_data         =   self.ignore_samples(self.3pGtoAfreq_data)
+        self.threepGtoAfreq_data         =   self.ignore_samples(self.threepGtoAfreq_data)
         #self.5pCtoTfreq_data          =   self.ignore_samples(self.5pCtoTfreq_data)
         #self.lgdist_fw_data   =   self.ignore_samples(self.lgdist_fw_data)
         #self.lgdist_rv_data      =   self.ignore_samples(self.lgdist_rv_data)
 
         # Warning when no files are found
-        if max(len(self.3pGtoAfreq_data), len(self.5pCtoTfreq_data), len(self.lgdist_fw_data), len(self.lgdist_rv_data)) == 0:
-            raise UserWarning
-        
+        #if max(len(self.threepGtoAfreq_data), len(self.5pCtoTfreq_data), len(self.lgdist_fw_data), len(self.lgdist_rv_data)) == 0:
+        #    raise UserWarning
+
         # Write parsed data to a file
-        self.write_data_file(self.3pGtoAfreq_data, 'multiqc_damageprofiler_3pGtoAfreq')
+        #self.write_data_file(self.threepGtoAfreq_data, 'multiqc_damageprofiler_3pGtoAfreq')
         #self.write_data_file(self.5pCtoTfreq_data, 'multiqc_damageprofiler_5pCtoTfreq')
         #self.write_data_file(self.lgdist_fw_data, 'multiqc_damageprofiler_lgdist_fw')
         #self.write_data_file(self.lgdist_rv_data, 'multiqc_damageprofiler_lgdist_rv')
@@ -77,14 +83,15 @@ class MultiqcModule(BaseMultiqcModule):
         #Sample name is always the only key in the YAML
         if parsed_data is not None:
             key = list(parsed_data.keys())[0] 
-            s_name = self.clean_s_name(list(parsed_data.keys())[0],f[root])
+            s_name = self.clean_s_name(list(parsed_data.keys())[0],'')
 
-            if s_name in self.summary_data:
+            if s_name in self.threepGtoAfreq_data:
                 log.debug("Duplicate sample name found! Overwriting: {}".format(s_name))
         
-            self.3pGtoAfreq_data[s_name] = parsed_data.get(key)
+            self.threepGtoAfreq_data[s_name] = parsed_data.get(key)
+            pprint.pprint(self.threepGtoAfreq_data)
         else: 
-            log.debug('No valid data {} in 3pGtoA report'..format(f['fn']))
+            log.debug('No valid data {} in 3pGtoA report'.format(f['fn']))
             return None
 
 
