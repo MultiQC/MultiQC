8144586eac9a5970b0d8975d5959be2ecb528a1f
Alexander Peltzer <alex.peltzer@gmail.com>
2019-10-16 17:18:36 +0200

    Continue later

--- multiqc/modules/sexdeterrmine/sexdeterrmine.py
+++ multiqc/modules/sexdeterrmine/sexdeterrmine.py
@@ -28,38 +28,44 @@ class MultiqcModule(BaseMultiqcModule):
         info="A python script to calculate the relative coverage of X and Y chromosomes, and their associated error bars, from the depth of coverage at specified SNPs. ")
 
         # Find and load any DeDup reports
-        self.sexdet_nraut = dict()
-        self.sexdet_nrX = dict()
-        self.sexdet_nrY = dict()
-        self.sexdet_rateErrX = dict()
-        self.sexdet_rateErrY = dict()
-        self.sexdet_rateX = dict()
-        self.sexdet_rateY = dict() 
-        self.sexdet_snpsauto = dict()
-        self.sexdet_Xsnps = dict()
-        self.sexdet_Ysnps = dict()
+        self.sexdet_data = dict()
+        
 
         # Find and load JSON file
         for f in self.find_log_files('sexdeterrmine',filehandles=True):
             self.parseJSON(f)
         
-        pp = pprint.PrettyPrinter(indent=4)
-        print(self.sexdet_nraut)
-        print(self.sexdet_nrX)
-        
-        #Filter to strip out ignored sample names
-        #self.sexdet_nraut = self.ignore_samples(self.sexdet_nraut)
-        #self.sexdet_nrX = self.ignore_samples(self.sexdet_nrX)
-        #self.sexdet_nrY = self.ignore_samples(self.sexdet_nrY)
-        #self.sexdet_rateErrX = self.ignore_samples(self.sexdet_rateErrX)
-        #self.sexdet_rateErrY = self.ignore_samples(self.sexdet_rateErrY)
-        #self.sexdet_rateX = self.ignore_samples(self.sexdet_rateX)
-        #self.sexdet_rateY = self.ignore_samples(self.sexdet_rateY)
-        #self.sexdet_snpsauto = self.ignore_samples(self.sexdet_snpsauto)
-        #self.sexdet_Xsnps = self.ignore_samples(self.sexdet_Xsnps)
-        #self.sexdet_Ysnps = self.ignore_samples(self.sexdet_Ysnps)
+        # Empty dictionary for sending to table
+        dict_to_plot = OrderedDict()
 
-        #self.write_data_file(self.sexdet_nraut, 'multiqc_sexdeter_nraut_metrics')
+        pp = pprint.PrettyPrinter(indent=4) 
+        pp.pprint(self.sexdet_data)
+        for k in self.sexdet_data:
+            if (k != 'Metadata'):
+                try:  
+                    s_name = self.clean_s_name(k,f['root'])
+                    self.add_data_source(f, s_name)
+                    dict_to_plot[s_name]['NR Aut'] = self.sexdet_data[k]['NR Aut']
+                    dict_to_plot[s_name]['NrX'] = self.sexdet_data[k]['NrX']
+                    dict_to_plot[s_name]['NrY'] = self.sexdet_data[k]['NrY']
+                    dict_to_plot[s_name]['RateErrX'] = self.sexdet_data[k]['RateErrX']
+                    dict_to_plot[s_name]['RateErrY'] = self.sexdet_data[k]['RateErrY']
+                    dict_to_plot[s_name]['RateX'] = self.sexdet_data[k]['RateX']
+                    dict_to_plot[s_name]['RateY'] = self.sexdet_data[k]['RateY']
+                    dict_to_plot[s_name]['Snps Autosomal'] = self.sexdet_data[k]['Snps Autosomal']
+                    dict_to_plot[s_name]['XSnps'] = self.sexdet_data[k]['XSnps']
+                    dict_to_plot[s_name]['YSnps'] = self.sexdet_data[k]['YSnps']
+                    self.addSummaryMetrics(dict_to_plot)
+                    dict_to_plot = OrderedDict()
+                except ValueError as error:
+                    print("Something isn't right with this error:", error)
+            else:
+                continue
+
+       
+        
+        
+        self.write_data_file(self.sexdet_data, 'multiqc_sexdeter_metrics')
 
 
         #Parse our nice little JSON file
@@ -68,30 +74,11 @@ class MultiqcModule(BaseMultiqcModule):
         """ Parse the JSON output from SexDeterrmine and save the summary statistics """
         try:
             parsed_json = json.load(f['f'])
+            self.sexdet_data = parsed_json
         except Exception as e:
             print(e)
             log.warn("Could not parse SexDeterrmine JSON: '{}'".format(f['fn']))
             return None
-        #Get sample name from JSON first
-        for k in parsed_json:
-            if (k != 'Metadata'):
-                try:  
-                    s_name = self.clean_s_name(k,f['root'])
-                    self.add_data_source(f, s_name)
-                    self.sexdet_nraut[s_name] = parsed_json[k]['NR Aut']
-                    self.sexdet_nrX[s_name] = parsed_json[k]['NrX']
-                    self.sexdet_nrY[s_name] = parsed_json[k]['NrY']
-                    self.sexdet_rateErrX[s_name] = float(parsed_json[k]['RateErrX'])
-                    self.sexdet_rateErrY[s_name] = float(parsed_json[k]['RateErrY'])
-                    self.sexdet_rateX[s_name] = float(parsed_json[k]['RateX'])
-                    self.sexdet_rateY[s_name] = float(parsed_json[k]['RateY'])
-                    self.sexdet_snpsauto[s_name] = parsed_json[k]['Snps Autosomal']
-                    self.sexdet_Xsnps[s_name] = parsed_json[k]['XSnps']
-                    self.sexdet_Ysnps[s_name] = parsed_json[k]['YSnps']
-                except ValueError as error:
-                    print("Something isn't right with this error:", error)
-            else:
-                continue
 
     def addSummaryMetrics(self, dict_to_plot):
         """ Take the parsed stats from SexDetErrmine and add it to the main plot """
