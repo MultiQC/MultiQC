73e1bbab9ccb8b897bd877ad839ef803814634a3
Phil Ewels <phil.ewels@scilifelab.se>
2019-11-15 15:35:42 +0100

    Deeptools: Use helper int() tool which is more tolerant of float strings
    Fixes Python3 ValueError in issue #1057 and #1011

--- multiqc/modules/deeptools/plotProfile.py
+++ multiqc/modules/deeptools/plotProfile.py
@@ -64,7 +64,7 @@ class plotProfileMixin():
             elif cols[0] == "bins":
                 for col in cols[2:len(cols)]:
                     if len(bins)!=len(bin_labels):
-                        bins.append(int(col))
+                        bins.append(self._int(col))
                     else:
                         break
             else:
@@ -77,9 +77,9 @@ class plotProfileMixin():
                     if k in bin_labels[0]:
                         convert_factor *= v
                         start = float(bin_labels[0].strip(k)) * convert_factor
-                step = int(abs(start/bin_labels.index('TSS')))
+                step = self._int(abs(start/bin_labels.index('TSS')))
                 end = step*(len(bin_labels)-bin_labels.index('TSS')-1)
-                converted_bin_labels = range((int(start)+step), (int(end)+step), step)
+                converted_bin_labels = range((self._int(start)+step), (self._int(end)+step), step)
 
                 for i in bins:
                     d[s_name].update({converted_bin_labels[i-1]:float(cols[i+1])})
