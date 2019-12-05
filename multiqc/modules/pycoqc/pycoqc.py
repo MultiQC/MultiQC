41e41232693bd437e497de2969a42d04c5457a39
Sara Sjunnebo <sara.sjunnebo@gmail.com>
2019-12-05 12:55:19 +0100

    find pycoqc report

--- multiqc/modules/pycoqc/pycoqc.py
+++ multiqc/modules/pycoqc/pycoqc.py
@@ -3,6 +3,11 @@
 """ MultiQC module to parse output from pycoQC """
 
 from multiqc.modules.base_module import BaseMultiqcModule
+import logging
+log = logging.getLogger(__name__)
+
+
+
 
 class MultiqcModule(BaseMultiqcModule):
     def __init__(self):
@@ -10,5 +15,5 @@ class MultiqcModule(BaseMultiqcModule):
         super(MultiqcModule, self).__init__(name='pycoQC', anchor='pycoqc',
         href="https://a-slide.github.io/pycoQC/",
         info="Computes metrics and generates interactive QC plots for Oxford Nanopore technologies sequencing data")
+        log.info('Hello World!')
 
-print('Hello, world!')
