fd09e999d3b94814c47138b70c723ead71a8ac8f
vladsaveliev <vladislav.sav@gmail.com>
2020-01-03 18:38:38 +1100

    Dragen: add coverage histogram and distribution

--- multiqc/modules/mosdepth/mosdepth.py
+++ multiqc/modules/mosdepth/mosdepth.py
@@ -95,7 +95,7 @@ class MultiqcModule(BaseMultiqcModule):
                     'id': 'mosdepth-coverage-dist-id',
                     'title': 'Mosdepth: Coverage Distribution',
                     'xlab': 'Coverage (X)',
-                    'ylab': '% bases in genome/regions covered by least X reads',
+                    'ylab': '% bases in genome/regions covered by at least X reads',
                     'ymax': 100,
                     'xmax': xmax,
                     'tt_label': '<b>{point.x}X</b>: {point.y:.2f}%',
