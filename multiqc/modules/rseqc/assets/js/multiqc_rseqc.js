13b9dcac3d93746bd018b4e0b83c022821c69e4e
sergpolly <sergpolly@gmail.com>
2020-04-01 16:43:41 -0400

    rseqc add hide/highlight compatibility

--- multiqc/modules/rseqc/assets/js/multiqc_rseqc.js
+++ multiqc/modules/rseqc/assets/js/multiqc_rseqc.js
@@ -26,12 +26,63 @@ function get_current_name(name) {
 }
 
 
+function is_name_hidden(name) {
+  // a helper function that checks if name is hidden:
+  // some local vars to make the code readable
+  const hiddenText = window.mqc_hide_f_texts;
+  const regexp_mode = window.mqc_hide_regex_mode;
+  const hide_show_mode = (window.mqc_hide_mode == "show");
+  var match = false;
+  // check hidden status for "name"
+  if( hiddenText.length > 0 ) {
+    $.each(hiddenText, function(idx, h_text) {
+      if(regexp_mode) { match = name.match(h_text); }
+      else { match = ( name.indexOf(h_text) > -1 ); }
+    });
+  }
+  if(hide_show_mode){ match = !match; }
+  // return natch, i.e. if "name" is hidden:
+  return match;
+}
+
+
+function is_highlight_enabled() {
+  // helper function to improve readability
+  // simply return if global highlighting enabled
+  const highlightedText = window.mqc_highlight_f_texts;
+  if(highlightedText.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+function get_highlight_color(name) {
+  // helper function to get highlight color
+  const highlightedText = window.mqc_highlight_f_texts;
+  const highlightColors = window.mqc_highlight_f_cols;
+  const regexp_mode = window.mqc_highlight_regex_mode;
+  var h_color = "";
+  // Highlight samples
+  if(highlightedText.length > 0){
+    $.each(highlightedText, function(idx, f_text){
+      if((regexp_mode && name.match(f_text)) || (!regexp_mode && name.indexOf(f_text) > -1)){
+        h_color = highlightColors[idx];
+      }
+    });
+  }
+  return h_color;
+}
+
 
 function single_sample_plot(e){
     // In case of repeated modules: #rseqc_junction_saturation_plot, #rseqc_junction_saturation_plot-1, ..
     var rseqc_junction_saturation_plot = $(e.currentTarget).closest('.hc-plot');
     var rseqc_junction_saturation_plot_id = rseqc_junction_saturation_plot.attr('id');
     var junction_sat_single_hint = rseqc_junction_saturation_plot.closest('.mqc-section').find('#junction_sat_single_hint');
+    var highlight_enabled = is_highlight_enabled();
+    var sample_color = "";
 
 
     // Get the three datasets for this sample
@@ -40,6 +91,7 @@ function single_sample_plot(e){
         {'name': 'Known Junctions'},
         {'name': 'Novel Junctions'}
     ];
+
     var k = 0;
     for (var i = 0; i < 3; i++) {
         var ds = mqc_plots[rseqc_junction_saturation_plot_id]['datasets'][i];
@@ -48,6 +100,7 @@ function single_sample_plot(e){
             // there is no protection against degenerate renaming:
             let current_sample_name = get_current_name(ds[k]['name']);
             if( current_sample_name == this.series.name ) {
+                if (highlight_enabled) { sample_color = get_highlight_color(current_sample_name); }
                 data[i]['data'] = JSON.parse(JSON.stringify(ds[k]['data']));
                 break;
             }
@@ -100,25 +153,34 @@ function single_sample_plot(e){
     // Listeners for previous / next plot
     newplot.find('.rseqc-junction_sat_single_prevnext').click(function(e){
       e.preventDefault();
-      if($(this).data('action') == 'prev'){
-        k--;
-        if(k < 0){
-          k = mqc_plots[rseqc_junction_saturation_plot_id]['datasets'][0].length - 1;
+      var current_sample_name;
+      var sample_is_hidden = true;
+      // this shouldn't result in infinite loop, as there has to be
+      // at least one sample non-hidden to activate single-sample view:
+      while (sample_is_hidden) {
+        if($(this).data('action') == 'prev'){
+          k--;
+          if(k < 0){
+            k = mqc_plots[rseqc_junction_saturation_plot_id]['datasets'][0].length - 1;
+          }
+        } else {
+          k++;
+          if(k >= mqc_plots[rseqc_junction_saturation_plot_id]['datasets'][0].length){
+            k = 0;
+          }
         }
-      } else {
-        k++;
-        if(k >= mqc_plots[rseqc_junction_saturation_plot_id]['datasets'][0].length){
-          k = 0;
-        }
+        // keep changing "k" until we find non-hidden one,
+        // take renaming into account when generating ptitle:
+        current_sample_name = get_current_name(mqc_plots[rseqc_junction_saturation_plot_id]['datasets'][0][k]['name']);
+        sample_is_hidden = is_name_hidden(current_sample_name);
       }
       var hc = newplot.find('.hc-plot').highcharts();
       for (var i = 0; i < 3; i++) {
           hc.series[i].setData(mqc_plots[rseqc_junction_saturation_plot_id]['datasets'][i][k]['data'], false);
       }
-      // take renaming into account when generating ptitle:
-      let current_sample_name = get_current_name(mqc_plots[rseqc_junction_saturation_plot_id]['datasets'][0][k]['name']);
       var ptitle = 'RSeQC Junction Saturation: ' + current_sample_name;
-      hc.setTitle({text: ptitle});
+      if (highlight_enabled) { sample_color = get_highlight_color(current_sample_name); }
+      hc.setTitle({text: ptitle, style: { color: sample_color }});
       hc.redraw({ duration: 200 });
     });
 
@@ -131,7 +193,8 @@ function single_sample_plot(e){
       colors: ['blue','red','green'],
       title: {
         text: 'RSeQC Junction Saturation: '+this.series.name,
-        x: 30 // fudge to center over plot area rather than whole plot
+        x: 30, // fudge to center over plot area rather than whole plot
+        style: { color: sample_color }
       },
       xAxis: {
         title: { text: 'Percent of total reads' },
