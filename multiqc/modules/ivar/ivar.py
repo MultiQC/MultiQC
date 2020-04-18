9e0d0b749fff3900345e288e67a6cf2304c91dc1
Alexander Peltzer <alex.peltzer@gmail.com>
2020-04-18 16:24:50 +0200

    Regex magic incoming

--- multiqc/modules/ivar/ivar.py
+++ multiqc/modules/ivar/ivar.py
@@ -6,10 +6,8 @@
 from __future__ import print_function
 from collections import OrderedDict
 import logging
+import re
 
-#TODO update this
-import json
-from multiqc.plots import bargraph, linegraph
 from multiqc.modules.base_module import BaseMultiqcModule
 
 # Initialise the logger
@@ -30,7 +28,7 @@ class MultiqcModule(BaseMultiqcModule):
         # Find and load iVar trim results
         self.ivar_data = dict()
         for f in self.find_log_files('ivar/trim', filehandles=True):
-            parsed_data = self.parse_ivar(f)
+            parsed_data = self.parse_ivar(f['f'])
             if parsed_data is not None and len(parsed_data) > 0:
                 self.ivar_data[f['s_name']] = parsed_data
                 self.add_data_source(f, f['s_name'])
@@ -53,33 +51,18 @@ class MultiqcModule(BaseMultiqcModule):
 
     # Parse a ivar report
     def parse_ivar(self, f):
-        total_reads = 0
-        reads_too_short_after_trimming = 0
-        reads_outside_primer_region = 0
         parsed_data = dict()
-
-        for l in f:
-            l = l.strip()
-            #Find total reads first, iVar 1 vs 2 are different here, thus more complex regex req.
-            if "Trimmed primers from" in l:
-                match = re.search(r'\(?\d+\)?[^\d+\.\d+\%]', l)
+        regexes = {
+            'total_reads': r'(?:Trimmed primers from )(?:\d+\.\d+\% \()?(\d+)',
+            'reads_outside_primer_region': r'^(?:\d+\.\d+\% \()?(\d+)(?:\))?(?:.*[of]?)reads\sstarted',
+            'reads_too_short_after_trimming': r'^(?:\d+\.\d+\% \()?(\d+)(?:\))?(?:.*[of]?)reads\swere(?: quality trimmed | shortened)'
+        }
+        for l in f['f']:
+            # Search regexes for stats
+            for k, r in regexes.items():
+                match = re.search(r, l)
                 if match:
-                    total_reads = str.replace(match.group(1), '(', '')
-                    total_reads = str.replace(total_reads, ')', '')
-                    parsed_data['total_reads'] = total_reads
-            if "below the minimum length of" in l:
-                match = re.search(r'\(?\d{3,}\)?', l)
-                if match:
-                    reads_too_short_after_trimming = str.replace(match.group(1), '(', '')
-                    reads_too_short_after_trimming = str.replace(reads_too_short_after_trimming, ')', '')
-                    parsed_data['reads_too_short_after_trimming'] = reads_too_short_after_trimming
-            if "reads started outside of primer regions" in l:
-                match = re.search(r'\(?\d{3,}\)?', l)
-                if match:
-                    reads_outside_primer_region = str.replace(match.group(1), '(', '')
-                    reads_outside_primer_region = str.replace(reads_outside_primer_region, ')', '')
-                    parsed_data['reads_outside_primer_region'] = reads_outside_primer_region
+                    parsed_data[k] = int(match.group(1))
         return parsed_data
-
 
 
