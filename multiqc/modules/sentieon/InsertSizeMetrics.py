e8887f0186b4297af7c8efbdac038aa859bd24e1
chrispyatt <34276603+chrispyatt@users.noreply.github.com>
2020-05-11 14:48:54 +0100

    final pep8?

--- multiqc/modules/sentieon/InsertSizeMetrics.py
+++ multiqc/modules/sentieon/InsertSizeMetrics.py
@@ -35,7 +35,8 @@ def parse_reports(self):
                     ins = int(sections[0])
                     tot_count = sum([int(x) for x in sections[1:]])
                     self.sentieon_insertSize_histogram[s_name][ins] = tot_count
-                    self.sentieon_insertSize_samplestats[s_name]['total_count'] += tot_count
+                    self.sentieon_insertSize_samplestats[s_name][
+                        'total_count'] += tot_count
                 except ValueError:
                     # Reset in case we have more in this log file
                     s_name = None
@@ -50,39 +51,54 @@ def parse_reports(self):
             if s_name is not None:
                 if 'InsertSizeMetricAlgo' in l and '#SentieonCommandLine' in l:
                     if s_name in self.sentieon_insertSize_data:
-                        log.debug("Duplicate sample name found in {}! Overwriting: {}".format(f['fn'], s_name))
-                    self.add_data_source(f, s_name, section='InsertSizeMetrics')
+                        log.debug("Duplicate sample name found in {}!\
+                             Overwriting: {}".format(f['fn'], s_name))
+                    self.add_data_source(
+                        f, s_name, section='InsertSizeMetrics')
                     keys = f['f'].readline().strip("\n").split("\t")
                     vals = f['f'].readline().strip("\n").split("\t")
-                    self.sentieon_insertSize_samplestats[s_name] = {'total_count': 0, 'meansum': 0, 'total_pairs': 0 }
+                    self.sentieon_insertSize_samplestats[s_name] = {
+                        'total_count': 0, 'meansum': 0, 'total_pairs': 0}
                     orientation_idx = keys.index('PAIR_ORIENTATION')
                     while len(vals) == len(keys):
                         pair_orientation = vals[orientation_idx]
                         rowkey = '{}_{}'.format(s_name, pair_orientation)
                         self.sentieon_insertSize_data[rowkey] = OrderedDict()
-                        self.sentieon_insertSize_data[rowkey]['SAMPLE_NAME'] = s_name
+                        self.sentieon_insertSize_data[rowkey]['SAMPLE_NAME']\
+                            = s_name
                         for i, k in enumerate(keys):
                             try:
-                                self.sentieon_insertSize_data[rowkey][k] = float(vals[i])
+                                self.sentieon_insertSize_data[rowkey][k]\
+                                     = float(vals[i])
                             except ValueError:
                                 try:
-                                    self.sentieon_insertSize_data[rowkey][k] = float(vals[i].replace(',','.'))
-                                    log.debug("Switching commas for points in '{}': {} - {}".format(f['fn'], vals[i], vals[i].replace(',', '.')))
+                                    self.sentieon_insertSize_data[rowkey][k] =\
+                                         float(vals[i].replace(',', '.'))
+                                    log.debug(
+                                        "Switching commas for points in '{}':\
+                                             {} - {}".format(
+                                                 f['fn'], vals[i], vals[i].
+                                                 replace(',', '.')))
                                 except ValueError:
-                                    self.sentieon_insertSize_data[rowkey][k] = vals[i]
+                                    self.sentieon_insertSize_data[rowkey][k]\
+                                         = vals[i]
                             except IndexError:
                                 pass  # missing data
                         # Add to mean sums
-                        rp = self.sentieon_insertSize_data[rowkey]['READ_PAIRS']
-                        mis = self.sentieon_insertSize_data[rowkey]['MEAN_INSERT_SIZE']
-                        self.sentieon_insertSize_samplestats[s_name]['meansum'] += (rp * mis)
-                        self.sentieon_insertSize_samplestats[s_name]['total_pairs'] += rp
+                        rp = self.sentieon_insertSize_data[rowkey][
+                            'READ_PAIRS']
+                        mis = self.sentieon_insertSize_data[rowkey][
+                            'MEAN_INSERT_SIZE']
+                        self.sentieon_insertSize_samplestats[s_name][
+                            'meansum'] += (rp * mis)
+                        self.sentieon_insertSize_samplestats[s_name][
+                            'total_pairs'] += rp
 
                         vals = f['f'].readline().strip("\n").split("\t")
 
                     # Skip lines on to histogram
-                    line = f['f'].readline().strip("\n")
-                    line = f['f'].readline().strip("\n")
+                    l = f['f'].readline().strip("\n")
+                    l = f['f'].readline().strip("\n")
 
                     self.sentieon_insertSize_histogram[s_name] = OrderedDict()
                     in_hist = True
@@ -93,28 +109,35 @@ def parse_reports(self):
         for s_name in list(self.sentieon_insertSize_histogram.keys()):
             if len(self.sentieon_insertSize_histogram[s_name]) == 0:
                 self.sentieon_insertSize_histogram.pop(s_name, None)
-                log.debug("Ignoring '{}' histogram as no data parsed".format(s_name))
+                log.debug("Ignoring '{}' histogram as no data parsed".format(
+                    s_name))
 
     # Calculate summed mean values for all read orientations
     for s_name, v in self.sentieon_insertSize_samplestats.items():
-        self.sentieon_insertSize_samplestats[s_name]['summed_mean'] = v['meansum'] / v['total_pairs']
+        self.sentieon_insertSize_samplestats[s_name]['summed_mean'] = v[
+            'meansum'] / v['total_pairs']
 
     # Calculate summed median values for all read orientations
     for s_name in self.sentieon_insertSize_histogram:
         j = 0
         for idx, c in self.sentieon_insertSize_histogram[s_name].items():
             j += c
-            if j > (self.sentieon_insertSize_samplestats[s_name]['total_count'] / 2):
-                self.sentieon_insertSize_samplestats[s_name]['summed_median'] = idx
+            if j > (
+                self.sentieon_insertSize_samplestats[s_name][
+                    'total_count'] / 2):
+                self.sentieon_insertSize_samplestats[s_name][
+                    'summed_median'] = idx
                 break
 
     # Filter to strip out ignored sample names
-    self.sentieon_insertSize_data = self.ignore_samples(self.sentieon_insertSize_data)
+    self.sentieon_insertSize_data\
+        = self.ignore_samples(self.sentieon_insertSize_data)
 
     if len(self.sentieon_insertSize_data) > 0:
 
         # Write parsed data to a file
-        self.write_data_file(self.sentieon_insertSize_data, 'multiqc_sentieon_insertSize')
+        self.write_data_file(
+            self.sentieon_insertSize_data, 'multiqc_sentieon_insertSize')
 
         # Do we have median insert sizes?
         missing_medians = False
@@ -143,7 +166,8 @@ def parse_reports(self):
         for s_name in self.sentieon_insertSize_samplestats:
             if s_name not in self.general_stats_data:
                 self.general_stats_data[s_name] = dict()
-            self.general_stats_data[s_name].update(self.sentieon_insertSize_samplestats[s_name])
+            self.general_stats_data[s_name].update(
+                self.sentieon_insertSize_samplestats[s_name])
 
         # Section with histogram plot
         if len(self.sentieon_insertSize_histogram) > 0:
@@ -157,8 +181,10 @@ def parse_reports(self):
 
             # Allow customisation of how smooth the the plot is
             try:
-                insertsize_smooth_points = int(config.sentieon_config['insertsize_smooth_points'])
-                log.debug("Custom Sentieon insert size smoothing: {}".format(insertsize_smooth_points))
+                insertsize_smooth_points = int(
+                    config.sentieon_config['insertsize_smooth_points'])
+                log.debug("Custom Sentieon insert size smoothing:\
+                     {}".format(insertsize_smooth_points))
             except (AttributeError, KeyError, ValueError):
                 insertsize_smooth_points = 500
 
@@ -181,8 +207,11 @@ def parse_reports(self):
             self.add_section(
                 name='Insert Size',
                 anchor='sentieon-insertsize',
-                description='Plot shows the number of reads at a given insert size. Reads with different orientations are summed.',
-                plot=linegraph.plot([self.sentieon_insertSize_histogram, data_percent], pconfig)
+                description='Plot shows the number of reads at a given insert\
+                     size. Reads with different orientations are summed.',
+                plot=linegraph.plot(
+                    [self.sentieon_insertSize_histogram, data_percent],
+                    pconfig)
             )
 
     # Return the number of detected samples to the parent module
