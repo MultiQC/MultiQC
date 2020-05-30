0cccf687cc589c2ee960bf808bafbf05607e5423
Phil Ewels <phil.ewels@scilifelab.se>
2020-05-30 10:25:33 +0200

    Kaiju: PR tweaks

--- multiqc/modules/kaiju/kaiju.py
+++ multiqc/modules/kaiju/kaiju.py
@@ -23,9 +23,11 @@ class MultiqcModule(BaseMultiqcModule):
             href="http://kaiju.binf.ku.dk/",
             info="a fast and sensitive taxonomic classification for metagenomics."
         )
+
         # Set up data structures
         self.kaiju_data = { }
         self.taxonomic_ranks = ["phylum","class","order","family","genus","species"]
+
         # Find and parse kaiju2table report
         for f in self.find_log_files('kaiju', filehandles=True):
             taxo_rank, parsed_data = self.parse_kaiju2table_report(f)
@@ -39,15 +41,19 @@ class MultiqcModule(BaseMultiqcModule):
                 if taxo_rank in self.kaiju_data.keys() :
                     self.kaiju_data[taxo_rank].update(parsed_data)
                 else :
-                    self.kaiju_data[taxo_rank]=parsed_data
+                    self.kaiju_data[taxo_rank] = parsed_data
 
         # Filters to strip out ignored sample names
         for taxo_rank in self.kaiju_data :
             self.kaiju_data[taxo_rank] = self.ignore_samples(self.kaiju_data[taxo_rank])
             self.write_data_file(self.kaiju_data[taxo_rank], 'multiqc_kaiju_'+taxo_rank)
+
         # no file found
         if len(self.kaiju_data) == 0:
             raise UserWarning
+
+        log.info("Found {} reports".format(len(self.kaiju_data)))
+
         self.kaiju_stats_table()
         self.kaiju_taxo_rank_plot()
 
@@ -68,11 +74,12 @@ class MultiqcModule(BaseMultiqcModule):
             elif taxon_names.startswith("cannot be assigned") :
                 parsed_data[s_name]["cannot_be_assigned"] = int(reads)
                 taxo_rank = taxon_names.split()[-1]
-            else :
+            else:
                 parsed_data[s_name]["assigned"] += int(reads)
 
         for s_name in parsed_data :
-            parsed_data[s_name]['percentage_assigned'] = (float(parsed_data[s_name]["assigned"]) / float(parsed_data[s_name]["assigned"] + parsed_data[s_name]["cannot_be_assigned"] + parsed_data[s_name]["unclassified"])) * 100.0
+            total_r = parsed_data[s_name]["assigned"] + parsed_data[s_name]["cannot_be_assigned"] + parsed_data[s_name]["unclassified"]
+            parsed_data[s_name]['percentage_assigned'] = (float(parsed_data[s_name]["assigned"]) / float(total_r)) * 100.0
 
         return taxo_rank, parsed_data
 
@@ -83,46 +90,45 @@ class MultiqcModule(BaseMultiqcModule):
         general_data ={}
         taxo_ranks = self.kaiju_data.keys()
 
-        #print only phylum rank in general table.
+        # print only phylum rank in general table.
         if len(taxo_ranks) >= 1 and "phylum" in taxo_ranks:
             general_data = self.kaiju_data["phylum"]
-            general_taxo_rank = "phylum"
+            general_taxo_rank = "Phylum"
         else:
             general_data = self.kaiju_data[self.kaiju_data.keys().sorted()[0]]
-            general_taxo_rank = self.kaiju_data.keys().sorted()[0]
+            general_taxo_rank = self.kaiju_data.keys().sorted()[0].capitalize()
 
         headers['percentage_assigned'] = {
-            'title': '% Reads assigned',
+            'title': '% Reads assigned {}'.format(general_taxo_rank),
             'description': 'Percentage of reads assigned at {} rank'.format(general_taxo_rank),
             'min': 0,
             'max': 100,
             'suffix': '%',
-            'scale': 'OrRd'
+            'scale': 'RdYlGn'
         }
         headers['assigned'] = {
             'title': '{} Reads assigned {} '.format(config.read_count_prefix, general_taxo_rank),
-            'description': 'Number of reads assigned ({})  at {} rank'.format(config.read_count_desc,general_taxo_rank),
+            'description': 'Number of reads assigned ({})  at {} rank'.format(config.read_count_desc, general_taxo_rank),
             'modify': lambda x: x * config.read_count_multiplier,
-            'scale': 'BuGn'
+            'scale': 'Blues'
         }
         self.general_stats_addcols(general_data, headers)
 
     def kaiju_taxo_rank_plot (self):
         """ Make the taxonomic rank barplot """
 
-        description = '<p>This plot shows the proportion of reads assigned, unclassified or not classified at different taxonomic rank.</p>'
         pconfig = {
             'id': 'kaiju_taxo_rank_plot',
-            'title': 'Kaiju: reads classification',
+            'title': 'Kaiju: Reads assigned',
             'ylab': 'Number of reads',
             'cpswitch_counts_label': 'Number of reads',
             'data_labels':[]
         }
-        datasets=[]
+        datasets = []
         cats = []
         for rank in self.taxonomic_ranks :
             if rank in self.kaiju_data :
-                pconfig['data_labels'].append( {'name': rank, 'ylab': 'Number of Reads at {} rank'.format(rank) })
+                pconfig['data_labels'].append( {'name': rank.capitalize(), 'ylab': 'Number of Reads at {} rank'.format(rank.capitalize()) })
                 cats.append(OrderedDict ())
                 cats[-1]['assigned'] = {'name': 'Assigned', 'color': '#1f78b4'}
                 cats[-1]['cannot_be_assigned'] = {'name': 'Cannot be assigned at this rank', 'color': '#a6cee3'}
@@ -130,8 +136,8 @@ class MultiqcModule(BaseMultiqcModule):
                 datasets.append(self.kaiju_data[rank])
 
         self.add_section (
-            name = 'Reads classification at different taxonomic level',
+            name = 'Read classification at different taxonomic levels',
             anchor = 'kaiju-classification',
-            description = description,
+            description = 'This plot shows the proportion of reads assigned, unclassified or not classified at different taxonomic rank.',
             plot = bargraph.plot(datasets, cats, pconfig)
         )
