29deafbd10ace57e7e3d8a5a47cb2503713f3897
Caspar Gross <mail@caspar.one>
2020-07-16 22:00:52 +0200

    Fix Plot labels, uniform plot colors for different datasets, fix lint errors

--- multiqc/modules/gffcompare/gffcompare.py
+++ multiqc/modules/gffcompare/gffcompare.py
@@ -25,9 +25,9 @@ class MultiqcModule(BaseMultiqcModule):
         # Parse stats file
         # Everything is hardcoded with linenumbers, needs to be adjusted if output should change. 
         # Other versions of GffCompare with different ouptut formats might not work.
+        
         self.gffcompare_data = {}
         for f in self.find_log_files('gffcompare'):
-            #print(f)
             sample = f['s_name']
             self.gffcompare_data[sample]= {}
             lines = f['f'].splitlines()
@@ -48,8 +48,7 @@ class MultiqcModule(BaseMultiqcModule):
             self.gffcompare_data[sample]['counts']['reference'] = {}
             self.gffcompare_data[sample]['counts']['reference']['mRNA'] = c[0]
             self.gffcompare_data[sample]['counts']['reference']['loci'] = c[1]
-            self.gffcompare_data[sample]['counts']['reference']['multi_exon_transcripts'] = c[2]
-            
+            self.gffcompare_data[sample]['counts']['reference']['multi_exon_transcripts'] = c[2]    
             c = [int(s) for s in lines[8].replace("(", " ").split() if s.isdigit()]
             self.gffcompare_data[sample]['counts']['super_loci'] = c[0]
 
@@ -68,7 +67,7 @@ class MultiqcModule(BaseMultiqcModule):
             self.gffcompare_data[sample]['counts']['matching_transcripts'] = [int(s) for s in lines[18].replace("(", " ").split() if s.isdigit()][0]
             self.gffcompare_data[sample]['counts']['matching_loci'] = [int(s) for s in lines[19].replace("(", " ").split() if s.isdigit()][0]
 
-            ## Additional count data
+            ## Missed/Novel genomic elements
             self.gffcompare_data[sample]['missed']['Exons'] = [int(s) for s in lines[21].replace("/", " ").split() if s.isdigit()]
             self.gffcompare_data[sample]['novel']['Exons'] = [int(s) for s in lines[22].replace("/", " ").split() if s.isdigit()]
             self.gffcompare_data[sample]['missed']['Introns'] = [int(s) for s in lines[23].replace("/", " ").split() if s.isdigit()]
@@ -76,7 +75,7 @@ class MultiqcModule(BaseMultiqcModule):
             self.gffcompare_data[sample]['missed']['Loci'] = [int(s) for s in lines[25].replace("/", " ").split() if s.isdigit()]
             self.gffcompare_data[sample]['novel']['Loci'] = [int(s) for s in lines[26].replace("/", " ").split() if s.isdigit()]
 
-        print(self.gffcompare_data)
+        #print(self.gffcompare_data)
 
         # Filter to strip out ignored sample names
         self.gffcompare_data = self.ignore_samples(self.gffcompare_data)
@@ -87,25 +86,29 @@ class MultiqcModule(BaseMultiqcModule):
 
         log.info("Found {} reports".format(len(self.gffcompare_data)))
 
-        # Add nothing to general statistics table  general statistics table
+        # Add nothing to general statistics table
        
         # Write data file
         self.write_data_file(self.gffcompare_data, 'multiqc_gffcompare')
 
         # Report sectionsq
         self.add_section (
-            name = "Gffcompare comparison accuracy",
+            name = "GffCompare: Accuracy values",
             description = (
                 """
-                Accuracy values for comparison of 
+                Displayed are the accuracy values precisiond and sensitivity for different levels of genomic features. 
+                The metrics are calculated for the comparison of a query and reference GTF file.
                 """
             ),
             helptext = (
                 """
-                There are three possible cases:
-                * **Primers found**: Full length cDNA reads with correct primers at both ends.
-                * **Rescued reads**: Split fusion reads.
-                * **Unusable**: Reads without correct primer combinations.
+                Sensitivity (also known as recall) is the True Positive Rate where True Positives are query features which agree with corresponding reference annotation features.
+
+                Precision (also known specificity) is the positive predictive value 
+
+                The accuracy metrics are calculated. Please not3
+
+                Full description of how the levels are compared can be found [here](https://ccb.jhu.edu/software/stringtie/gffcompare.shtml#levels)
                 """
             ),
             anchor = 'gffcompare_accuracy',
@@ -113,7 +116,7 @@ class MultiqcModule(BaseMultiqcModule):
         )
 
         self.add_section (
-            name = "Gffcompare novel loci",
+            name = "Gffcompare: Novel features",
             description = (
                 """
                 Comparison of samples with new loci
@@ -132,7 +135,7 @@ class MultiqcModule(BaseMultiqcModule):
         )
 
         self.add_section (
-            name = "Gffcompare missed loci",
+            name = "Gffcompare: Missed features",
             description = (
                 """
                 Accuracy values for comparison of 
@@ -166,7 +169,11 @@ class MultiqcModule(BaseMultiqcModule):
             'ymax': 1,
             'xmin': 0,
             'xmax': 1,
-            'data_labels' : [{'name' : x} for x in datasets]
+            'data_labels' : [{
+                'name' : x,
+                'ylab': 'Precision',
+                'xlab': 'Sensitivity'
+                } for x in datasets]
         }
 
         data_classification = [{
@@ -180,16 +187,28 @@ class MultiqcModule(BaseMultiqcModule):
         for dataset in datasets
         ]
                         
-        print(data_classification)
         return scatter.plot(data_classification, pconfig)
 
     # Plot number of novel and missing transcripts
     def plot_novel(self):
         """ Generate GffCompare novel elements plot"""
 
-        cats = ['novel', 'found']
+        cats = [{
+            'novel':{
+                'name': 'Novel features',
+                'color': '#f7a35c'
+            }, 
+            'matching': {
+                'name': 'Matching features',
+                'color': '#8bbc21'
+            }
+        }] *3
+
         datasets = ['Exons', 'Introns', 'Loci']
         pconfig = {
+            'id': 'gffcompare_novel_plot',
+            'title': 'Gffcompare: Novel features',
+            'ylab': 'Reads',
             'ymin': 0,    
             'data_labels': datasets
         }
@@ -197,7 +216,7 @@ class MultiqcModule(BaseMultiqcModule):
         data_novel = [{
                 sample:{
                     'novel' : self.gffcompare_data[sample]['novel'][dataset][0],
-                    'found' : (self.gffcompare_data[sample]['novel'][dataset][1] -
+                    'matching' : (self.gffcompare_data[sample]['novel'][dataset][1] -
                         self.gffcompare_data[sample]['novel'][dataset][0])
                 }
             for sample in self.gffcompare_data.keys()
@@ -205,16 +224,28 @@ class MultiqcModule(BaseMultiqcModule):
         for dataset in datasets
         ]
         
-        print(data_novel)
         return bargraph.plot(data_novel, cats, pconfig)
         
     
     def plot_missed(self):
         """ Generate GffCompare missed elements plot"""
 
-        cats = ['missed', 'found']
+        cats = [{
+            'missed':{
+                'name': 'Missed features',
+                'color': '#f7a35c'
+            }, 
+            'matching': {
+                'name': 'Matching features',
+                'color': '#8bbc21'
+            }
+        }] *3
+
         datasets = ['Exons', 'Introns', 'Loci']
         pconfig = {
+            'id': 'gffcompare_missing_plot',
+            'title': 'Gffcompare: Missing features',
+            'ylab': 'Reads',
             'ymin': 0,    
             'data_labels': datasets
         }
@@ -222,7 +253,7 @@ class MultiqcModule(BaseMultiqcModule):
         data_missed = [{
                 sample:{
                     'missed' : self.gffcompare_data[sample]['missed'][dataset][0],
-                    'found' : (self.gffcompare_data[sample]['missed'][dataset][1] -
+                    'matching' : (self.gffcompare_data[sample]['missed'][dataset][1] -
                         self.gffcompare_data[sample]['missed'][dataset][0])
                 }
             for sample in self.gffcompare_data.keys()
@@ -230,5 +261,4 @@ class MultiqcModule(BaseMultiqcModule):
         for dataset in datasets
         ]
         
-        print(data_missed)
         return bargraph.plot(data_missed, cats, pconfig)
