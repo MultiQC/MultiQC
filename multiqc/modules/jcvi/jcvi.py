6dfe68fab45212dfdd6000c7e7dd5e32e9efa6ea
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2020-08-18 14:41:42 +0200

    Add JCVI Genome Annotation module (https://github.com/tanghaibao/jcvi)

---
+++ multiqc/modules/jcvi/jcvi.py
@@ -0,0 +1,432 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from collections import OrderedDict
+import logging
+import os
+import re
+
+from multiqc.modules.base_module import BaseMultiqcModule
+from multiqc.plots import table, bargraph, linegraph
+
+# Initialise the logger
+log = logging.getLogger(__name__)
+
+
+class MultiqcModule(BaseMultiqcModule):
+    def __init__(self):
+        # Initialise the parent object
+        super(MultiqcModule, self).__init__(name='JCVI Genome Annotation', anchor='jcvi',
+                                            href="https://pypi.org/project/jcvi/",
+                                            info="computes statistics on genome annotation.")
+
+        # Parse logs
+        self.jcvi = dict()
+        for f in self.find_log_files('jcvi', filehandles=True):
+            self.parse_jcvi(f)
+
+        # Filter to strip out ignored sample names
+        self.jcvi = self.ignore_samples(self.jcvi)
+
+        if len(self.jcvi) == 0:
+            raise UserWarning
+
+        log.info("Found {} logs".format(len(self.jcvi)))
+        self.write_data_file(self.jcvi, 'multiqc_jcvi')
+
+        # Add most important JCVI annotation stats to the general table
+        headers = OrderedDict()
+        headers['genes'] = {
+            'title': 'Number of genes',
+            'description': 'Number of genes',
+            'format': '{:i}',
+        }
+        headers['transcripts'] = {
+            'title': 'Number of transcripts',
+            'description': 'Number of transcripts',
+            'format': '{:i}',
+        }
+        headers['mean_gene_size'] = {
+            'title': 'Mean gene length',
+            'description': 'Mean gene length',
+        }
+        self.general_stats_addcols(self.jcvi, headers)
+
+        self.add_section(plot=self.jcvi_table(),
+                         name="Summary")
+
+        self.add_section(plot=self.jcvi_barplot_genes(),
+                         name="Number of genes")
+
+        self.add_section(plot=self.jcvi_barplot_transcripts(),
+                         name="Number of transcripts")
+
+        self.add_section(plot=self.jcvi_barplot_exons(),
+                         name="Number of exons")
+
+        self.add_section(plot=self.jcvi_barplot_genes_len(),
+                         name="Mean size of genes")
+
+        self.add_section(plot=self.jcvi_barplot_transcripts_len(),
+                         name="Mean size of transcripts")
+
+        self.add_section(plot=self.jcvi_barplot_exons_len(),
+                         name="Mean size of exons")
+
+        self.add_section(
+            name='Gene length distribution',
+            anchor='jcvi_gene_length',
+            description='This plot shows the distribution of gene length. Values are binned in buckets of 100bp (e.g. the value at 150bp represents the number of genes having a length between 100 and 199bp).',
+            plot=self.jcvi_linegraph_gene_length()
+        )
+
+        self.add_section(
+            name='Exon length distribution',
+            anchor='jcvi_exon_length',
+            description='This plot shows the distribution of exon length. Values are binned in buckets of 25bp (e.g. the value at 112bp represents the number of exons having a length between 100 and 124bp).',
+            plot=self.jcvi_linegraph_exon_length()
+        )
+
+        self.add_section(
+            name='Intron length distribution',
+            anchor='jcvi_intron_length',
+            description='This plot shows the distribution of intron length. Values are binned in buckets of 25bp (e.g. the value at 112bp represents the number of introns having a length between 100 and 124bp).',
+            plot=self.jcvi_linegraph_intron_length()
+        )
+
+        self.add_section(
+            name='Exon count distribution',
+            anchor='jcvi_exon_count',
+            description='This plot shows the distribution of exon number per genes.',
+            helptext='''
+            If you look at y-axis value corresponding to the x-axis value "3", you get the number of genes having 3 exons.
+            ''',
+            plot=self.jcvi_linegraph_exon_count()
+        )
+
+    def jcvi_linegraph_exon_count(self):
+        plot_config = {
+            'id': 'jcvi_exon_count_plot',
+            'title': 'JCVI: Exon count repartition',
+            'ylab': '# genes',
+            'xlab': 'Exon count',
+            'xDecimals': False,
+            'ymin': 0,
+        }
+
+        plot_data = {x: self.jcvi[x]['exon_count'] for x in self.jcvi}
+
+        return linegraph.plot(plot_data, plot_config)
+
+    def jcvi_linegraph_exon_length(self):
+        plot_config = {
+            'id': 'jcvi_exon_length_plot',
+            'title': 'JCVI: Exon length repartition',
+            'ylab': '# exons',
+            'xlab': 'Exon length (bp)',
+            'xDecimals': False,
+            'ymin': 0,
+        }
+
+        plot_data = {x: self.jcvi[x]['exon_length'] for x in self.jcvi}
+
+        return linegraph.plot(plot_data, plot_config)
+
+    def jcvi_linegraph_gene_length(self):
+        plot_config = {
+            'id': 'jcvi_gene_length_plot',
+            'title': 'JCVI: gene length repartition',
+            'ylab': '# genes',
+            'xlab': 'Gene length (bp)',
+            'xDecimals': False,
+            'ymin': 0
+        }
+
+        plot_data = {x: self.jcvi[x]['gene_length'] for x in self.jcvi}
+
+        return linegraph.plot(plot_data, plot_config)
+
+    def jcvi_linegraph_intron_length(self):
+        plot_config = {
+            'id': 'jcvi_intron_length_plot',
+            'title': 'JCVI: intron length repartition',
+            'ylab': '# introns',
+            'xlab': 'Intron length (bp)',
+            'xDecimals': False,
+            'ymin': 0,
+        }
+
+        plot_data = {x: self.jcvi[x]['intron_length'] for x in self.jcvi}
+
+        return linegraph.plot(plot_data, plot_config)
+
+    def parse_jcvi(self, f):
+
+        s_name = None
+
+        # Look at the first three lines, they are always the same
+        first_line = f['f'].readline()
+        second_line = f['f'].readline()
+        third_line = f['f'].readline()
+        # If any of these fail, it's probably not a jcvi summary file
+        if not all((first_line.startswith("==================================================================="),
+                   second_line.startswith("                                                   o            all"),
+                   third_line.startswith("-------------------------------------------------------------------"))):
+            return
+
+        s_name = f['s_name']
+
+        if s_name in self.jcvi:
+            log.debug("Duplicate sample name found! Overwriting: {}".format(s_name))
+
+        self.jcvi[s_name] = dict()
+
+        match = re.search(r'Max number of transcripts per gene\s+([\d,]+)', f['f'].readline())
+        if match:
+            self.jcvi[s_name]['transcripts_per_gene'] = int(match.group(1).replace(',', ''))
+
+        match = re.search(r'Mean exon size\s+([\d,.]+)', f['f'].readline())
+        if match:
+            self.jcvi[s_name]['mean_exons_size'] = float(match.group(1).replace(',', ''))
+
+        match = re.search(r'Mean gene locus size \(first to last exon\)\s+([\d,.]+)', f['f'].readline())
+        if match:
+            self.jcvi[s_name]['mean_gene_size'] = float(match.group(1).replace(',', ''))
+
+        match = re.search(r'Mean number of distinct exons per gene\s+([\d,.]+)', f['f'].readline())
+        if match:
+            self.jcvi[s_name]['mean_exon_number'] = float(match.group(1).replace(',', ''))
+
+        match = re.search(r'Mean number of transcripts per gene\s+([\d,.]+)', f['f'].readline())
+        if match:
+            self.jcvi[s_name]['mean_transcript_number'] = float(match.group(1).replace(',', ''))
+
+        match = re.search(r'Mean transcript size \(UTR, CDS\)\s+([\d,.]+)', f['f'].readline())
+        if match:
+            self.jcvi[s_name]['mean_transcript_size'] = float(match.group(1).replace(',', ''))
+
+        match = re.search(r'Number of distinct exons\s+([\d,]+)', f['f'].readline())
+        if match:
+            self.jcvi[s_name]['exons'] = int(match.group(1).replace(',', ''))
+
+        match = re.search(r'Number of genes\s+([\d,]+)', f['f'].readline())
+        if match:
+            self.jcvi[s_name]['genes'] = int(match.group(1).replace(',', ''))
+
+        match = re.search(r'Number of genes with alternative transcript variants\s+(\d+)\s\(([\d.]+)%\)', f['f'].readline())
+        if match:
+            self.jcvi[s_name]['genes_with_alt'] = int(match.group(1).replace(',', ''))
+            self.jcvi[s_name]['genes_with_alt_percent'] = float(match.group(2).replace(',', ''))
+
+        match = re.search(r'Number of multi-exon genes\s+([\d,]+)\s\(([\d.]+)%\)', f['f'].readline())
+        if match:
+            self.jcvi[s_name]['multiexon_genes'] = int(match.group(1).replace(',', ''))
+            self.jcvi[s_name]['multiexon_genes_percent'] = float(match.group(2).replace(',', ''))
+
+        match = re.search(r'Number of predicted transcripts\s+([\d,]+)', f['f'].readline())
+        if match:
+            self.jcvi[s_name]['transcripts'] = int(match.group(1).replace(',', ''))
+
+        match = re.search(r'Number of single-exon genes\s+([\d,]+)\s\(([\d.]+)%\)', f['f'].readline())
+        if match:
+            self.jcvi[s_name]['singleexon_genes'] = int(match.group(1).replace(',', ''))
+            self.jcvi[s_name]['singleexon_genes_percent'] = float(match.group(2).replace(',', ''))
+
+        self.jcvi[s_name]['exon_count'] = self.parse_hists(os.path.join(f['root'], 'Exon_Count', s_name + '.txt'))
+
+        self.jcvi[s_name]['exon_length'] = self.parse_hists(os.path.join(f['root'], 'Exon_Length', s_name + '.txt'), bin_by=25)
+
+        self.jcvi[s_name]['gene_length'] = self.parse_hists(os.path.join(f['root'], 'Gene_Length', s_name + '.txt'), bin_by=100)
+
+        self.jcvi[s_name]['intron_length'] = self.parse_hists(os.path.join(f['root'], 'Intron_Length', s_name + '.txt'), bin_by=25)
+
+        self.add_data_source(f, s_name)
+
+    def parse_hists(self, stat_file, bin_by=1):
+
+        stat_table = {}
+
+        if os.path.isfile(stat_file):
+            with open(stat_file, 'r') as stat_handle:
+                for stat_line in stat_handle:
+                    val = ((int(stat_line) / bin_by) * bin_by) + bin_by / 2
+                    if val not in stat_table:
+                        stat_table[val] = 1
+                    else:
+                        stat_table[val] += 1
+
+        # Add missing values
+        if stat_table:
+            values = sorted(stat_table.keys())
+            for required_val in range(bin_by / 2, max(values), bin_by):
+                if required_val not in stat_table:
+                    stat_table[required_val] = 0
+
+        return stat_table
+
+    def jcvi_table(self):
+        """ Make basic table of the annotation stats """
+
+        # Specify the order of the different possible categories
+        headers = OrderedDict()
+        headers['genes'] = {
+            'title': '# genes',
+            'description': 'Number of genes',
+            'format': '{:i}',
+        }
+        headers['transcripts'] = {
+            'title': '# transcripts',
+            'description': 'Number of transcripts',
+            'format': '{:i}',
+        }
+        headers['exons'] = {
+            'title': '# exons',
+            'description': 'Number of distinct exons',
+            'format': '{:i}',
+        }
+        headers['mean_gene_size'] = {
+            'title': 'Mean gene len',
+            'description': 'Mean gene length',
+            'format': '{:i}',
+        }
+        headers['mean_transcript_size'] = {
+            'title': 'Mean transcript len',
+            'description': 'Mean transcript length',
+            'format': '{:i}',
+        }
+        headers['mean_exons_size'] = {
+            'title': 'Mean exon len',
+            'description': 'Mean exon length',
+            'format': '{:i}',
+        }
+        headers['mean_transcript_number'] = {
+            'title': 'Mean transcripts/gene',
+            'description': 'Mean number of transcripts per gene',
+            'format': '{:i}',
+        }
+        headers['transcripts_per_gene'] = {
+            'title': 'Max transcripts/gene',
+            'description': 'Max number of transcripts per gene',
+            'format': '{:i}',
+        }
+        headers['genes_with_alt'] = {
+            'title': '# genes with alt',
+            'description': 'Number of genes with alternative transcript variants',
+            'format': '{:i}',
+        }
+        headers['genes_with_alt_percent'] = {
+            'title': '% genes with alt',
+            'description': 'Percentage of genes with alternative transcript variants',
+            'format': '{:i}',
+        }
+        headers['mean_exon_number'] = {
+            'title': 'Mean exons/gene',
+            'description': 'Mean number of distinct exons per gene',
+            'format': '{:i}',
+        }
+        headers['singleexon_genes'] = {
+            'title': '# single-exon',
+            'description': 'Number of single-exon genes',
+            'format': '{:i}',
+        }
+        headers['singleexon_genes_percent'] = {
+            'title': '% single-exon',
+            'description': 'Percentage of single-exon genes',
+            'format': '{:i}',
+        }
+        headers['multiexon_genes'] = {
+            'title': '# multi-exon',
+            'description': 'Number of multi-exon genes',
+            'format': '{:i}',
+        }
+        headers['multiexon_genes_percent'] = {
+            'title': '% multi-exon',
+            'description': 'Percentage of multi-exon genes',
+            'format': '{:i}',
+        }
+
+        table_config = {
+            'namespace': 'jcvi',
+            'min': 0,
+        }
+
+        return table.plot(self.jcvi, headers, table_config)
+
+    def jcvi_barplot_genes(self):
+        keys = OrderedDict()
+        keys['multiexon_genes'] = {'name': 'Multi-exon'}
+        keys['singleexon_genes'] = {'name': 'Single-exon'}
+
+        plot_config = {
+            'id': 'jcvi_plot_singlemultiexons',
+            'title': 'JCVI: Number of genes',
+            'ylab': '# Counts',
+            'cpswitch_counts_label': 'Number of genes'
+        }
+
+        return bargraph.plot(self.jcvi, keys, plot_config)
+
+    def jcvi_barplot_transcripts(self):
+        keys = OrderedDict()
+        keys['transcripts'] = {'name': 'transcripts'}
+
+        plot_config = {
+            'id': 'jcvi_plot_transcripts',
+            'title': 'JCVI: Number of transcripts',
+            'ylab': '# Counts',
+            'cpswitch_counts_label': 'Number of transcripts'
+        }
+
+        return bargraph.plot(self.jcvi, keys, plot_config)
+
+    def jcvi_barplot_exons(self):
+        keys = OrderedDict()
+        keys['exons'] = {'name': 'exons'}
+
+        plot_config = {
+            'id': 'jcvi_plot_exons',
+            'title': 'JCVI: Number of exons',
+            'ylab': '# Counts',
+            'cpswitch_counts_label': 'Number of exons'
+        }
+
+        return bargraph.plot(self.jcvi, keys, plot_config)
+
+    def jcvi_barplot_genes_len(self):
+        keys = OrderedDict()
+        keys['mean_gene_size'] = {'name': 'mean_gene_size'}
+
+        plot_config = {
+            'id': 'jcvi_plot_genes_len',
+            'title': 'JCVI: Mean size of genes',
+            'ylab': '# Counts',
+            'cpswitch_counts_label': 'Mean size of genes'
+        }
+
+        return bargraph.plot(self.jcvi, keys, plot_config)
+
+    def jcvi_barplot_transcripts_len(self):
+        keys = OrderedDict()
+        keys['mean_transcript_size'] = {'name': 'mean_transcript_size'}
+
+        plot_config = {
+            'id': 'jcvi_plot_transcripts_len',
+            'title': 'JCVI: Mean size of transcripts',
+            'ylab': '# Counts',
+            'cpswitch_counts_label': 'Mean size of transcripts'
+        }
+
+        return bargraph.plot(self.jcvi, keys, plot_config)
+
+    def jcvi_barplot_exons_len(self):
+        keys = OrderedDict()
+        keys['mean_exons_size'] = {'name': 'mean_exons_size'}
+
+        plot_config = {
+            'id': 'jcvi_plot_exons_len',
+            'title': 'JCVI: Mean size of exons',
+            'ylab': '# Counts',
+            'cpswitch_counts_label': 'Mean size of exons'
+        }
+
+        return bargraph.plot(self.jcvi, keys, plot_config)
