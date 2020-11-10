25a0efeff26b4c0e3edb8798a772129f5b823973
Imipenem <philipp_ehm@protonmail.com>
2020-11-10 17:13:41 +0100

    General odgi stats table and section

--- multiqc/modules/odgi/stats.py
+++ multiqc/modules/odgi/stats.py
@@ -6,8 +6,6 @@ from __future__ import print_function
 import logging
 from collections import OrderedDict
 
-from multiqc import config
-from multiqc.plots import bargraph, linegraph
 from multiqc.modules.base_module import BaseMultiqcModule
 
 # Initialise the logger
@@ -16,7 +14,7 @@ log = logging.getLogger(__name__)
 
 class MultiqcModule(BaseMultiqcModule):
     """
-    Some class desc #TODO
+    The MultiQC module to parse and plot odgi stats
     """
 
     def __init__(self):
@@ -40,96 +38,73 @@ class MultiqcModule(BaseMultiqcModule):
         # Write parsed report data to a file
         self.write_data_file(self.odgi_stats_map, 'multiqc_odgi_stats')
 
-        self.plot_odgi_stats()
-
-        # Assignment bar plot
-        # self.rsem_mapped_reads_plot()
-
-        # Multimapping line plot
-        # self.rsem_multimapping_plot()
+        # Add the general section containing the general odgi stats
+        self.plot_general_odgi_stats()
 
     def parse_odgi_stats_report(self, f):
         """
-        Bla
+        Parse odgi stats reports
         """
         lines = f['f'].splitlines()
         # parse the generals stats
         stats = lines[1].split('\t')
-        length, nodes, edges, paths = stats[0], stats[1], stats[2], stats[3]
         # parse the mean links length
         mean_links_length = lines[4].split('\t')
-        path, in_node_space, in_nucleotide_space, num_links_considered = mean_links_length[0], mean_links_length[1], mean_links_length[2], mean_links_length[3]
         # parse sum of path nodes distances
         sum_of_path_nodes_distances = lines[7].split('\t')
-        path, in_node_space, in_nucleotide_space, \
-        nodes, nucleotides, num_penalties, \
-        num_penalties_different_orientation = sum_of_path_nodes_distances[0], sum_of_path_nodes_distances[1], \
-                                              sum_of_path_nodes_distances[2], sum_of_path_nodes_distances[3], \
-                                              sum_of_path_nodes_distances[4], sum_of_path_nodes_distances[5], \
-                                              sum_of_path_nodes_distances[6]
         self.odgi_stats_map.update(MultiqcModule.compress_stats_data(f['fn'], stats, mean_links_length, sum_of_path_nodes_distances))
 
-    def plot_odgi_stats(self):
+    def plot_general_odgi_stats(self):
         """
-        Plot the odgi stats
+        Plot the odgi stats with each column in a different color scale
         """
         data = dict()
-
         headers = OrderedDict()
+        headers['Length'] = {
+            'title': 'Length',
+            'description': 'Graph length',
+            'scale': 'BuPu'
+        }
         headers['Nodes'] = {
             'title': 'Nodes',
-            'description': 'My Second Column',
-            'max': 100,
-            'min': 0,
-            'scale': 'Set1',
+            'description': 'Number of nodes in the graph',
+            'scale': 'OrRd',
         }
         headers['Edges'] = {
             'title': 'Edges',
-            'description': 'My Second Column',
-            'max': 100,
-            'min': 0,
-            'scale': 'Set1'
-        }
-        headers['Length'] = {
-            'title': 'Length',
-            'description': 'My Second Column',
-            'max': 100,
-            'min': 0,
-            'scale': 'Set1'
+            'description': 'Number of edges in the graph',
+            'scale': 'PuBu'
         }
         headers['Paths'] = {
             'title': 'Paths',
-            'description': 'My Second Column',
-            'max': 100,
-            'min': 0,
-            'scale': 'Set1'
+            'description': 'Number of paths through the graph',
+            'scale': 'Greens'
         }
-        for i, fn in enumerate(self.odgi_stats_map.keys()):
+        for fn in self.odgi_stats_map.keys():
             file_stats = self.odgi_stats_map[fn]
-            data.update({f'sample{i}': file_stats[f'General stats {fn}']})
-        log.info(data)
+            data.update({fn: file_stats['General stats {}'.format(fn)]})
         self.general_stats_addcols(data, headers)
 
     @staticmethod
     def compress_stats_data(fn, stats, mean_links_length, sum_of_path_nodes_distances) -> dict:
         """
-        Bla
+        Compress odgi stats into a single dictionary
         """
         return {
             fn: {
-                f'General stats {fn}': {
-                    'Length': stats[0],
-                    'Nodes': stats[1],
-                    'Edges': stats[2],
-                    'Paths': stats[3],
+                'General stats {}'.format(fn): {
+                    'Length': int(stats[0]),
+                    'Nodes': int(stats[1]),
+                    'Edges': int(stats[2]),
+                    'Paths': int(stats[3]),
                 },
-                'Mean_links_length': {
+                'Mean_links_length {}'.format(fn): {
                     'Path': mean_links_length[0],
                     'In_node_space': mean_links_length[1],
                     'In_nucleotide_space': mean_links_length[2],
                     'Num_links_considered': mean_links_length[3]
                 },
-                'Sum_of_path_nodes_distances': {
+                'Sum_of_path_nodes_distances {}'.format(fn): {
                     'Path': sum_of_path_nodes_distances[0],
                     'In_node_space': sum_of_path_nodes_distances[1],
                     'In_nucleotide_space': sum_of_path_nodes_distances[2],
@@ -140,53 +115,3 @@ class MultiqcModule(BaseMultiqcModule):
                 }
             }
         }
-
-    def rsem_mapped_reads_plot(self):
-        """ Make the rsem assignment rates plot """
-
-        # Plot categories
-        keys = OrderedDict()
-        keys['Unique'] = {'color': '#437bb1', 'name': 'Aligned uniquely to a gene'}
-        keys['Multi'] = {'color': '#e63491', 'name': 'Aligned to multiple genes'}
-        keys['Filtered'] = {'color': '#b1084c', 'name': 'Filtered due to too many alignments'}
-        keys['Unalignable'] = {'color': '#7f0000', 'name': 'Unalignable reads'}
-
-        # Config for the plot
-        config = {
-            'id': 'rsem_assignment_plot',
-            'title': 'RSEM: Mapped reads',
-            'ylab': '# Reads',
-            'cpswitch_counts_label': 'Number of Reads',
-            'hide_zero_cats': False
-        }
-
-        self.add_section(
-            name='Mapped Reads',
-            anchor='rsem_mapped_reads',
-            description='A breakdown of how all reads were aligned for each sample.',
-            plot=bargraph.plot(self.rsem_mapped_data, keys, config)
-        )
-
-    def rsem_multimapping_plot(self):
-        """ Make a line plot showing the multimapping levels """
-
-        pconfig = {
-            'id': 'rsem_multimapping_rates',
-            'title': 'RSEM: Multimapping Rates',
-            'ylab': 'Counts',
-            'xlab': 'Number of alignments',
-            'xDecimals': False,
-            'ymin': 0,
-            'tt_label': '<b>{point.x} alignments</b>: {point.y:.0f}',
-        }
-
-        self.add_section(
-            name='Multimapping rates',
-            anchor='rsem_multimapping',
-            description='A frequency histogram showing how many reads were aligned to `n` reference regions.',
-            helptext='''In an ideal world, every sequence reads would align uniquely to a single location in the
-                reference. However, due to factors such as repeititve sequences, short reads and sequencing errors,
-                reads can be align to the reference 0, 1 or more times. This plot shows the frequency of each factor
-                of multimapping. Good samples should have the majority of reads aligning once.''',
-            plot=linegraph.plot(self.rsem_multimapping_data, pconfig)
-        )
