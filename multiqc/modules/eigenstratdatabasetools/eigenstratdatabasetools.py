0ca30ecc2e8d4f4018ac7c2d24f17b1e9a0f258d
Thiseas Christos Lamnidis <thisseass@gmail.com>
2021-03-03 16:42:38 +0100

    Black reformatting

--- multiqc/modules/eigenstratdatabasetools/eigenstratdatabasetools.py
+++ multiqc/modules/eigenstratdatabasetools/eigenstratdatabasetools.py
@@ -13,22 +13,26 @@ from multiqc.modules.base_module import BaseMultiqcModule
 # Initialise the logger
 log = logging.getLogger(__name__)
 
+
 class MultiqcModule(BaseMultiqcModule):
     """ eigenstratdatabasetools module """
 
     def __init__(self):
 
         # Initialise the parent object
-        super(MultiqcModule, self).__init__(name='eigenstratdatabasetools', anchor='eigenstrat_snp_coverage',
-        href="https://github.com/TCLamnidis/EigenStratDatabaseTools",
-        info="""A set of tools to compare and manipulate the contents of EingenStrat 
-        databases, and to calculate SNP coverage statistics in such databases. """)
+        super(MultiqcModule, self).__init__(
+            name="eigenstratdatabasetools",
+            anchor="eigenstrat_snp_coverage",
+            href="https://github.com/TCLamnidis/EigenStratDatabaseTools",
+            info="""A set of tools to compare and manipulate the contents of EingenStrat 
+        databases, and to calculate SNP coverage statistics in such databases. """,
+        )
 
         # Find and load any DeDup reports
         self.snp_cov_data = dict()
 
         # Find and load JSON file
-        for f in self.find_log_files('eigenstratdatabasetools', filehandles=True):
+        for f in self.find_log_files("eigenstratdatabasetools", filehandles=True):
             self.parse_data(f)
 
         # Filter samples
@@ -39,25 +43,25 @@ class MultiqcModule(BaseMultiqcModule):
             raise UserWarning
 
         # Save data output file
-        self.write_data_file(self.snp_cov_data, 'multiqc_snp_cov_metrics')
+        self.write_data_file(self.snp_cov_data, "multiqc_snp_cov_metrics")
 
         # Add to General Statistics
         self.addSummaryMetrics()
 
     def parse_data(self, f):
         try:
-            data = json.load(f['f'])
+            data = json.load(f["f"])
         except Exception as e:
             log.debug(e)
-            log.warning("Could not parse eigenstrat_snp_coverage JSON: '{}'".format(f['fn']))
+            log.warning("Could not parse eigenstrat_snp_coverage JSON: '{}'".format(f["fn"]))
             return
 
         # Parse JSON data to a dict
         for s_name in data:
-            if (s_name == 'Metadata'):
+            if s_name == "Metadata":
                 continue
 
-            s_clean = self.clean_s_name(s_name, f['root'])
+            s_clean = self.clean_s_name(s_name, f["root"])
             if s_clean in self.snp_cov_data:
                 log.debug("Duplicate sample name found! Overwriting: {}".format(s_clean))
 
@@ -73,22 +77,22 @@ class MultiqcModule(BaseMultiqcModule):
 
     def addSummaryMetrics(self):
         """ Take the parsed stats from eigenstrat_snp_coverage and add it to the main plot """
-        
+
         headers = OrderedDict()
-        headers['Covered_Snps'] = {
-            'title': '#SNPs Covered',
-            'description': 'The number of SNPs for which a genotype has been called.',
-            'scale': 'PuBuGn',
-            'format': '{:,.0f}',
-            'min': 0
+        headers["Covered_Snps"] = {
+            "title": "#SNPs Covered",
+            "description": "The number of SNPs for which a genotype has been called.",
+            "scale": "PuBuGn",
+            "format": "{:,.0f}",
+            "min": 0,
         }
-        headers['Total_Snps'] = {
-            'title': '#SNPs Total',
-            'description': 'The total number of SNPs in the genotype dataset.',
-            'scale': False,
-            'format': '{:,.0f}',
-            'hidden': True,
-            'min': 0
+        headers["Total_Snps"] = {
+            "title": "#SNPs Total",
+            "description": "The total number of SNPs in the genotype dataset.",
+            "scale": False,
+            "format": "{:,.0f}",
+            "hidden": True,
+            "min": 0,
         }
 
         self.general_stats_addcols(self.snp_cov_data, headers)
