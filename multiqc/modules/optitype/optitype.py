77e74ff5a1d74fc8aa129ef8bacedf94b15a85fe
Phil Ewels <phil.ewels@scilifelab.se>
2021-03-08 00:00:42 +0100

    PR 1387 - tidy up code

--- multiqc/modules/optitype/optitype.py
+++ multiqc/modules/optitype/optitype.py
@@ -38,20 +38,12 @@ class MultiqcModule(BaseMultiqcModule):
             headers = rows[0].split("\t")[1:]
             cols = rows[1].split("\t")[1:]
 
+            if f["s_name"] in self.optitype_data:
+                log.debug("Duplicate sample name found! Overwriting: {}".format(f["s_name"]))
             self.optitype_data[f["s_name"]] = dict()
             for header, col in zip(headers, cols):
-                self.optitype_data[f["s_name"]].update({header: col})
-
-        if len(self.optitype_data) == 0:
-            raise UserWarning
-
-        self.optitype_data = self.ignore_samples(self.optitype_data)
-
-        if len(self.optitype_data) == 0:
-            raise UserWarning
-            if parsed_data is not None and len(parsed_data) > 0:
-                self.optitype_data[f["s_name"]] = parsed_data
-                self.add_data_source(f, f["s_name"])
+                self.optitype_data[f["s_name"]][header] = col
+            self.add_data_source(f)
 
         # Filter to strip out ignored sample names
         self.optitype_data = self.ignore_samples(self.optitype_data)
@@ -75,55 +67,47 @@ class MultiqcModule(BaseMultiqcModule):
         headers["A1"] = {
             "title": "HLA-A1",
             "description": "HLA-I A1 subtype",
-            "scale": "RdGn",
-            "shared_key": "hla",
+            "scale": False,
         }
         headers["A2"] = {
             "title": "HLA-A2",
             "description": "HLA-I A2 subtype",
-            "scale": "RdYlGn",
-            "shared_key": "hla",
+            "scale": False,
             "hidden": True,
         }
         headers["B1"] = {
             "title": "HLA-B1",
             "description": "HLA-I B1 subtype",
-            "scale": "YlGn",
-            "shared_key": "hla",
+            "scale": False,
         }
         headers["B2"] = {
             "title": "HLA-B2",
             "description": "HLA-I B2 subtype",
-            "scale": "OrRd",
-            "shared_key": "hla",
+            "scale": False,
             "hidden": True,
         }
         headers["C1"] = {
             "title": "HLA-C1",
             "description": "HLA-I C1 subtype",
-            "scale": "OrRd",
-            "shared_key": "hla",
+            "scale": False,
         }
         headers["C2"] = {
             "title": "HLA-C2",
             "description": "HLA-I C2 subtype",
-            "scale": "OrRd",
-            "shared_key": "hla",
+            "scale": False,
             "hidden": True,
         }
         headers["Reads"] = {
             "title": "Reads",
             "description": "Number of reads covering the HLA",
-            "scale": "OrRd",
-            "shared_key": "hla",
+            "scale": "YlGnBu",
             "hidden": True,
             "format": "{:,.0f}",
         }
         headers["Objective"] = {
             "title": "Objective Score",
             "description": "Score of the objective function for the prediction.",
-            "scale": "OrRd",
-            "shared_key": "hla",
+            "scale": "BuGn",
             "hidden": True,
         }
         self.general_stats_addcols(self.optitype_data, headers)
