50e331fe6ea61e71a064936721018e2fcc7e767d
Erik Danielsson <53212377+ErikDanielsson@users.noreply.github.com>
2021-06-21 16:54:21 +0200

    Apply suggestions from code review
    Co-authored-by: Phil Ewels <phil.ewels@scilifelab.se>

--- multiqc/modules/qc3C/qc3C.py
+++ multiqc/modules/qc3C/qc3C.py
@@ -964,7 +964,6 @@ class MultiqcModule(BaseMultiqcModule):
                     )
                 )
             parsed = parsed[-1]
-            # Check if fields in json file are named as in the old version
         except json.JSONDecodeError:
             log.warning("Could not parse qc3C JSON: '{}'".format(f["fn"]))
             return
@@ -975,11 +974,11 @@ class MultiqcModule(BaseMultiqcModule):
 
         try:
             analysis_mode = parsed["mode"]
-            if "unobs_fraction" not in parsed:
+            if "unobs_fraction" not in parsed and "unobs_frac" in parsed:
                 parsed["unobs_fraction"] = parsed["unobs_frac"]
 
             if analysis_mode == "bam":
-                if "vs_accepted" not in parsed["separation_bins"]:
+                if "vs_accepted" not in parsed["separation_bins"] and "zvs_accepted " in parsed["separation_bins"]:
                     parsed["separation_bins"]["vs_accepted"] = parsed["separation_bins"]["zvs_accepted"]
 
                 # set some variables to shorten the lines below
