6b41bfdf88c1499a30a9c430e4ad52ed18f74674
Phil Ewels <phil.ewels@scilifelab.se>
2021-06-23 15:09:05 +0200

    Move some of the longer table header descriptions into a help text

--- multiqc/modules/pangolin/pangolin.py
+++ multiqc/modules/pangolin/pangolin.py
@@ -7,8 +7,7 @@ from collections import OrderedDict
 import logging
 import csv
 
-from multiqc import config
-from multiqc.plots import beeswarm, table
+from multiqc.plots import table
 from multiqc.modules.base_module import BaseMultiqcModule
 
 # Initialise the logger
@@ -46,6 +45,28 @@ class MultiqcModule(BaseMultiqcModule):
             name="Run table",
             anchor="pangolin-run-table",
             description="Statistics gathered from the input pangolin files",
+            helptext="""
+            This table shows some of the metrics parsed by Pangolin.
+
+            Hover over the column headers to see a description of the contents. Longer help text for certain columns is shown below:
+
+            * Conflict
+                * In the pangoLEARN decision tree model, a given sequence gets assigned to the most likely category based on known diversity.
+                  If a sequence can fit into more than one category, the conflict score will be greater than 0 and reflect the number of categories the sequence could fit into.
+                  If the conflict score is `0`, this means that within the current decision tree there is only one category that the sequence could be assigned to.
+            * Ambiguity score
+                * This score is a function of the quantity of missing data in a sequence.
+                  It represents the proportion of relevant sites in a sequence which were imputed to the reference values.
+                  A score of 1 indicates that no sites were imputed, while a score of 0 indicates that more sites were imputed than were not imputed.
+                  This score only includes sites which are used by the decision tree to classify a sequence.
+            * Scorpio conflict
+                * The conflict score is the proportion of defining variants which have the reference allele in the sequence.
+                  Ambiguous/other non-ref/alt bases at each of the variant positions contribute only to the denominators of these scores.
+            * Note
+                * If any conflicts from the decision tree, this field will output the alternative assignments.
+                  If the sequence failed QC this field will describe why.
+                  If the sequence met the SNP thresholds for scorpio to call a constellation, it’ll describe the exact SNP counts of Alt, Ref and Amb (Alternative, reference and ambiguous) alleles for that call.
+            """,
             plot=self.pangolin_table(),
         )
 
@@ -86,11 +107,7 @@ class MultiqcModule(BaseMultiqcModule):
         }
         headers["conflict"] = {
             "title": "Conflict",
-            "description": (
-                "In the pangoLEARN decision tree model, a given sequence gets assigned to the most likely category based on known diversity. "
-                "If a sequence can fit into more than one category, the conflict score will be greater than 0 and reflect the number of categories the sequence could fit into. "
-                "If the conflict score is 0, this means that within the current decision tree there is only one category that the sequence could be assigned to."
-            ),
+            "description": "Conflict between categories in decision tree",
             "min": 0,
             "max": 1,
             "scale": "RdBl-rev",
@@ -98,12 +115,7 @@ class MultiqcModule(BaseMultiqcModule):
 
         headers["ambiguity_score"] = {
             "title": "Ambiguity score",
-            "description": (
-                "This score is a function of the quantity of missing data in a sequence. "
-                "It represents the proportion of relevant sites in a sequence which were imputed to the reference values. "
-                "A score of 1 indicates that no sites were imputed, while a score of 0 indicates that more sites were imputed than were not imputed. "
-                "This score only includes sites which are used by the decision tree to classify a sequence."
-            ),
+            "description": "Auantity of missing data in a sequence",
             "min": 0,
             "max": 1,
             "scale": "RdYlGn",
@@ -115,7 +127,7 @@ class MultiqcModule(BaseMultiqcModule):
 
         headers["scorpio_support"] = {
             "title": "Scorpio support",
-            "description": "The support score is the proportion of defining variants which have the alternative allele in the sequence.",
+            "description": "The proportion of defining variants which have the alternative allele in the sequence.",
             "min": 0,
             "max": 1,
             "scale": "RdYlBl",
@@ -123,10 +135,7 @@ class MultiqcModule(BaseMultiqcModule):
 
         headers["scorpio_conflict"] = {
             "title": "Scorpio conflict",
-            "description": (
-                "The conflict score is the proportion of defining variants which have the reference allele in the sequence. "
-                "Ambiguous/other non-ref/alt bases at each of the variant positions contribute only to the denominators of these scores."
-            ),
+            "description": "The proportion of defining variants which have the reference allele in the sequence.",
             "min": 0,
             "max": 1,
             "scale": "RdYlGn-rev",
@@ -159,11 +168,7 @@ class MultiqcModule(BaseMultiqcModule):
 
         headers["note"] = {
             "title": "Note",
-            "description": (
-                "If any conflicts from the decision tree, this field will output the alternative assignments. "
-                "If the sequence failed QC this field will describe why. "
-                "If the sequence met the SNP thresholds for scorpio to call a constellation, it’ll describe the exact SNP counts of Alt, Ref and Amb (Alternative, reference and ambiguous) alleles for that call."
-            ),
+            "description": "Additional information from Pangolin",
         }
 
         return table.plot(self.pangolin_data, headers)
