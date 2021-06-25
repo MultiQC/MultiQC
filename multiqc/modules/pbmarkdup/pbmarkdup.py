4b75116fcf8db66b3c4e13b590c9eaaee297f82b
Phil Ewels <phil.ewels@scilifelab.se>
2021-06-26 00:27:00 +0200

    Run Black and Prettier

--- multiqc/modules/pbmarkdup/pbmarkdup.py
+++ multiqc/modules/pbmarkdup/pbmarkdup.py
@@ -12,6 +12,7 @@ from multiqc.modules.base_module import BaseMultiqcModule
 # Initialise the logger
 log = logging.getLogger(__name__)
 
+
 class MultiqcModule(BaseMultiqcModule):
     """
     pbmarkdup module class, parses pbmarkdup output.
@@ -19,18 +20,21 @@ class MultiqcModule(BaseMultiqcModule):
 
     def __init__(self):
         super(MultiqcModule, self).__init__(
-                name='pbmarkdup', anchor='pbmarkdup',
-                href='https://github.com/PacificBiosciences/pbmarkdup',
-                info=("""
+            name="pbmarkdup",
+            anchor="pbmarkdup",
+            href="https://github.com/PacificBiosciences/pbmarkdup",
+            info=(
+                """
                  takes one or multiple sequencing chips of an amplified libray
                  as HiFi reads and marks or removes duplicates.
-                 """)
+                 """
+            ),
         )
 
         self.pbmarkdup = dict()
 
-        for logfile in self.find_log_files('pbmarkdup', filehandles=True):
-            self.pbmarkdup[logfile['s_name']] = self.parse_logfile(logfile)
+        for logfile in self.find_log_files("pbmarkdup", filehandles=True):
+            self.pbmarkdup[logfile["s_name"]] = self.parse_logfile(logfile)
 
         # Filter to strip out ignored sample names
         self.pbmarkdup = self.ignore_samples(self.pbmarkdup)
@@ -40,7 +44,7 @@ class MultiqcModule(BaseMultiqcModule):
             raise UserWarning
 
         # Write the parsed data to file
-        self.write_data_file(self.pbmarkdup, 'multiqc_pbmarkdup')
+        self.write_data_file(self.pbmarkdup, "multiqc_pbmarkdup")
 
         self.pbmarkdup_add_general_stats()
 
@@ -56,12 +60,12 @@ class MultiqcModule(BaseMultiqcModule):
         order, without any missing values.
         """
 
-        file_content = logfile['f']
+        file_content = logfile["f"]
 
         # The number of spaces in the header can vary, based on the length of
         # the name of the library. We therefore need a regex to match the
         # header.
-        header_pattern = 'LIBRARY *READS *UNIQUE MOLECULES *DUPLICATE READS'
+        header_pattern = "LIBRARY *READS *UNIQUE MOLECULES *DUPLICATE READS"
         pattern = re.compile(header_pattern)
 
         # The file header
@@ -78,7 +82,7 @@ class MultiqcModule(BaseMultiqcModule):
         for line in file_content:
             # Lines which start with --- denote separators in the file, and do
             # not need to be parsed
-            if line.startswith('-'):
+            if line.startswith("-"):
                 continue
 
             # Not very nice, we assume that all fields are always present
@@ -86,54 +90,57 @@ class MultiqcModule(BaseMultiqcModule):
 
             # We are only interested in the counts, not the percentages
             data[name] = {
-                    'READS': int(reads),
-                    'UNIQUE MOLECULES': int(unique_mol_count),
-                    'DUPLICATE READS': int(duplicate_count)
+                "READS": int(reads),
+                "UNIQUE MOLECULES": int(unique_mol_count),
+                "DUPLICATE READS": int(duplicate_count),
             }
 
         return data
 
     def pbmarkdup_add_general_stats(self):
-        """ Add pbmarkdup duplicates to the general stats table """
+        """Add pbmarkdup duplicates to the general stats table"""
 
-        general_stats_headers = OrderedDict([
-            ('unique_molecules', {
-                'id': 'unique_molecules',
-                'title': '% Unique Molecules',
-                'description': 'Percentage of unique molecules',
-                'suffix': '%',
-                'min': 0,
-                'max': 100,
-                'modify': lambda x: x * 100,
-                'scale': 'RdYlGn'
-                }
-            ),
-            ('duplicate_reads', {
-                'id': 'duplicate_erads',
-                'title': '% Duplicate Reads',
-                'description': 'Percentage of duplicate reads',
-                'suffix': '%',
-                'min': 0,
-                'max': 100,
-                'modify': lambda x: x * 100,
-                'scale': 'RdYlGn-rev'
-                }
-            )
-        ])
+        general_stats_headers = OrderedDict(
+            [
+                (
+                    "unique_molecules",
+                    {
+                        "id": "unique_molecules",
+                        "title": "% Unique Molecules",
+                        "description": "Percentage of unique molecules",
+                        "suffix": "%",
+                        "min": 0,
+                        "max": 100,
+                        "modify": lambda x: x * 100,
+                        "scale": "RdYlGn",
+                    },
+                ),
+                (
+                    "duplicate_reads",
+                    {
+                        "id": "duplicate_erads",
+                        "title": "% Duplicate Reads",
+                        "description": "Percentage of duplicate reads",
+                        "suffix": "%",
+                        "min": 0,
+                        "max": 100,
+                        "modify": lambda x: x * 100,
+                        "scale": "RdYlGn-rev",
+                    },
+                ),
+            ]
+        )
 
         general = dict()
 
         for sample, sample_stats in self.pbmarkdup.items():
             # We are only interested in the total counts per sample
-            total = sample_stats['TOTAL']
+            total = sample_stats["TOTAL"]
 
             # Calculate the percentages unique and duplicated
-            perc_unique = total['UNIQUE MOLECULES']/total['READS']
-            perc_duplicate = total['DUPLICATE READS']/total['READS']
+            perc_unique = total["UNIQUE MOLECULES"] / total["READS"]
+            perc_duplicate = total["DUPLICATE READS"] / total["READS"]
 
-            general[sample] = {
-                    'unique_molecules': perc_unique,
-                    'duplicate_reads': perc_duplicate
-            }
+            general[sample] = {"unique_molecules": perc_unique, "duplicate_reads": perc_duplicate}
 
         self.general_stats_addcols(general, general_stats_headers)
