11b5d3ca7671cfb0a93ed7eb6460dd31f4eece7c
Phil Ewels <phil.ewels@scilifelab.se>
2021-06-25 23:55:57 +0200

    Run Black for python formatting

--- multiqc/modules/lima/lima.py
+++ multiqc/modules/lima/lima.py
@@ -13,12 +13,15 @@ from multiqc.utils import config
 # Initialise the logger
 log = logging.getLogger(__name__)
 
+
 class MultiqcModule(BaseMultiqcModule):
     def __init__(self):
         # Initialse the parent object
-        super(MultiqcModule, self).__init__(name='Lima', anchor='lima',
-              href='https://github.com/PacificBiosciences/barcoding',
-              info= ' is used to demultiplex single-molecule sequencing reads.'
+        super(MultiqcModule, self).__init__(
+            name="Lima",
+            anchor="lima",
+            href="https://github.com/PacificBiosciences/barcoding",
+            info=" is used to demultiplex single-molecule sequencing reads.",
         )
 
         # To store the summary data
@@ -43,21 +46,21 @@ class MultiqcModule(BaseMultiqcModule):
         self.plot_counts_data()
 
     def parse_summary_files(self):
-        for f in self.find_log_files('lima/summary', filehandles=True):
-            data = parse_PacBio_log(f['f'])
-            filename = f['s_name']
+        for f in self.find_log_files("lima/summary", filehandles=True):
+            data = parse_PacBio_log(f["f"])
+            filename = f["s_name"]
             self.lima_summary[filename] = data
             self.add_data_source(f)
 
     def parse_counts_files(self):
-        for f in self.find_log_files('lima/counts', filehandles=True):
-            data = self.parse_lima_counts(f['f'], f['root'])
+        for f in self.find_log_files("lima/counts", filehandles=True):
+            data = self.parse_lima_counts(f["f"], f["root"])
             # Update sample names if --lima-barcodes was specified
             self.update_sample_names(data)
             # Check for duplicate samples
             for sample in data:
                 if sample in self.lima_counts:
-                    msg = f'Duplicate sample name found! Overwriting: {sample}'
+                    msg = f"Duplicate sample name found! Overwriting: {sample}"
                     log.debug(msg)
             # After warning the user, overwrite the samples
             self.lima_counts.update(data)
@@ -69,12 +72,12 @@ class MultiqcModule(BaseMultiqcModule):
         if not self.lima_summary and not self.lima_counts:
             raise UserWarning
         if self.lima_summary:
-            self.write_data_file(self.lima_summary, 'multiqc_lima_summary')
+            self.write_data_file(self.lima_summary, "multiqc_lima_summary")
         if self.lima_counts:
-            self.write_data_file(self.lima_counts, 'multiqc_lima_counts')
+            self.write_data_file(self.lima_counts, "multiqc_lima_counts")
 
     def parse_lima_counts(self, file_content, root):
-        """ Parse lima counts file """
+        """Parse lima counts file"""
 
         # The first line is the header
         header = next(file_content).strip().split()
@@ -85,22 +88,19 @@ class MultiqcModule(BaseMultiqcModule):
             spline = line.strip().split()
             data = {field: value for field, value in zip(header, spline)}
 
-            first_barcode = data['IdxFirstNamed']
-            second_barcode = data['IdxCombinedNamed']
+            first_barcode = data["IdxFirstNamed"]
+            second_barcode = data["IdxCombinedNamed"]
             # The format barcode1--barcode2 is also used in
             # self.update_sample_names
-            sample = self.clean_s_name(f'{first_barcode}--{second_barcode}', root)
-            counts = data['Counts']
-            mean_score = data['MeanScore']
-            lima_counts[sample] = {
-                    'Counts': int(counts),
-                    'MeanScore': float(mean_score)
-            }
+            sample = self.clean_s_name(f"{first_barcode}--{second_barcode}", root)
+            counts = data["Counts"]
+            mean_score = data["MeanScore"]
+            lima_counts[sample] = {"Counts": int(counts), "MeanScore": float(mean_score)}
 
         return lima_counts
 
     def update_sample_names(self, data):
-        lima_barcodes = config.kwargs['lima_barcodes']
+        lima_barcodes = config.kwargs["lima_barcodes"]
         # If --lima-barcodes wasn't specified
         if not lima_barcodes:
             return
@@ -127,8 +127,8 @@ class MultiqcModule(BaseMultiqcModule):
         barcodes = dict()
         with open(lima_barcodes) as fin:
             for line in fin:
-                sample, first_barcode, second_barcode = line.strip().split('\t')
-                barcodes[f'{first_barcode}--{second_barcode}'] = sample
+                sample, first_barcode, second_barcode = line.strip().split("\t")
+                barcodes[f"{first_barcode}--{second_barcode}"] = sample
         return barcodes
 
     def add_sections(self):
@@ -146,10 +146,10 @@ class MultiqcModule(BaseMultiqcModule):
             for reason in all_filters:
                 d[reason] = dict()
                 # This is where the filter reasons are stored
-                zmw_marginals = data['ZMWs below any threshold']['ZMW marginals']
+                zmw_marginals = data["ZMWs below any threshold"]["ZMW marginals"]
                 for filter_reason in zmw_marginals:
                     if filter_reason == reason:
-                        d[reason][filename] = zmw_marginals[filter_reason]['count']
+                        d[reason][filename] = zmw_marginals[filter_reason]["count"]
                         break
                 # If we didn't find filter reason for this filename, set it to
                 # zero
@@ -157,92 +157,80 @@ class MultiqcModule(BaseMultiqcModule):
                     d[reason][filename] = 0
                 # Unless 'reason' is actually the special case of the number of
                 # unfiltered reads
-                if reason == 'ZMWs above all thresholds':
-                    d[reason][filename] = data['ZMWs above all thresholds']['count']
+                if reason == "ZMWs above all thresholds":
+                    d[reason][filename] = data["ZMWs above all thresholds"]["count"]
             plot_data.append(d)
 
         # Plot configuration
         config = {
-                'id': 'lima-filter-graph',
-                'title': 'Lima: ZMW results',
-                'ylab': 'Number of ZMWs',
-                'xlab': 'Lima summary file',
-                'cpswitch': False,
-                'logswitch': True,
-                'stacking': None,
-                'data_labels': [
-                    {
-                        'name': filename,
-                        'cpswitch_counts_label': filename
-                    } for filename in self.lima_summary
-                ]
+            "id": "lima-filter-graph",
+            "title": "Lima: ZMW results",
+            "ylab": "Number of ZMWs",
+            "xlab": "Lima summary file",
+            "cpswitch": False,
+            "logswitch": True,
+            "stacking": None,
+            "data_labels": [{"name": filename, "cpswitch_counts_label": filename} for filename in self.lima_summary],
         }
 
         formatting = [filename for filename in self.lima_summary]
 
         self.add_section(
-                name='ZMWs filtered by Lima',
-                anchor='lima-filter',
-                description=(
-                    'The number of ZMWs that failed or passed all Lima '
-                    'filters'
-                ),
-                helptext=(
-                    'The number of ZMWs that passed all filters is shown as '
-                    '**ZMWs above all thresholds**, all other categories that '
-                    'are shown in the graph represent the number of ZMWs that '
-                    'were dropped for the specified reason.'
-                ),
-                plot = bargraph.plot(plot_data, formatting, config)
+            name="ZMWs filtered by Lima",
+            anchor="lima-filter",
+            description=("The number of ZMWs that failed or passed all Lima " "filters"),
+            helptext=(
+                "The number of ZMWs that passed all filters is shown as "
+                "**ZMWs above all thresholds**, all other categories that "
+                "are shown in the graph represent the number of ZMWs that "
+                "were dropped for the specified reason."
+            ),
+            plot=bargraph.plot(plot_data, formatting, config),
         )
 
     def filter_and_pass(self, data):
-        """ Get the reasons why each ZMW was filtered """
+        """Get the reasons why each ZMW was filtered"""
         reasons = dict()
 
         # Add why ZMWs were filtered
-        filter_data = data['ZMWs below any threshold']['ZMW marginals']
+        filter_data = data["ZMWs below any threshold"]["ZMW marginals"]
         for reason in filter_data:
-            reasons[reason] = filter_data[reason]['count']
+            reasons[reason] = filter_data[reason]["count"]
         # Add the ZMWs that passed
-        reasons['ZMWs above all thresholds'] = data['ZMWs above all thresholds']['count']
+        reasons["ZMWs above all thresholds"] = data["ZMWs above all thresholds"]["count"]
 
         return reasons
 
     def plot_counts_data(self):
-        """ Plot the lima counts data """
+        """Plot the lima counts data"""
         pdata = list()
 
-        categories = ['Counts', 'MeanScore']
-        description = ['Number of Reads', 'Mean Quality Score']
+        categories = ["Counts", "MeanScore"]
+        description = ["Number of Reads", "Mean Quality Score"]
 
         configuration = {
-            'id': 'multiqc_lima_counts',
-            'title': 'Lima: Number of Reads',
-            'anchor': 'multiqc_lima_counts',
+            "id": "multiqc_lima_counts",
+            "title": "Lima: Number of Reads",
+            "anchor": "multiqc_lima_counts",
             # Placeholder y-label, the actual values will be set below in
             # data_labels
-            'ylab': '# Reads',
-            'data_labels': [
-                {
-                    'name': category,
-                    'cpswitch_counts_label': category,
-                    'ylab': description
-                } for category, description in zip(categories, description)
-            ]
+            "ylab": "# Reads",
+            "data_labels": [
+                {"name": category, "cpswitch_counts_label": category, "ylab": description}
+                for category, description in zip(categories, description)
+            ],
         }
 
         for category in categories:
             data = dict()
             for sample, entry in self.lima_counts.items():
-                data[sample] = { category: entry[category] }
+                data[sample] = {category: entry[category]}
             pdata.append(data)
 
         self.add_section(
-                name='Per sample count data',
-                anchor='multiqc_lima_count',
-                description=
-                """
+            name="Per sample count data",
+            anchor="multiqc_lima_count",
+            description="""
                     Per sample or per barcode statistics from Lima.
 
                     The **Counts** show the
@@ -251,18 +239,17 @@ class MultiqcModule(BaseMultiqcModule):
                     The **MeanScore** shows the mean quality score of the reads for
                     each sample or barcode pair.
                 """,
-                helptext=
-                """
+            helptext="""
                 To display sample names instead of `barcode--barcode` pairs,
                 please re-run MultiQC and specify which barcodes belong to
                 which sample using the `--lima-barcodes` flag.
                 """,
-
-                plot=bargraph.plot(pdata, categories, configuration)),
+            plot=bargraph.plot(pdata, categories, configuration),
+        ),
 
 
 def parse_PacBio_log(file_content):
-    """ Parse PacBio log file """
+    """Parse PacBio log file"""
     data = dict()
     # This is a local dictionary to store which annotations belong to which
     # result dictionary. This will be used to structure the output, but will
@@ -272,14 +259,14 @@ def parse_PacBio_log(file_content):
 
     for line in file_content:
         # Get rid of trailing newlines
-        line = line.strip('\n')
+        line = line.strip("\n")
         # Did we enter a new section with annotations for an earlier result?
         # If so, we will only add an empty dictionary with the correct name
         # These field are of the format "something something for (A):"
-        section_header_pattern = ' for [(][A-Z][)][:]$'
+        section_header_pattern = " for [(][A-Z][)][:]$"
         if re.search(section_header_pattern, line):
             linedata = parse_line(line)
-            ann = linedata['annotation']
+            ann = linedata["annotation"]
             # Cut off the ' for (B):' part
             name = line[:-9]
             # We make a new heading with the current name under the data that
@@ -299,7 +286,7 @@ def parse_PacBio_log(file_content):
             continue
 
         # Lets get the name of the data
-        name = linedata.pop('name')
+        name = linedata.pop("name")
         # If we are in an annotated section, we add the data to the current
         # annotation
         if current_annotation is not None:
@@ -308,7 +295,7 @@ def parse_PacBio_log(file_content):
         # we find a corresponding section later on.
         # The data from the current line we add directly to the output data
         else:
-            annotation = linedata.pop('annotation')
+            annotation = linedata.pop("annotation")
             annotations[annotation] = linedata
             data[name] = linedata
 
@@ -316,7 +303,7 @@ def parse_PacBio_log(file_content):
 
 
 def parse_line(line):
-    """ Parse a line from the Lima log file """
+    """Parse a line from the Lima log file"""
     data = dict()
 
     # If we got an empty line to parse
@@ -324,20 +311,20 @@ def parse_line(line):
         return data
 
     # Split the line on the colon character
-    key, values = line.strip().split(':')
+    key, values = line.strip().split(":")
 
     # The key can have multiple parts
     keys = key.strip().split()
 
     # Does the key have an annotation (A), (B) etc
-    if re.fullmatch('[(][A-Z][)]', keys[-1]):
+    if re.fullmatch("[(][A-Z][)]", keys[-1]):
         # We store the annotation without the bracets
-        data['annotation'] = keys[-1][1:-1]
+        data["annotation"] = keys[-1][1:-1]
         # And we add the rest of the key as name
-        data['name'] = ' '.join(keys[:-1])
+        data["name"] = " ".join(keys[:-1])
     # Otherwise, we just store the name
     else:
-        data['name'] = ' '.join(keys)
+        data["name"] = " ".join(keys)
 
     # Parsing the values
     values = values.strip().split()
@@ -349,11 +336,11 @@ def parse_line(line):
     if len(values) == 1:
         value = values.pop()
         # Is the value a percentage
-        if value.endswith('%'):
-            data['percentage'] = float(value[:-1])
+        if value.endswith("%"):
+            data["percentage"] = float(value[:-1])
         # Otherwise, it is an integer
         else:
-            data['count'] = int(value)
+            data["count"] = int(value)
     elif len(values) == 2:
         # If there are multiple values, they are in the format
         # count (percentage%)
@@ -365,14 +352,14 @@ def parse_line(line):
         # So we can remove the bracets first
         percentage = percentage[1:-1]
         # Then we make sure it is one of the supported patterns
-        assert re.fullmatch(r'(\d+\.\d+%|\d+%|-nan%)', percentage)
+        assert re.fullmatch(r"(\d+\.\d+%|\d+%|-nan%)", percentage)
 
         # If the percentage is this weird nan, set it to 0
-        if percentage == '-nan%':
-            data['percentage'] = 0.0
+        if percentage == "-nan%":
+            data["percentage"] = 0.0
         else:  # Otherwise, we cut of the % sign and convert to float
-            data['percentage'] = float(percentage[:-1])
+            data["percentage"] = float(percentage[:-1])
         # Add the count as an integer
-        data['count'] = int(count)
+        data["count"] = int(count)
 
     return data
