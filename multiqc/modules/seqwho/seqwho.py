85a8bde8c96afae872c9040ee43c9e919286bd3f
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2021-07-26 13:04:38 -0500

    Fix syntax.

--- multiqc/modules/seqwho/seqwho.py
+++ multiqc/modules/seqwho/seqwho.py
@@ -37,7 +37,6 @@ class MultiqcModule(BaseMultiqcModule):
         self.seqwho_qualscore = dict()
         self.seqwho_readdist = dict()
 
-
         for f in self.find_log_files("seqwho", filehandles=True):
             try:
                 self.parseJSON(f)
@@ -50,7 +49,6 @@ class MultiqcModule(BaseMultiqcModule):
         self.seqwho_qualscore = self.ignore_samples(self.seqwho_qualscore)
         self.seqwho_readdist = self.ignore_samples(self.seqwho_readdist)
 
-
         if len(self.seqwho_data) == 0:
             raise UserWarning
 
@@ -59,20 +57,19 @@ class MultiqcModule(BaseMultiqcModule):
         # Write parsed report data to a file
         self.write_data_file(self.seqwho_data, "multiqc_seqwho")
 
-
         # General Stats Table
         self.seqwho_general_stats_table()
 
         # SeqWho MLE Species Table
         self.add_section(
             description="This plot shows the Maximum Likelihood that a given Species matches.",
-            plot=self.seqwho_species_plot()
+            plot=self.seqwho_species_plot(),
         )
 
         # SeqWho MLE Sequencing Table
         self.add_section(
             description="This plot shows the Maximum Likelihood that a given Library matches.",
-            plot=self.seqwho_library_plot()
+            plot=self.seqwho_library_plot(),
         )
 
         # Quality Distribution
@@ -83,8 +80,6 @@ class MultiqcModule(BaseMultiqcModule):
 
         # Read Distribution
         self.seqwho_readdist_plot()
-
-
 
     # Parse our nice little JSON file
     def parseJSON(self, f):
@@ -101,37 +96,47 @@ class MultiqcModule(BaseMultiqcModule):
             self.add_data_source(f, s_name)
             self.seqwho_data[s_name] = {}
 
-            call_data = parsed_json[s_name]['Call']
+            call_data = parsed_json[s_name]["Call"]
             self.seqwho_data[s_name]["predicted_species"] = call_data["Species"]
             self.seqwho_data[s_name]["predicted_library"] = call_data["Library"]
             self.seqwho_data[s_name]["mle"] = call_data["Maximum Likelihood Estimate"]
             self.seqwho_data[s_name]["est_read_number"] = parsed_json[s_name]["Estimated Read Number"]
 
             # Make MLE Matrix table for subset of predicted species and library
-            mle_matrix = call_data['MLE Matrix']
-            mle_columns = ['human', 'mouse', 'amplicon', 'bisulf', 'wxs', 'chip', 'wgs', 'dnase', 'rnaseq', 'atac', 'mirnaseq']
+            mle_matrix = call_data["MLE Matrix"]
+            mle_columns = [
+                "human",
+                "mouse",
+                "amplicon",
+                "bisulf",
+                "wxs",
+                "chip",
+                "wgs",
+                "dnase",
+                "rnaseq",
+                "atac",
+                "mirnaseq",
+            ]
             for i in mle_columns:
                 self.seqwho_data[s_name][i] = mle_matrix[i]
 
-
             # Make a Quality Score vs count  version of the data
             self.seqwho_qualdis[s_name] = OrderedDict()
-            qual_dist =  parsed_json[s_name]["Quality Dist"]
+            qual_dist = parsed_json[s_name]["Quality Dist"]
             for i in range(len(qual_dist)):
                 self.seqwho_qualdis[s_name][i] = qual_dist[i]
 
             # Make a Quality score vs position  version of the data
             self.seqwho_qualscore[s_name] = OrderedDict()
-            qual_scores =  parsed_json[s_name]["Quality Scores"]
+            qual_scores = parsed_json[s_name]["Quality Scores"]
             for i in range(len(qual_scores)):
                 self.seqwho_qualscore[s_name][i] = qual_scores[i]
 
             # Make a Read Length vs count of the data
             self.seqwho_readdist[s_name] = OrderedDict()
-            read_dist =  parsed_json[s_name]["Read lengths"]
+            read_dist = parsed_json[s_name]["Read lengths"]
             for i in range(len(read_dist)):
                 self.seqwho_readdist[s_name][i] = read_dist[i]
-
 
     def seqwho_general_stats_table(self):
         """Take the parsed stats from the SeqWho report and add it to the
@@ -162,46 +167,43 @@ class MultiqcModule(BaseMultiqcModule):
         }
         self.general_stats_addcols(self.seqwho_data, headers)
 
-
     def seqwho_species_plot(self):
         """Take the Maximimum Likelihood Species data from the SeqWho report and make
         barchart"""
 
-        headers = ['human', 'mouse']
+        headers = ["human", "mouse"]
         # Config for the plot
         config = {
             "id": "SeqWho_species_plot",
             "title": "SeqWho: Species",
             "ylab": "Sample",
-            'ymax':1.0,
+            "ymax": 1.0,
             "stacking": None,
-            'cpswitch': False,
-            'cpswitch_counts_label': 'Maximum Likelihood',
-            'tt_decimals': 3,
+            "cpswitch": False,
+            "cpswitch_counts_label": "Maximum Likelihood",
+            "tt_decimals": 3,
         }
 
         return bargraph.plot(self.seqwho_data, headers, config)
-
 
     def seqwho_library_plot(self):
         """Take the Maximimum Likelihood Library data from the SeqWho report and make a
         barplot"""
 
-        headers = ['amplicon', 'bisulf', 'wxs', 'chip', 'wgs', 'dnase', 'rnaseq', 'atac', 'mirnaseq']
+        headers = ["amplicon", "bisulf", "wxs", "chip", "wgs", "dnase", "rnaseq", "atac", "mirnaseq"]
         # Config for the plot
         config = {
             "id": "SeqWho_sequencing_plot",
             "title": "SeqWho: Library",
             "ylab": "Sample",
-            'ymax':1.0,
+            "ymax": 1.0,
             "stacking": None,
-            'cpswitch': False,
-            'cpswitch_counts_label': 'Maximum Likelihood',
-            'tt_decimals': 3,
+            "cpswitch": False,
+            "cpswitch_counts_label": "Maximum Likelihood",
+            "tt_decimals": 3,
         }
 
         return bargraph.plot(self.seqwho_data, headers, config)
-
 
     def seqwho_qualdist_plot(self):
         """Generate the Average Sequence Quality Distribution plot"""
@@ -221,7 +223,6 @@ class MultiqcModule(BaseMultiqcModule):
             plot=linegraph.plot([self.seqwho_qualdis], pconfig),
         )
 
-
     def seqwho_qualscore_plot(self):
         """Generate the Average Quality Score per position plot"""
         pconfig = {
@@ -239,7 +240,6 @@ class MultiqcModule(BaseMultiqcModule):
             description="This plot shows the predicted mean quality value across each base position in the read.",
             plot=linegraph.plot([self.seqwho_qualscore], pconfig),
         )
-
 
     def seqwho_readdist_plot(self):
         """Generate the number of reads with certain length plot"""
