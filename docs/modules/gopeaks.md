a470e625725f60174d5f094c93d4bbdd9d068870
Jake VanCampen <jake.vancampen7@gmail.com>
2021-08-12 20:09:36 -0700

    add plotting and configuration

--- docs/modules/gopeaks.md
+++ docs/modules/gopeaks.md
@@ -2,8 +2,17 @@
 Name: gopeaks
 URL: https://github.com/maxsonBraunLab/gopeaks
 Description: >
-    Peak caller for CUT&TAG sequencing data.
+  Peak caller for CUT&TAG sequencing data.
 ---
 
-The gopeaks module parses runtime data output from [gopeaks](https://github.com/maxsonBraunLab/gopeaks) 
-by searching for the files ending in "_gopeaks.json", and adds to the multiqc data table. 
+[Gopeaks](https://github.com/maxsonBraunLab/gopeaks) is designed to call peaks from aligned CUT&TAG sequencing reads. Gopeaks uses a binomial distribution to model the read counts in sliding windows across the genome and calculate peak regions that are enriched over the background.
+
+The gopeaks module parses runtime data output from [gopeaks](https://github.com/maxsonBraunLab/gopeaks) by searching for files ending in "\_gopeaks.json".
+
+By default the module will add data from the gopeaks.json file to the table data, and also include a barchart of the number peaks found for each sample. The following configuration options can be switched to `False` to turn off either or both display options.
+
+```yaml
+gopeaks_config:
+  show_table_data: True
+  show_peak_counts_plot: True
+```
--- multiqc/modules/gopeaks/gopeaks.py
+++ multiqc/modules/gopeaks/gopeaks.py
@@ -1,4 +1,4 @@
-"""MultiQC module to plot output statistics from gopeaks 
+"""MultiQC module to plot output statistics from gopeaks peak caller 
 
 https://github.com/maxsonBraunLab/gopeaks
 """
@@ -8,9 +8,9 @@ from collections import OrderedDict
 import logging
 import json
 
-from multiqc import config
-from multiqc.plots import linegraph, scatter
+from multiqc.plots import bargraph
 from multiqc.modules.base_module import BaseMultiqcModule
+from multiqc import config
 
 # Initialise the logger
 log = logging.getLogger(__name__)
@@ -19,14 +19,18 @@ log = logging.getLogger(__name__)
 class MultiqcModule(BaseMultiqcModule):
     def __init__(self):
         super(MultiqcModule, self).__init__(
-            name="gopeaks",
-            anchor="gopeaks",
+            name="Gopeaks",
+            anchor="Gopeaks",
             href="https://github.com/maxsonBraunLab/gopeak",
-            info="Peak caller for CUT&TAG data.",
+            info="is designed to call peaks from aligned CUT&TAG sequencing reads.\
+                  Gopeaks uses a binomial distribution to model the read counts \
+                  in sliding windows across the genome and calculate peak regions \
+                  that are enriched over the background."
         )
 
         self.macs_data = dict()
-        self.data = {}
+        self.table_data = {}
+        self.plot_data = {}
         for f in self.find_log_files("gopeaks", filehandles=True):
             self.add_data_source(f)
             self.parse_gopeaks_json(f)
@@ -46,10 +50,35 @@ class MultiqcModule(BaseMultiqcModule):
                 'format': '{:d}',
             },
         })
-        self.general_stats_addcols(self.data, headers)
+
+        # get display config
+        gopeaks_config = getattr(config, 'gopeaks_config', {})
+        show_table_data = gopeaks_config.get('show_table_data', True)
+        show_peak_counts_plot = gopeaks_config.get('show_peak_counts_plot', True)
+
+        if show_table_data:
+            self.general_stats_addcols(self.table_data, headers)
+        if show_peak_counts_plot:
+            self.add_plot_data()
         
     
     def parse_gopeaks_json(self, file):
-        # parse the json fields
+
+        # parse the gopeaks metrics 
         gopeaks_data = json.load(file['f'])
-        self.data[gopeaks_data['prefix']] = gopeaks_data
+        sample = gopeaks_data['prefix']
+
+        # log the clobber
+        if sample in self.table_data:
+            log.debug("Duplicate sample name found, overwriting: {}.".format(sample))
+
+        self.table_data[gopeaks_data['prefix']] = gopeaks_data
+        self.plot_data[gopeaks_data['prefix']] = {"counts": gopeaks_data['peak_counts']}
+
+    def add_plot_data(self):
+        self.add_section(
+            name = 'Peak Counts',
+            anchor = 'gopeaks_counts',
+            description = 'This plot displays the number of peaks determined by gopeaks for each sample.',
+            plot = bargraph.plot(self.plot_data)
+        )
