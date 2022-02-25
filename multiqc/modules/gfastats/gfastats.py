a3c559249fe96086679947a16bd45ed62ff0d0d1
gf777 <giulio.formenti@gmail.com>
2022-02-25 00:27:29 -0500

    added gfastats

---
+++ multiqc/modules/gfastats/gfastats.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+""" MultiQC module to parse output from gfastats """
+from __future__ import print_function
+from collections import OrderedDict
+import logging
+
+from multiqc.modules.base_module import BaseMultiqcModule
+
+# Import the gfastats submodules
+from .stats import StatsReportMixin
+
+# Initialise the logger
+log = logging.getLogger(__name__)
+
+class MultiqcModule(BaseMultiqcModule, StatsReportMixin):
+    """gfastats has a number of different commands and outputs.
+    This MultiQC module supports some but not all. The code for
+    each script is split into its own file and adds a section to
+    the module output if logs are found."""
+
+    def __init__(self):
+
+        # Initialise the parent object
+        super(MultiqcModule, self).__init__(
+            name="gfastats",
+            anchor="gfastats",
+            target="gfastats",
+            href="https://github.com/vgl-hub/gfastats",
+            info=" is a single fast and exhaustive tool for summary statistics and simultaneous *fa* (fasta, fastq, gfa [.gz]) genome assembly file manipulation..",
+            doi="",
+        )
+
+        # Set up class objects to hold parsed data
+        self.general_stats_headers = OrderedDict()
+        self.general_stats_data = dict()
+        n = dict()
+        
+        # Call submodule functions
+        n["stats"] = self.parse_gfastats_stats()
+        if n["stats"] > 0:
+            log.info("Found {} gfastats stats reports".format(n["stats"]))
+
+        # Exit if we didn't find anything
+        if sum(n.values()) == 0:
+            raise UserWarning
+
+        # Add to the General Stats table (has to be called once per MultiQC module)
+        self.general_stats_addcols(self.general_stats_data, self.general_stats_headers)
---
+++ multiqc/modules/gfastats/stats.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" MultiQC submodule to parse output from gfastats --stats """
+
+import logging
+from collections import OrderedDict
+from multiqc import config
+from multiqc.plots import beeswarm, bargraph
+
+# Initialise the logger
+log = logging.getLogger(__name__)
+
+class StatsReportMixin:
+    """Mixin class, loaded by main gfastats MutliqcModule class."""
+
+    def parse_gfastats_stats(self):
+        """Find gfastats stats logs and parse summary statistics"""
+        
+        self.gfastats_stats = dict()
+        for f in self.find_log_files("gfastats/stats"):
+            parsed_data = dict()
+            for line in f["f"].splitlines()[1:]:
+                sections = line.split("\t")
+                field = sections[0]
+                field = field.replace(" ", "_")
+                if not field == "Base_composition_(ACGT)":
+                    value = float(sections[1])
+                    parsed_data[field] = value
+
+            if len(parsed_data) > 0:
+                if f["s_name"] in self.gfastats_stats:
+                    log.debug("Duplicate sample name found! Overwriting: {}".format(f["s_name"]))
+                self.add_data_source(f, section="stats")
+                self.gfastats_stats[f["s_name"]] = parsed_data
+
+        # Filter to strip out ignored sample names
+        self.gfastats_stats = self.ignore_samples(self.gfastats_stats)
+
+        if len(self.gfastats_stats) > 0:
+
+            # Write parsed report data to a file
+            self.write_data_file(self.gfastats_stats, "multiqc_gfastats_stats")
+
+            # General Stats Table
+#            stats_headers = OrderedDict()
+#            stats_headers["Total_scaffold_length"] = {
+#                "title": "Total scaffold length",
+#                "description": "Total length of scaffolds",
+#            }
+            
+            self.general_stats_addcols(self.gfastats_stats)
+
+        # Return the number of logs that were found
+        return len(self.gfastats_stats)
