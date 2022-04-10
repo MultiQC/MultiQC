c255ffe730f4d4463e62ad841b4777c62826e811
Josh Chorlton <jchorlton@gmail.com>
2022-04-10 15:02:34 +0000

    allow default sorting of mqc tables

--- multiqc/plots/table.py
+++ multiqc/plots/table.py
@@ -356,9 +356,9 @@ def make_table(dt):
     html += """
         <div id="{tid}_container" class="mqc_table_container">
             <div class="table-responsive mqc-table-responsive {cc}">
-                <table id="{tid}" class="table table-condensed mqc_table" data-title="{title}">
+                <table id="{tid}" class="table table-condensed mqc_table" data-title="{title}" data-sortlist="{sortlist}">
         """.format(
-        tid=table_id, title=table_title, cc=collapse_class
+        tid=table_id, title=table_title, cc=collapse_class, sortlist=dt.pconfig.get('defaultsort', ''),
     )
 
     # Build the header row
