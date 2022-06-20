e43877af1b1d2651a5fd71ca727e8551ce0091b0
nicolawonggg <nicola.wong@cegx.co.uk>
2022-06-20 12:10:37 +0100

    removed some copied code

--- multiqc/modules/cegx/cegx.py
+++ multiqc/modules/cegx/cegx.py
@@ -26,7 +26,6 @@ class MultiqcModule(BaseMultiqcModule):
         data = dict()
         header = ['pos', 'modC', 'unmodC', 'perc_mod', 'coverage']
 
-
         for line in file_content:
             # Get rid of trailing newlines
             line = line.strip("\t")
@@ -37,8 +36,6 @@ class MultiqcModule(BaseMultiqcModule):
 
             linedata = self.parse_line(line, header)
             print(tuple(linedata))
-
-
 
         return data
 
@@ -52,35 +49,5 @@ class MultiqcModule(BaseMultiqcModule):
 
         # convert data into a dict applying header definition
         data = zip(headers, line)
-
-        # # If the percentage is this weird nan, set it to 0
-        # if percentage == "-nan%":
-        #     data["perc_mod"] = 0.0
-        # # Add the count as an integer
-        # data["count"] = int(count)
-        #
-        # # If there is a single value
-        # if len(values) == 1:
-        #     value = values.pop()
-        #     # Is the value a percentage
-        #     if value.endswith("%"):
-        #         data["percentage"] = float(value[:-1])
-        #     # Otherwise, it is an integer
-        #     else:
-        #         data["count"] = int(value)
-        # elif len(values) == 2:
-        #     # If there are multiple values, they are in the format
-        #     # count (percentage%)
-        #     count = values[0]
-        #     percentage = values[1]
-        #
-        #     # The percentage should be in the format:
-        #     # (12.34%) or (100%) or (0%) or (-nan%)
-        #     # So we can remove the bracets first
-        #     percentage = percentage[1:-1]
-        #     # Then we make sure it is one of the supported patterns
-        #     assert re.fullmatch(r"(\d+\.\d+%|\d+%|-nan%)", percentage)
-        #
-        #
 
         return data
