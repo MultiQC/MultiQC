51fe5c781c98dd26a87b61ad86df3bb5ef52de54
James Fellows Yates <jfy133@gmail.com>
2022-07-19 18:31:49 +0200

    parse all data into dictionary(?) object

--- multiqc/modules/porechop/porechop.py
+++ multiqc/modules/porechop/porechop.py
@@ -1,5 +1,6 @@
 from multiqc.modules.base_module import BaseMultiqcModule
 import logging
+import re
 
 log = logging.getLogger(__name__)
 
@@ -14,24 +15,52 @@ class MultiqcModule(BaseMultiqcModule):
             info="A tool for finding and removing adapters from Oxford Nanopore reads.",
         )
 
-        log.info("Hello World!")
-
-        self.find_log_files("porechop")
+        # Find and load Malt reports
+        self.porechop_data = dict()
 
         # Find all files for porechop
         for f in self.find_log_files("porechop", filehandles=True):
             self.parse_logs(f)
+
+        self.porechop_data = self.ignore_samples(self.porechop_data)
+
+        if len(self.porechop_data) == 0:
+            raise UserWarning
+
+        log.info("Found {} reports".format(len(self.porechop_data)))
+
+        # Write data to file
+        self.write_data_file(self.porechop_data, "porechop")
+
+        print(self.porechop_data)
 
     def parse_logs(self, logfile):
 
         file_content = logfile["f"]
         for l in file_content:
             if "Loading reads" in l:
-                print(next(file_content))
-
-        ## SAMPLE NAME
-        ## Loading reads
-        ## test.fastq.gz
+                ## Find line after loading reads, and remove suffixes
+                s_name = next(file_content).rstrip()
+                s_name = self.clean_s_name(s_name, logfile)
+                self.add_data_source(logfile, s_name=s_name)
+                self.porechop_data[s_name] = {}
+            elif "reads loaded" in l:
+                self.porechop_data[s_name]["input_reads"] = {}
+                self.porechop_data[s_name]["input_reads"]["total"] = l.split(" ")[0]
+            elif "from their start" in l:
+                self.porechop_data[s_name]["trimmed_from_start"] = {}
+                self.porechop_data[s_name]["trimmed_from_start"]["trimmed"] = l.split(" ")[0]
+                self.porechop_data[s_name]["trimmed_from_start"]["total"] = l.split(" ")[2]
+                self.porechop_data[s_name]["trimmed_from_start"]["bp"] = l.split(" ")[10].strip("(").replace(",", "")
+            elif "from their end" in l:
+                self.porechop_data[s_name]["trimmed_from_end"] = {}
+                self.porechop_data[s_name]["trimmed_from_end"]["trimmed"] = l.split(" ")[0]
+                self.porechop_data[s_name]["trimmed_from_end"]["total"] = l.split(" ")[2]
+                self.porechop_data[s_name]["trimmed_from_end"]["bp"] = l.split(" ")[10].strip("(").replace(",", "")
+            elif "split based on" in l:
+                self.porechop_data[s_name]["split_middle"] = {}
+                self.porechop_data[s_name]["split_middle"]["trimmed"] = l.split(" ")[0]
+                self.porechop_data[s_name]["split_middle"]["total"] = l.split(" ")[2]
 
         ## DETECTION
         ## Looking for known adapter sets
