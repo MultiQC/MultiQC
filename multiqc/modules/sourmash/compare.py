d7bbf06e5b958fb870a7a05d35897bfb3c6a08e0
Taylor Reiter <taylorreiter@gmail.com>
2022-12-07 10:48:25 -0500

    add compare mqc mod code

---
+++ multiqc/modules/sourmash/compare.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+""" MultiQC module to parse similarity matrix output by sourmash compare """
+
+import logging
+import numpy
+import re
+
+from multiqc.plots import heatmap
+
+# Initialise the logger
+log = logging.getLogger(__name__)
+
+class compare:
+    def parse_compare(self):
+        """
+        Modeled after vcftools relatedness2 module, which also has many samples represented.
+        The alternative would be to have the root of the labels.txt file be ignored
+        """
+        matrices = {}
+        
+        for f in self.find_log_files("sourmash/compare", filehandles=True):
+            m = compare2matrix(f)
+            if m.data and m.comparelabels:
+                 matrices[f["s_name"]] = m
+            self.add_data_source(f, section="compare")
+
+        matrices = self.ignore_samples(matrices)
+
+        if len(matrices) == 0:
+            return 0
+
+        log.info("Found {} valid compare matrices".format(len(matrices)))
+
+        # The matrices cannot be written to a file in their current format
+        # self.write_data_file(matrices, "sourmash_compare")
+
+        helptext = """
+        Sourmash compare outputs a similarity score between two samples. A higher score indicates a higher degree of
+        similarity, up to a maximum of 1. Samples are clustered by similarity on each axis, and specific IDs can be
+        found in the graph with the Highlight tab.
+        """
+
+        idx = 0
+        for name, m in matrices.items():
+            idx += 1
+            self.add_section(
+                name = "compare",
+                anchor = "sourmash-compare-{}".format(idx),
+                description = "**Input:** `{}`.\n\n Heatmap of similarity values from the output of sourmash compare".format(
+                    name
+                ),
+                helptext = helptext,
+                plot = heatmap.plot(
+                     m.data,
+                     xcats = m.comparelabels,
+                     ycats = m.comparelabels,
+                     pconfig = {
+                        "id": "sourmash-compare-heatmap-{}".format(idx),
+                        "title": "sourmash: compare",
+                        "square": True,
+                        "decimalPlaces": 7,
+                     },
+                ),
+            )
+
+        return len(matrices)
+
+
+class compare2matrix:
+    def __init__(self, compare_file):
+        self.data = []
+        self.comparelabels = set()
+
+        self.load_matrix_and_labels(compare_file["f"])
+
+    def load_matrix_and_labels(self, f):
+        """
+        source for first two lines: https://github.com/sourmash-bio/sourmash/blob/9083d20aabcb77c67ba050b727efdd3f5d0a0398/src/sourmash/fig.py#L13
+        """
+        basefile = re.sub(".labels.txt", "", str(f))
+        comparematrix = numpy.load(open(basefile, 'rb'))
+        self.comparelabels = [x.strip() for x in open(basefile + '.labels.txt')] 
+        comparedict = {}
+        for i in range(len(comparelabels)):
+            comparevalues = list(comparematrix[i])
+            res = {self.comparelabels[i]: comparevalues[i] for i in range(len(comparelabels))}
+            comparedict[self.comparelabels[i]] = res
+    
+        # impose alphabetical order and avoid json serialisation errors in utils.report
+        self.comparelabels = sorted(self.comparelabels)
+
+        for x in self.comparelabels:
+            line = []
+            for y in sef.comparelabels:
+                line.append(comparedict[x][y])
+            self.data.append(line)
---
+++ multiqc/modules/sourmash/sourmash.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+""" MultiQC module to parse output from sourmash """
+
+import logging
+
+from multiqc.modules.base_module import BaseMultiqcModule
+from multiqc import config
+
+from .compare import compare
+
+# Initialise the logger
+log = logging.getLogger(__name__)
+
+
+class MultiqcModule(BaseMultiqcModule, compare):
+    def __init__(self):
+        super(MultiqcModule, self).__init__(
+            name="sourmash",
+            anchor="sourmash",
+            href="https://github.com/sourmash-bio/sourmash",
+            info="Quickly search, compare, and analyze genomic and metagenomic data sets.",
+            doi="10.21105/joss.00027",
+        )
+
+        n = dict()
+        n["compare"] = self.parse_compare()
+        if n["compare"] > 0:
+             log.info("Found {} compare reports".format(n["compare"]))
+        
+        if sum(n.values()) == 0:
+            raise UserWarning
