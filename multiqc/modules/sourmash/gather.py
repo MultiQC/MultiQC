ac2573352379ddda6e9601a1c2579fb5d4740f3c
Taylor Reiter <taylorreiter@gmail.com>
2022-12-08 11:41:55 -0500

    rip f_unique_to_query bc it isn't intuitive

--- multiqc/modules/sourmash/gather.py
+++ multiqc/modules/sourmash/gather.py
@@ -38,7 +38,6 @@ class gather:
         log.info("Found {} reports".format(len(self.gather_raw_data)))
 
         # sum counts across all samples, so that we can pick top 5
-        self.gather_total_pct_unique_to_query = dict()
         self.gather_total_pct_unique_weighted = dict()
         #self.sum_sample_counts()
 
@@ -58,9 +57,7 @@ class gather:
                 # loop over all samples and sum different variables 
                 if match_name not in self.gather_total_pct_unique_weighted:
                     self.gather_total_pct_unique_weighted[match_name] = 0
-                    self.gather_total_pct_unique_to_query[match_name] = 0
                 self.gather_total_pct_unique_weighted[match_name] += row["pct_unique_weighted"]
-                self.gather_total_pct_unique_to_query[match_name] += row["pct_unique_to_query"]
 
 
     def general_stats_column(self):
@@ -81,8 +78,6 @@ class gather2data:
             row = {
                 "query_name" : line['query_name'],
                 "match_name" : line['name'],
-                "pct_orig_query" : float(line['f_orig_query']) * 100,
-                "pct_unique_to_query" : float(line['f_unique_to_query']) * 100,
                 "pct_unique_weighted" : float(line['f_unique_weighted']) * 100,
                 "pct_match" : float(line['f_match']) * 100
             }
