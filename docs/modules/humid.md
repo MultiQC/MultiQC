4e05406f16943fa99f8fd2129e1430f3c5001118
Phil Ewels <phil.ewels@seqera.io>
2023-01-01 00:40:01 +0100

    HUMID: Bit of tidying up

--- docs/modules/humid.md
+++ docs/modules/humid.md
@@ -1,10 +1,7 @@
 ---
-
 Name: HUMID
 URL: https://github.com/jfjlaros/dedup
-Description: >
-High-performance UMI Deduplicator
-
+Description: High-performance UMI deduplicator
 ---
 
-HUMID is a tool for reference free (UMI) deduplication
+HUMID is a tool to quickly and easily remove duplicate reads from FastQ files, with or without UMIs.
--- multiqc/modules/humid/humid.py
+++ multiqc/modules/humid/humid.py
@@ -44,13 +44,15 @@ class MultiqcModule(BaseMultiqcModule):
 
     def parse_stat_files(self):
         for f in self.find_log_files("humid", filehandles=True):
-            data = parse_stat_file(f["f"])
-            # There is no sample name in the log, so we use the root of the
-            # file as sample name (since the filename is always stats.dat
-            name = f["root"]
-            f["s_name"] = name
-            self.humid[name] = data
-            self.add_data_source(f)
+            s_name = self.clean_s_name(f["root"], f)
+            data = parse_stat_file(f["f"], s_name)
+            if data:
+                # There is no sample name in the log, so we use the root of the
+                # file as sample name (since the filename is always stats.dat
+                if s_name in self.humid:
+                    log.debug("Duplicate sample name found! Overwriting: {}".format(s_name))
+                self.humid[s_name] = data
+                self.add_data_source(f, s_name)
 
     def add_general_stats(self):
         # Add the number of unique reads (=clusters) to the general statistics
@@ -58,22 +60,27 @@ class MultiqcModule(BaseMultiqcModule):
         data = {k: {"uniq": v["clusters"]} for k, v in self.humid.items()}
         headers = OrderedDict()
         headers["uniq"] = {
-            "title": "Unique reads",
-            "description": "Number of unique reads after UMI deduplication",
-            "format": None,
+            "title": f"{config.read_count_prefix} Unique",
+            "description": f"Number of unique reads after UMI deduplication ({config.read_count_desc})",
+            "shared_key": "read_count",
+            "modify": lambda x: x * config.read_count_multiplier,
         }
         self.general_stats_addcols(data, headers)
 
     def add_humid_section(self):
-        # The values we want to plot (add to the toal number of reads)
+        # The values we want to plot (add to the total number of reads)
         cats = OrderedDict()
-
         cats["clusters"] = {"name": "Unique reads"}
         cats["duplicates"] = {"name": "Duplicate reads"}
         cats["filtered"] = {"name": "Filtered reads"}
 
         # Bargraph configuration
-        config = {"id": "humid-bargraph", "title": "HUMID: Deduplication results", "ylab": "Number of reads"}
+        config = {
+            "id": "humid-bargraph",
+            "title": "HUMID: Deduplication results",
+            "ylab": "Number of reads",
+            "hide_zero_cats": False,
+        }
         self.add_section(
             name="Duplication Summary",
             anchor="humid-section",
@@ -83,31 +90,31 @@ class MultiqcModule(BaseMultiqcModule):
                 shown here.
                 """,
             helptext="""
-                **Unique reads** are the reads that are left over after
-                deduplication.
-                **Duplicate reads** are the reads that were determined to be
-                duplicates of the **Unique reads**.
-                **Filtered reads** were reads that could not be analysed, due to N
-                nucleotides, or because the UMI or sequences were too short to
-                use.
+                - **Unique reads** are the reads that are left over after deduplication.
+                - **Duplicate reads** are the reads that were determined to be duplicates of the **Unique reads**.
+                - **Filtered reads** were reads that could not be analysed, due to N nucleotides, or because the UMI or sequences were too short to use.
                 """,
             plot=bargraph.plot(self.humid, cats, config),
         )
 
 
-def parse_stat_file(fin):
+def parse_stat_file(fin, s_name):
     """Parse the stats file"""
     data = dict()
     for line in fin:
         field, value = line.strip().split(": ")
         data[field] = int(value)
-    process_stats(data)
-    return data
+    if process_stats(data, s_name):
+        return data
 
 
-def process_stats(stats):
+def process_stats(stats, s_name):
     """Process the statistics, to calculate some useful values"""
     stats["filtered"] = stats["total"] - stats["usable"]
     stats["duplicates"] = stats["total"] - stats["clusters"] - stats["filtered"]
     # Sanity check
-    assert stats["duplicates"] + stats["clusters"] + stats["filtered"] == stats["total"]
+    try:
+        assert stats["duplicates"] + stats["clusters"] + stats["filtered"] == stats["total"]
+    except AssertionError:
+        log.warning(f"HUMID stats looked wrong, skipping: {s_name}")
+        return False
