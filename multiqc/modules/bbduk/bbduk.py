826a11e7a903a4b6bd5882bbe45431f9b3745018
Phil Ewels <phil.ewels@seqera.io>
2023-01-08 00:33:19 +0100

    BBDuk: Code clean up

--- multiqc/modules/bbduk/bbduk.py
+++ multiqc/modules/bbduk/bbduk.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
 """ Module to parse output from BBDuk """
-from __future__ import print_function
 
 import logging
 from collections import OrderedDict
@@ -22,7 +20,7 @@ class MultiqcModule(BaseMultiqcModule):
             name="BBDuk",
             anchor="bbduk",
             href="https://jgi.doe.gov/data-and-tools/software-tools/bbtools/bb-tools-user-guide/bbduk-guide/",
-            info="""is a tool performing common data-quality-related trimming, 
+            info="""is a tool performing common data-quality-related trimming,
 			filtering, and masking operations with a kmer based approach""",
             ## One publication, but only for the merge tool:
             # doi="10.1371/journal.pone.0185056",
@@ -48,21 +46,27 @@ class MultiqcModule(BaseMultiqcModule):
         self.bbduk_bargraph_plot()
         self.bbduk_beeswarm_plot()
 
-    def parse_logs(self, logfile):
+    def parse_logs(self, f):
         """Parses a BBDuk stdout saved in a file"""
+
+        ## Parsing helper functions
+        def grab_reads(l):
+            """Extracts read counts from STDOUT entry"""
+            return l.split(":")[1].lstrip().split(" ")[0]
+
+        def grab_perc(l):
+            """Extracts percent from STDOUT entry"""
+            return l.split(":")[1].lstrip().split(" ")[2].strip("(|)|%")
+
         ## Assume take from the name of the file as the includes pair names,
         ## which we can't 'collapse' into a single one.
-        s_name = logfile["fn"]
-        s_name = self.clean_s_name(s_name, logfile)
+        if f["s_name"] in self.bbduk_data:
+            log.warn("Duplicate sample name found based on filename! Overwriting: {}".format(f["s_name"]))
+        self.bbduk_data[f["s_name"]] = {}
 
-        if self.bbduk_data.get(s_name) is not None:
-            log.warn("Duplicate sample name found based on filename! Overwriting: {}".format(s_name))
+        self.add_data_source(f)
 
-        self.bbduk_data[s_name] = {}
-        self.add_data_source(logfile, s_name=s_name)
-        file_content = logfile["f"]
-
-        for l in file_content:
+        for l in f["f"]:
             ## Find line after loading reads, and remove suffixes for sample name
 
             for cat in [
@@ -75,10 +79,10 @@ class MultiqcModule(BaseMultiqcModule):
                 "Result",
             ]:
                 if cat in l:
-                    self.bbduk_data[s_name][cat + " reads"] = int(grab_reads(l))
-                    self.bbduk_data[s_name][cat + " percent"] = float(grab_perc(l))
+                    self.bbduk_data[f["s_name"]][cat + " reads"] = int(grab_reads(l))
+                    self.bbduk_data[f["s_name"]][cat + " percent"] = float(grab_perc(l))
                 elif "Input:" in l:
-                    self.bbduk_data[s_name]["Input reads"] = int(grab_reads(l))
+                    self.bbduk_data[f["s_name"]]["Input reads"] = int(grab_reads(l))
 
     def bbduk_general_stats(self):
         """BBDuk read counts for general stats"""
@@ -86,14 +90,14 @@ class MultiqcModule(BaseMultiqcModule):
 
         headers["Input reads"] = {
             "title": "Total Input Reads ({})".format(config.read_count_prefix),
-            "description": "Total number of input reads to BBDuk ({})".format(config.read_count_prefix),
+            "description": "Total number of input reads to BBDuk ({})".format(config.read_count_desc),
             "scale": "Greens",
             "shared_key": "read_count",
             "modify": lambda x: x * config.read_count_multiplier,
         }
         headers["QTrimmed reads"] = {
             "title": "QTrimmed Reads ({})".format(config.read_count_prefix),
-            "description": "QTrimmed Reads removed ({})".format(config.read_count_prefix),
+            "description": "QTrimmed Reads removed ({})".format(config.read_count_desc),
             "scale": "Greens",
             "shared_key": "read_count",
             "modify": lambda x: x * config.read_count_multiplier,
@@ -107,7 +111,7 @@ class MultiqcModule(BaseMultiqcModule):
         }
         headers["KTrimmed reads"] = {
             "title": "KTrimmed Reads ({})".format(config.read_count_prefix),
-            "description": "KTrimmed Reads removed ({})".format(config.read_count_prefix),
+            "description": "KTrimmed Reads removed ({})".format(config.read_count_desc),
             "scale": "Greens",
             "shared_key": "read_count",
             "modify": lambda x: x * config.read_count_multiplier,
@@ -121,7 +125,7 @@ class MultiqcModule(BaseMultiqcModule):
         }
         headers["Trimmed by overlap reads"] = {
             "title": "Trimmed by overlap reads ({})".format(config.read_count_prefix),
-            "description": "Reads removed trimmed by overlap ({})".format(config.read_count_prefix),
+            "description": "Reads removed trimmed by overlap ({})".format(config.read_count_desc),
             "scale": "Greens",
             "shared_key": "read_count",
             "modify": lambda x: x * config.read_count_multiplier,
@@ -135,7 +139,7 @@ class MultiqcModule(BaseMultiqcModule):
         }
         headers["Low quality discards reads"] = {
             "title": "Low quality discards Reads ({})".format(config.read_count_prefix),
-            "description": "Reads removed by the Low quality discards filter ({})".format(config.read_count_prefix),
+            "description": "Reads removed by the Low quality discards filter ({})".format(config.read_count_desc),
             "scale": "Greens",
             "shared_key": "read_count",
             "modify": lambda x: x * config.read_count_multiplier,
@@ -149,7 +153,7 @@ class MultiqcModule(BaseMultiqcModule):
         }
         headers["Low entropy discards reads"] = {
             "title": "Low quality discards Reads ({})".format(config.read_count_prefix),
-            "description": "Reads removed by the Low entropy discards filter ({})".format(config.read_count_prefix),
+            "description": "Reads removed by the Low entropy discards filter ({})".format(config.read_count_desc),
             "scale": "Greens",
             "shared_key": "read_count",
             "modify": lambda x: x * config.read_count_multiplier,
@@ -163,7 +167,7 @@ class MultiqcModule(BaseMultiqcModule):
         }
         headers["Total Removed reads"] = {
             "title": "Total Reads Removed ({})".format(config.read_count_prefix),
-            "description": "Total Reads removed ({})".format(config.read_count_prefix),
+            "description": "Total Reads removed ({})".format(config.read_count_desc),
             "scale": "Greens",
             "shared_key": "read_count",
             "modify": lambda x: x * config.read_count_multiplier,
@@ -177,7 +181,7 @@ class MultiqcModule(BaseMultiqcModule):
         }
         headers["Result reads"] = {
             "title": "Remaining Reads ({})".format(config.read_count_prefix),
-            "description": "Remaining Reads removed ({})".format(config.read_count_prefix),
+            "description": "Remaining Reads removed ({})".format(config.read_count_desc),
             "scale": "Greens",
             "shared_key": "read_count",
             "modify": lambda x: x * config.read_count_multiplier,
@@ -194,15 +198,15 @@ class MultiqcModule(BaseMultiqcModule):
 
     def bbduk_bargraph_plot(self):
         """Bargraph summarising reported filtered reads by BBduk"""
-        cats = OrderedDict()
-
-        for cat in [
+        cats = [
             "Total Removed reads",
             "Result reads",
-        ]:
-            cats[cat] = {"name": cat}
-
-        config = {"id": "bbduk-bargraph", "title": "BBDuk: Percentage Summary", "ylab": "Percentage of Reads"}
+        ]
+        config = {
+            "id": "bbduk-bargraph",
+            "title": "BBDuk: Percentage Summary",
+            "ylab": "Percentage of Reads",
+        }
 
         self.add_section(
             name="Filtered Reads Percentages",
@@ -243,16 +247,3 @@ class MultiqcModule(BaseMultiqcModule):
             description="Shows the number of reads removed by various BBDuk filters",
             plot=beeswarm.plot(self.bbduk_data, headers, {"id": "bbduk-beeswarm-reads"}),
         )
-
-
-## Parsing helper functions
-
-
-def grab_reads(l):
-    """Extracts read counts from STDOUT entry"""
-    return l.split(":")[1].lstrip().split(" ")[0]
-
-
-def grab_perc(l):
-    """Extracts percent from STDOUT entry"""
-    return l.split(":")[1].lstrip().split(" ")[2].strip("(|)|%")
