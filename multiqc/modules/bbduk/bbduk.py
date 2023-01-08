458a21775974eac19b4d079946d79a382288fe21
Phil Ewels <phil.ewels@seqera.io>
2023-01-08 01:05:55 +0100

    Report bases as well as reads

--- multiqc/modules/bbduk/bbduk.py
+++ multiqc/modules/bbduk/bbduk.py
@@ -80,7 +80,7 @@ class MultiqcModule(BaseMultiqcModule):
                     matches = re.search(f"{cat}:\s+(\d+) reads \(([\d\.]+)%\)\s+(\d+) bases \(([\d\.]+)%\)", l)
                     if matches:
                         self.bbduk_data[s_name][cat + " reads"] = int(matches.group(1))
-                        self.bbduk_data[s_name][cat + " percent"] = float(matches.group(2))
+                        self.bbduk_data[s_name][cat + " reads percent"] = float(matches.group(2))
                         self.bbduk_data[s_name][cat + " bases"] = int(matches.group(3))
                         self.bbduk_data[s_name][cat + " bases percent"] = float(matches.group(4))
                         break
@@ -91,14 +91,28 @@ class MultiqcModule(BaseMultiqcModule):
         """BBDuk read counts for general stats"""
         headers = OrderedDict()
 
-        headers["Total Removed percent"] = {
-            "title": "Total Reads Removed (%)",
-            "description": "Percentage of reads removed after filtering(%)",
+        headers["Total Removed bases percent"] = {
+            "title": "Bases Removed (%)",
+            "description": "Percentage of bases removed after filtering",
+            "scale": "YlOrBr",
+            "max": 100,
+        }
+        headers["Total Removed bases"] = {
+            "title": "Bases Removed ({})".format(config.base_count_prefix),
+            "description": "Total Bases removed ({})".format(config.base_count_desc),
+            "scale": "Reds",
+            "shared_key": "base_count",
+            "modify": lambda x: x * config.base_count_multiplier,
+            "hidden": True,
+        }
+        headers["Total Removed reads percent"] = {
+            "title": "Reads Removed (%)",
+            "description": "Percentage of reads removed after filtering",
             "scale": "OrRd",
             "max": 100,
         }
         headers["Total Removed reads"] = {
-            "title": "Total Reads Removed ({})".format(config.read_count_prefix),
+            "title": "Reads Removed ({})".format(config.read_count_prefix),
             "description": "Total Reads removed ({})".format(config.read_count_desc),
             "scale": "Reds",
             "shared_key": "read_count",
@@ -124,17 +138,21 @@ class MultiqcModule(BaseMultiqcModule):
         assume there is additional default filtering carried out)
         """
         cats = [
-            "Result reads",
-            "QTrimmed reads",
-            "KTrimmed reads",
-            "Trimmed by overlap reads",
-            "Low quality discards reads",
-            "Low entropy discards reads",
+            "Result",
+            "QTrimmed",
+            "KTrimmed",
+            "Trimmed by overlap",
+            "Low quality discards",
+            "Low entropy discards",
         ]
         pconfig = {
             "id": "bbduk-filtered-barplot",
-            "title": "BBDuk: Percentage Summary",
-            "ylab": "Percentage of Reads",
+            "title": "BBDuk: Filtered reads",
+            "ylab": "Number of Reads",
+            "data_labels": [
+                {"name": "Reads", "ylab": "Number of Reads"},
+                {"name": "Bases", "ylab": "Number of Base Pairs"},
+            ],
         }
 
         self.add_section(
@@ -142,8 +160,11 @@ class MultiqcModule(BaseMultiqcModule):
             anchor="bbduk-filtered-reads",
             description="The number of reads removed by various BBDuk filters",
             plot=bargraph.plot(
-                self.bbduk_data,
-                cats,
+                [self.bbduk_data, self.bbduk_data],
+                [
+                    [f"{cat} reads" for cat in cats],
+                    [f"{cat} bases" for cat in cats],
+                ],
                 pconfig,
             ),
         )
