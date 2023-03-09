19ee8dcee30b67796a08181e3021716b403fd10d
Lucas Taniguti <lucas.biotec07@gmail.com>
2023-03-09 20:06:26 -0300

    feat: represented with a plot instead of a table

--- multiqc/modules/haplocheck/haplocheck.py
+++ multiqc/modules/haplocheck/haplocheck.py
@@ -3,7 +3,7 @@ import logging
 from collections import OrderedDict
 
 from multiqc.modules.base_module import BaseMultiqcModule
-from multiqc.plots import table
+from multiqc.plots import bargraph, table
 
 log = logging.getLogger(__name__)
 
@@ -14,7 +14,10 @@ class MultiqcModule(BaseMultiqcModule):
             name="Haplocheck",
             anchor="haplocheck",
             href="https://github.com/genepi/haplocheck",
-            info="Haplocheck detects contamination patterns in mtDNA AND WGS sequencing studies by analyzing the mitochondrial DNA.",
+            info=(
+                "Haplocheck detects contamination patterns in mtDNA AND WGS sequencing studies by analyzing "
+                "the mitochondrial DNA."
+            ),
             doi="10.1101/gr.256545.119",
         )
         log_files = self.find_log_files("haplocheck", filehandles=True)
@@ -24,9 +27,60 @@ class MultiqcModule(BaseMultiqcModule):
             self.add_data_source(f)
 
         self._samples = samples
-        self.add_section(plot=table.plot(data=self._samples, headers=self._setup_headers()))
+
+        self._add_table(samples)
+        self._add_coverage_plot(samples)
+        self._add_heteroplasmy_level_plot(samples)
+
         self.write_data_file(self._samples, "multiqc_haplocheck")
         log.info("Found {} samples".format(len(samples)))
+
+    def _add_table(self, samples):
+        self.add_section(
+            description=(
+                "For further information regarding the output values, refer to the "
+                "[tool's documentation](https://mitoverse.readthedocs.io/haplocheck/haplocheck/#textual-report-file)."
+            ),
+            plot=table.plot(data=samples, headers=self._setup_headers()),
+        )
+
+    def _add_coverage_plot(self, samples):
+        coverage_plot = bargraph.plot(
+            data=samples,
+            cats=["Sample Coverage"],
+            pconfig={
+                "id": "haplocheck-coverage",
+                "title": "Haplocheck: coverages",
+                "xlab": "Sample",
+                "ylab": "Average Coverage",
+                "tt_suffix": "x",
+            },
+        )
+        self.add_section(
+            name="Average coverage per sample",
+            anchor="haplocheck-coverage-id",
+            description="Average coverage per sample",
+            plot=coverage_plot,
+        )
+
+    def _add_heteroplasmy_level_plot(self, samples):
+        coverage_plot = bargraph.plot(
+            data=samples,
+            cats=["Major Heteroplasmy Level", "Minor Heteroplasmy Level"],
+            pconfig={
+                "id": "haplocheck-het-level",
+                "title": "Haplocheck: Heteroplasmy Level",
+                "xlab": "Sample",
+                "ylab": "Heteroplasmy Level",
+                "tt_decimals": 2,
+            },
+        )
+        self.add_section(
+            name="Heteroplasmy level per sample",
+            anchor="haplocheck-het-level-id",
+            description="Heteroplasmy level per sample",
+            plot=coverage_plot,
+        )
 
     def _setup_headers(self):
         headers = OrderedDict()
@@ -35,11 +89,14 @@ class MultiqcModule(BaseMultiqcModule):
 
         headers["Contamination Status"] = {
             "title": "Contamination Status",
+            "description": "This column can either be YES, NO or ND (not detectable)",
             "hidden": False,
             "bgcols": {"YES": "#f8d7da", "ND": "#fff3cd", "NO": "#d1e7dd"},
         }
         headers["Contamination Level"] = {
             "title": "Contamination Level",
+            "description": "The detected contamination level for the sample",
+            "bgcols": {"ND": "#d1e7dd"},
             "hidden": False,
         }
         headers["Major Heteroplasmy Level"] = {
@@ -47,7 +104,7 @@ class MultiqcModule(BaseMultiqcModule):
             "max": 1,
             "min": 0,
             "format": "{:,.3f}",
-            "hidden": False,
+            "hidden": True,
         }
 
         headers["Minor Heteroplasmy Level"] = {
@@ -55,7 +112,7 @@ class MultiqcModule(BaseMultiqcModule):
             "max": 1,
             "min": 0,
             "format": "{:,.3f}",
-            "hidden": False,
+            "hidden": True,
         }
         headers["Major Haplogroup"] = {
             "title": "Major Haplogroup",
@@ -63,6 +120,7 @@ class MultiqcModule(BaseMultiqcModule):
         }
         headers["Minor Haplogroup"] = {
             "title": "Minor Haplogroup",
+            "description": "Only consider it when contamination is detected.",
             "hidden": False,
         }
         return headers
