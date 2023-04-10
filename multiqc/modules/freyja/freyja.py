46f96e2cebad3f4fec56df8ac389e7383d287556
Joon-Klaps <joonklaps@gmail.com>
2023-04-10 20:37:48 +0200

    Step 2 part done, data parse for freyja needed

--- multiqc/modules/freyja/freyja.py
+++ multiqc/modules/freyja/freyja.py
@@ -16,19 +16,58 @@ class MultiqcModule(BaseMultiqcModule):
     def __init__(self):
         # Initialise the parent object
         super(MultiqcModule, self).__init__(
-          name='Freyja',
-          anchor='freyja',
-          href="https://github.com/andersen-lab/Freyja",
-          info="Recover relative lineage abundances from mixed SARS-CoV-2 samples."
+            name='Freyja',
+            anchor='freyja',
+            href="https://github.com/andersen-lab/Freyja",
+            info="Recover relative lineage abundances from mixed SARS-CoV-2 samples."
         )
 
-    # # To store the summary data
-    #     self.freyja = dict()
+        # To store the summary data
+        self.freyja_data = dict()
 
-    #     # Parse the output files
-    #     self.parse_stat_files()
+        # Parse the output files
+        self.parse_stat_files()
 
-    #     # Remove filtered samples
-    #     self.freyja = self.ignore_samples(self.freyja)
+        # Remove filtered samples
+        self.freyja_data = self.ignore_samples(self.freyja_data)
 
-    myfile = self.find_log_files('freyja', filehandles=True)
+        # Let MultiQC know this module found no data
+        if len(self.freyja_data) == 0:
+            raise UserWarning
+
+        log.info(f"Found {len(self.freyja_data)} reports")
+        self.write_data_file(self.freyja_data, "multiqc_freyja")
+
+    def parse_stat_files(self, f):    
+        for f in self.find_log_files('freyja'):
+            s_name = self.clean_s_name(f["root"], f)
+            data = parse_stat_file(f["f"], s_name)
+            if data:
+                # There is no sample name in the log, so we use the root of the
+                # file as sample name (since the filename is always stats.dat
+                if s_name in self.freyja_data:
+                    log.debug("Duplicate sample name found! Overwriting: {}".format(s_name))
+                self.freyja_data[s_name] = data
+                self.add_data_source(f, s_name)
+                
+# TODO: This needs to be customised for Freyja        
+def parse_stat_file(fin, s_name):
+    """Parse the stats file"""
+    data = dict()
+    for line in fin:
+        field, value = line.strip().split(": ")
+        data[field] = int(value)
+    if process_stats(data, s_name):
+        return data
+
+# TODO: This needs to be customised for Freyja    
+def process_stats(stats, s_name):
+    """Process the statistics, to calculate some useful values"""
+    stats["filtered"] = stats["total"] - stats["usable"]
+    stats["duplicates"] = stats["total"] - stats["clusters"] - stats["filtered"]
+    # Sanity check
+    try:
+        assert stats["duplicates"] + stats["clusters"] + stats["filtered"] == stats["total"]
+    except AssertionError:
+        log.warning(f"HUMID stats looked wrong, skipping: {s_name}")
+        return False
