1981e5cf71eff67d8c02ee463d0e5575bf272ae0
Joon-Klaps <joonklaps@gmail.com>
2023-04-13 11:48:09 +0200

    fixing linting

--- multiqc/modules/freyja/freyja.py
+++ multiqc/modules/freyja/freyja.py
@@ -63,7 +63,6 @@ class MultiqcModule(BaseMultiqcModule):
                         summarized_line = summarized_line.strip().split('\t')[1]
                         d = eval(summarized_line) # Make sure no input is corrupted and does not contain any malicious code
                         d = dict(d)
-                        log.info(f"Adding top variant for {s_name}, {d}")
                 except ValueError:
                     pass
             
@@ -79,39 +78,43 @@ class MultiqcModule(BaseMultiqcModule):
             self.add_data_source(f, s_name)
 
     def general_stats_cols(self):
-        """Add a single column displaying the most abundant variant to the General Statistics table"""
-        top_variant_dict= {}
+        """Add a single column displaying the most abundant lineage to the General Statistics table"""
+        top_lineage_dict= {}
         for s_name, sub_dict in self.freyja_data.items():
-            top_variant = max(sub_dict, key=sub_dict.get)
-            top_variant_value = sub_dict[top_variant]
-            top_variant_dict[s_name] = {
-                "Top variant": top_variant,
-                "Top variant %": top_variant_value
+            top_lineage = max(sub_dict, key=sub_dict.get)
+            top_lineage_value = sub_dict[top_lineage]
+            top_lineage_dict[s_name] = {
+                "Top_lineage_freyja": top_lineage,
+                "Top_lineage_freyja_percentage": top_lineage_value
             }
         
         headers = OrderedDict()
-        headers['Top variant'] = {
-            'title': 'Top variant',
-            'description': 'The most abundant variant in the sample',
+        headers['Top_lineage_freyja'] = {
+            'title': 'Top lineage (Freyja)',
+            'description': 'The most abundant lineage in the sample',
             'scale': 'RdYlGn-rev' # Not sure if this is the best scale
         }
-        headers['Top variant %'] = {
-            'title': 'Top variant %',
-            'description': 'The percentage of the most abundant variant in the sample',
+        headers['Top_lineage_freyja_percentage'] = {
+            'title': 'Top lineage (Freyja) %',
+            'description': 'The percentage of the most abundant lineage in the sample',
             'max': 100,
             'min': 0,
             'scale': 'Blues',
+            "modify": lambda x: x * 100,
             'suffix': '%'
         }
 
-        self.general_stats_addcols(top_variant_dict, headers)
+        self.general_stats_addcols(top_lineage_dict, headers)
 
     def add_freyja_section(self):
         cats = list()
         pconfig = {
-            "id": "Freyja plot",
-            "title": "Freyja: Top variants",
-            "ylab": "relative abundance"
+            "id": "Freyja_plot",
+            "title": "Freyja: Top lineages",
+            "ylab": "relative abundance",
+            "yCeiling": 1,
+            'cpswitch': False,
+            'cpswitch_c_active': False
         }
 
         rank_cats = OrderedDict()
@@ -123,12 +126,14 @@ class MultiqcModule(BaseMultiqcModule):
             name="Freyja Summary",
             anchor="freyja-summary",
             description="""
-                Relative lineage abundances from mixed SARS-CoV-2 samples. 
+                Relative lineage abundances from mixed SARS-CoV-2 samples. Hover over the column headers for descriptions and click _Help_ for more in-depth documentation. 
                 """,
             helptext="""
-                The graph denotes a sum of all lineage abundances in a particular WHO designation, otherwise they are grouped into "Other".
-                lineages abundances are calculated as the number of reads that are assigned to a particular lineage. 
+                The graph denotes a sum of all lineage abundances in a particular WHO designation , otherwise they are grouped into "Other".
+                Lineages abundances are calculated as the number of reads that are assigned to a particular lineage. 
                 Lineages and their corresponding abundances are summarized by constellation. 
+
+                > **Note**: Lineage designation is based on the used WHO nomenclature, which could vary over time. 
                 """,
             plot=bargraph.plot(self.freyja_data, None, pconfig),
         )
