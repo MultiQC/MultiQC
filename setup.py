e52b2e99ebddf40da1045277a66328295bb8a495
Yuhe Cheng <57512009+YuheCheng62@users.noreply.github.com>
2023-08-21 14:36:16 -0700

    Update setup.py
    Co-authored-by: Phil Ewels <phil.ewels@seqera.io>

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
         "requests",
         "rich>=10",
         "rich-click",
-        "spectra>=0.0.10"
+        "spectra>=0.0.10",
     ],
     entry_points={
         "console_scripts": [
