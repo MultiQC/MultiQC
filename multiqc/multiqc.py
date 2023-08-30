37eb2968083df715256d27e8b0da6b915ea06fb1
Pontus Höjer <pontushojer@gmail.com>
2023-08-30 11:59:22 +0200

    add option to exclude software version section

--- multiqc/multiqc.py
+++ multiqc/multiqc.py
@@ -246,6 +246,12 @@ click.rich_click.OPTION_GROUPS = {
     help="Don't upload generated report to MegaQC, even if MegaQC options are found",
 )
 @click.option(
+    "--no-versions-section",
+    "no_versions_section",
+    is_flag=True,
+    help="Exclude section 'Software Versions' listing software versions from the report",
+)
+@click.option(
     "-c",
     "--config",
     "config_file",
@@ -322,6 +328,7 @@ def run(
     lint=False,
     make_pdf=False,
     no_megaqc_upload=False,
+    no_versions_section=False,
     config_file=(),
     cl_config=(),
     verbose=0,
@@ -761,7 +768,7 @@ def run(
         report.software_versions[software] = versions
 
     # Add section for software versions if any are found
-    if report.software_versions and len(report.modules_output) > 0:
+    if not no_versions_section and report.software_versions and len(report.modules_output) > 0:
         report.modules_output.append(software_versions.MultiqcModule())
 
     # Special-case module if we want to profile the MultiQC running time
