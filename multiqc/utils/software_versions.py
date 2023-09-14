c78c7c49131cd0cfdb6bcdbfd1b5915ee97af243
Phil Ewels <phil.ewels@seqera.io>
2023-09-14 12:00:57 +0200

    Only show 'Group' column if it's not identical to 'Software'

--- multiqc/utils/software_versions.py
+++ multiqc/utils/software_versions.py
@@ -30,22 +30,7 @@ class MultiqcModule(BaseMultiqcModule):
         )
 
         self.report_software_versions()
-
-        # Get rid of the default dicts and Version objects
-        flat_software_versions = {
-            group: {
-                software: [str(ver) for ver in software_versions] for software, software_versions in versions.items()
-            }
-            for group, versions in mqc_report.software_versions.items()
-        }
-        # TSV only allows 2 levels of nesting.
-        if mqc_config.data_format == "tsv":
-            flat_software_versions = {
-                group: {software: ", ".join(software_versions) for software, software_versions in versions.items()}
-                for group, versions in flat_software_versions.items()
-            }
-        # Write to a file for downstream use
-        util_functions.write_data_file(flat_software_versions, "multiqc_software_versions")
+        self.write_software_versions_data_file()
 
     def report_software_versions(self):
         """Create section listing software versions."""
@@ -56,7 +41,20 @@ class MultiqcModule(BaseMultiqcModule):
     def _make_versions_html(versions):
         """Generate a tabular HTML output of all versions."""
         table_id = mqc_report.save_htmlid("mqc_versions_table")
-        # source: https://github.com/nf-core/rnaseq/blob/3bec2331cac2b5ff88a1dc71a21fab6529b57a0f/modules/nf-core/custom/dumpsoftwareversions/templates/dumpsoftwareversions.py#L12
+
+        # Check if the Group column is identical to Software column
+        groups_rows = []
+        software_rows = []
+        for group, tmp_versions in sorted(versions.items()):
+            for tool, _ in sorted(tmp_versions.items()):
+                groups_rows.append(group)
+                software_rows.append(tool)
+        ignore_groups = groups_rows == software_rows
+
+        # Based on: https://github.com/nf-core/rnaseq/blob/3bec2331cac2b5ff88a1dc71a21fab6529b57a0f/modules/nf-core/custom/dumpsoftwareversions/templates/dumpsoftwareversions.py#L12
+        header_rows = ["<th>Software</th>", "<th>Version</th>"]
+        if not ignore_groups:
+            header_rows.insert(0, f"<th>{mqc_config.versions_table_group_header}</th>")
         html = [
             dedent(
                 f"""\
@@ -65,11 +63,7 @@ class MultiqcModule(BaseMultiqcModule):
                 </button>
                 <table class="table mqc_versions_table" id="{table_id}">
                     <thead>
-                        <tr>
-                            <th>{mqc_config.versions_table_group_header}</th>
-                            <th>Software</th>
-                            <th>Version</th>
-                        </tr>
+                        <tr>{''.join(header_rows)}</tr>
                     </thead>
                 """
             )
@@ -77,21 +71,33 @@ class MultiqcModule(BaseMultiqcModule):
         for group, tmp_versions in sorted(versions.items()):
             html.append("<tbody>")
             for i, (tool, versions) in enumerate(sorted(tmp_versions.items())):
-                versions = list(map(str, versions))
-                html.append(
-                    dedent(
-                        f"""\
-                        <tr>
-                            <td><samp>{group if (i == 0) else ''}</samp></td>
-                            <td><samp>{tool}</samp></td>
-                            <td><samp>{', '.join(versions)}</samp></td>
-                        </tr>
-                        """
-                    )
-                )
+                rows = [
+                    f"<td><samp>{tool}</samp></td>",
+                    f"<td><samp>{', '.join(list(map(str, versions)))}</samp></td>",
+                ]
+                if not ignore_groups:
+                    rows.insert(0, f"<td><samp>{group if (i == 0) else ''}</samp></td>")
+                html.append(f"<tr>{''.join(rows)}</tr>")
             html.append("</tbody>")
         html.append("</table>")
         return "\n".join(html)
+
+    @staticmethod
+    def write_software_versions_data_file():
+        """Write software versions to a file for downstream use."""
+        # Get rid of the default dicts and Version objects
+        flat_software_versions = {
+            group: {software: list(map(str, software_versions)) for software, software_versions in versions.items()}
+            for group, versions in mqc_report.software_versions.items()
+        }
+        # TSV only allows 2 levels of nesting.
+        if mqc_config.data_format == "tsv":
+            flat_software_versions = {
+                group: {software: ", ".join(software_versions) for software, software_versions in versions.items()}
+                for group, versions in flat_software_versions.items()
+            }
+        # Write to a file for downstream use
+        util_functions.write_data_file(flat_software_versions, "multiqc_software_versions")
 
 
 def update_versions_from_config(config, report):
