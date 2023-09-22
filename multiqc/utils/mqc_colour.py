4ceffe281a8975f3edae81adaf65dd42d5819f7b
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2023-09-22 15:16:21 +0200

    Move license comment about color up

--- multiqc/utils/mqc_colour.py
+++ multiqc/utils/mqc_colour.py
@@ -19,6 +19,20 @@ logger = logging.getLogger(__name__)
 class mqc_colour_scale(object):
     """Class to hold a colour scheme."""
 
+    # ColorBrewer colours, taken from Chroma.js source code
+    # https://github.com/gka/chroma.js
+    ###    Copyright (c) 2002 Cynthia Brewer, Mark Harrower, and The
+    ###    Pennsylvania State University.
+    ###
+    ###    Licensed under the Apache License, Version 2.0 (the "License");
+    ###    you may not use this file except in compliance with the License.
+    ###    You may obtain a copy of the License at
+    ###    http://www.apache.org/licenses/LICENSE-2.0
+    ###
+    ###    Unless required by applicable law or agreed to in writing, software distributed
+    ###    under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+    ###    CONDITIONS OF ANY KIND, either express or implied. See the License for the
+    ###    specific language governing permissions and limitations under the License.
     COLORBREWER_SCALES = {
         # sequential
         "OrRd": ["#fff7ec", "#fee8c8", "#fdd49e", "#fdbb84", "#fc8d59", "#ef6548", "#d7301f", "#b30000", "#7f0000"],
@@ -391,22 +405,6 @@ class mqc_colour_scale(object):
                Defaults to 'GnBu' if scale not found.
         Returns: List of hex colours
         """
-        # ColorBrewer colours, taken from Chroma.js source code
-        # https://github.com/gka/chroma.js
-
-        ###    Copyright (c) 2002 Cynthia Brewer, Mark Harrower, and The
-        ###    Pennsylvania State University.
-        ###
-        ###    Licensed under the Apache License, Version 2.0 (the "License");
-        ###    you may not use this file except in compliance with the License.
-        ###    You may obtain a copy of the License at
-        ###    http://www.apache.org/licenses/LICENSE-2.0
-        ###
-        ###    Unless required by applicable law or agreed to in writing, software distributed
-        ###    under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-        ###    CONDITIONS OF ANY KIND, either express or implied. See the License for the
-        ###    specific language governing permissions and limitations under the License.
-
         if name.startswith("#"):
             return [name]
 
