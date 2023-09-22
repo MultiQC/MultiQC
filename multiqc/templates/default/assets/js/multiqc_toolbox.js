9d876f1dba4ca99a328813b61657315c9f5259e5
Vlad Savelyev <vladislav.sav@gmail.com>
2023-09-22 11:22:10 +0200

    Update multiqc/templates/default/assets/js/multiqc_toolbox.js
    Co-authored-by: Phil Ewels <phil.ewels@seqera.io>

--- multiqc/templates/default/assets/js/multiqc_toolbox.js
+++ multiqc/templates/default/assets/js/multiqc_toolbox.js
@@ -4,7 +4,7 @@
 
 var mqc_colours_idx = 0;
 var mqc_colours = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#ffff33", "#a65628", "#f781bf", "#999999"];
-var zip_threshold = 1;
+var zip_threshold = 8;
 
 //////////////////////////////////////////////////////
 // TOOLBOX LISTENERS
