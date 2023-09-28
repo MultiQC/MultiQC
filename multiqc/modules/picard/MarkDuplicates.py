028a087a3cc8ff7c21fcedee672ec34b157e36b9
Phil Ewels <phil.ewels@seqera.io>
2023-09-29 00:57:21 +0200

    Update multiqc/modules/picard/MarkDuplicates.py

--- multiqc/modules/picard/MarkDuplicates.py
+++ multiqc/modules/picard/MarkDuplicates.py
@@ -90,7 +90,7 @@ def parse_reports(
             #
             if "markduplicates" in l.lower() and "input" in l.lower():
                 # Pull sample name from input
-                fn_search = re.search(r"INPUT(?:=|\s+)(\[?[^\s]+]?)", l, flags=re.IGNORECASE)
+                fn_search = re.search(r"INPUT(?:=|\s+)(\[?[^\s]+\]?)", l, flags=re.IGNORECASE)
                 if fn_search:
                     s_name = os.path.basename(fn_search.group(1).strip("[]"))
                     s_name = self.clean_s_name(s_name, f)
