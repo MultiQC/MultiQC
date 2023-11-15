21d75bf8fe43edbb23cea1ec94066dd99a54dbe9
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2023-11-15 18:29:14 +0100

    Fix for py38

--- multiqc/utils/config.py
+++ multiqc/utils/config.py
@@ -2,8 +2,6 @@
 
 """ MultiQC config module. Holds a single copy of
 config variables to be used across all other modules """
-from types import NoneType
-
 from typing import List, Dict, Optional
 
 import inspect
@@ -305,7 +303,7 @@ def mqc_env_vars_config() -> Dict:
                 except ValueError:
                     logger.warning(f"Could not parse a float value from the environment variable ${k}={v}")
                     continue
-            elif not isinstance(globals()[conf_key], (str, NoneType)):
+            elif not isinstance(globals()[conf_key], str) and globals()[conf_key] is not None:
                 logger.warning(
                     f"Can only set scalar config entries (str, int, float, bool) with environment variable, "
                     f"but config.{conf_key} expects a type '{type(globals()[conf_key]).__name__}'. Ignoring ${k}"
