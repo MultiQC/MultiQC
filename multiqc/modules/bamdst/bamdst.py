72891e4d2149818eb467fe158d2450e20921d50e
Vlad Savelyev <vladislav.sav@gmail.com>
2023-12-17 11:45:47 +0100

    Update multiqc/modules/bamdst/bamdst.py
    Co-authored-by: Phil Ewels <phil.ewels@seqera.io>

--- multiqc/modules/bamdst/bamdst.py
+++ multiqc/modules/bamdst/bamdst.py
@@ -114,7 +114,8 @@ class MultiqcModule(BaseMultiqcModule):
                 # Cannot split by spaces because the file names can contain spaces as well
                 path_line = line.split(":")[1].strip().replace(".bam", "<EXT>").replace(".cram", "<EXT>")
                 names = [self.clean_s_name(sn) for sn in path_line.split("<EXT>") if sn]
-                f["s_name"] = "-".join(names)  # concatenating file names
+                if names:
+                    f["s_name"] = "-".join(names)  # concatenating file names
                 continue
             if line.startswith("## Version : "):
                 version = line.split(":")[1].strip()
