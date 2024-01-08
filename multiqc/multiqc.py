bbb8ef07ffeeb4d5da3b759900a2a59616997acb
Phil Ewels <phil.ewels@seqera.io>
2024-01-08 14:51:33 +0100

    Log header emoji pickjer [skip changelog] (#2259)
    * Easter egg: Festive header emoji
    
    * Add couple more days, hide in utils
    
    * Small refactor
    
    ---------
    
    Co-authored-by: vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
    Co-authored-by: Vlad Savelyev <vladislav.sav@gmail.com>

--- multiqc/multiqc.py
+++ multiqc/multiqc.py
@@ -36,13 +36,12 @@ logger = config.logger
 
 OLDEST_SUPPORTED_PYTHON_VERSION = "3.8"
 
+
 # Configuration for rich-click CLI help
 click.rich_click.USE_RICH_MARKUP = True
 click.rich_click.SHOW_METAVARS_COLUMN = False
 click.rich_click.APPEND_METAVARS_HELP = True
-click.rich_click.HEADER_TEXT = (
-    f"[dark_orange]///[/] [bold][link=https://multiqc.info]MultiQC[/link][/] :mag: [dim]| v{config.version}"
-)
+click.rich_click.HEADER_TEXT = f"[dark_orange]///[/] [bold][link=https://multiqc.info]MultiQC[/link][/] :{util_functions.choose_emoji()}: [dim]| v{config.version}"
 click.rich_click.FOOTER_TEXT = "See [link=http://multiqc.info]http://multiqc.info[/] for more details."
 click.rich_click.ERRORS_SUGGESTION = f"This is MultiQC [cyan]v{config.version}[/]\nFor more help, run '[yellow]multiqc --help[/]' or visit [link=http://multiqc.info]http://multiqc.info[/]"
 click.rich_click.STYLE_ERRORS_SUGGESTION = ""
