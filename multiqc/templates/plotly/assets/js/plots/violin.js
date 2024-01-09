d741a757a17e9c75573fd0844545cf67c005c9b5
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2024-01-09 01:43:51 +0100

    Violing plots WIP (left: custom hover function to highlight all samples for a dot; color by sample)

--- multiqc/templates/plotly/assets/js/plots/violin.js
+++ multiqc/templates/plotly/assets/js/plots/violin.js
@@ -1,73 +1,46 @@
 class ViolinPlot extends Plot {
   activeDatasetSize() {
     if (this.datasets.length === 0) return 0; // no datasets
-    return this.datasets[this.active_dataset_idx].data.length; // no samples in a dataset
+    return this.datasets[this.active_dataset_idx].data_by_metric.length; // no samples in a dataset
   }
 
   // Constructs and returns traces for the Plotly plot
   buildTraces() {
-    let data = this.datasets[this.active_dataset_idx].data;
-    if (data.length === 0) return [];
+    let data_by_metric = this.datasets[this.active_dataset_idx].data_by_metric;
+    let header_by_metric = this.datasets[this.active_dataset_idx].header_by_metric;
+    if (data_by_metric.length === 0) return [];
 
-    // let samples = lines.map((line) => line.name);
-    // let sampleSettings = applyToolboxSettings(samples);
-    // if (sampleSettings == null) return; // All series are hidden, do not render the graph.
-    // lines = lines.map((line, idx) => {
-    //   line.name = sampleSettings[idx].name ?? line.name;
-    //   line.highlight = sampleSettings[idx].highlight;
-    //   if (!sampleSettings[idx].hidden) return line;
-    // });
-    //
-    // // Reorder points so highlighted points are on top
-    // let highlighted = lines.filter((p) => p.highlight);
-    // let nonHighlighted = lines.filter((p) => !p.highlight);
-    // lines = nonHighlighted.concat(highlighted);
-    //
-    // // Toggle buttons for Y-axis limis
-    // let ymaxSet = this.ymax !== "undefined" && this.ymax !== null;
-    // let yminSet = this.ymin !== "undefined" && this.ymin !== null;
-    // // Only create if there is a y-axis limit
-    // let groupDiv = $("#" + this.target).closest(".mqc_hcplot_plotgroup");
-    // if (ymaxSet || (yminSet && this.ymin !== 0)) {
-    //   let wrapper = $('<div class="mqc_hcplot_yaxis_limit_toggle hidden-xs" />').prependTo(groupDiv);
-    //   wrapper.append(
-    //     '<span class="mqc_switch_wrapper"' +
-    //       '"' +
-    //       ' data-ymax="' +
-    //       this.ymax +
-    //       '"' +
-    //       ' data-ymin="' +
-    //       this.ymin +
-    //       '"' +
-    //       ' data-target="' +
-    //       this.target +
-    //       '">Y-Limits: <span class="mqc_switch on">on</span></span>',
-    //   );
-    //   wrapper.after('<div class="clearfix" />');
-    // }
-    //
-    // return lines.map((line) => {
-    //   let x, y;
-    //   if (line.data.length > 0 && Array.isArray(line.data[0])) {
-    //     x = line.data.map((x) => x[0]);
-    //     y = line.data.map((x) => x[1]);
-    //   } else {
-    //     x = [...Array(line.data.length).keys()];
-    //     y = line.data;
-    //   }
-    //
-    //   let params = JSON.parse(JSON.stringify(this.trace_params)); // deep copy
-    //   if (highlighted.length > 0) params.marker.color = line.highlight ?? "#cccccc";
-    //   else params.marker.color = line.color;
-    //
-    //   return {
-    //     type: "scatter",
-    //     x: x,
-    //     y: y,
-    //     name: line.name,
-    //     text: x.map(() => line.name),
-    //     ...params,
-    //   };
-    // });
+    this.layout.grid = {
+      rows: Object.keys(data_by_metric).length,
+      columns: 1,
+      pattern: "independent",
+      roworder: "top to bottom",
+      ygap: 0.3,
+    };
+
+    let layout = this.layout;
+    layout.margin.l = 160;
+    layout.margin.pad = 20;
+
+    return Object.keys(data_by_metric).map((metric, idx) => {
+      let params = JSON.parse(JSON.stringify(this.trace_params)); // deep copy
+
+      let akey = "xaxis" + (idx === 0 ? "" : idx + 1);
+      let axis = layout[akey] ?? {};
+      axis.rangemode = header_by_metric[metric]["tozero"] ? "tozero" : "normal";
+      axis.range = [header_by_metric[metric]["min"], header_by_metric[metric]["max"]];
+      layout[akey] = axis;
+
+      return {
+        type: "violin",
+        x: Object.values(data_by_metric[metric]),
+        name: header_by_metric[metric].title,
+        text: Object.keys(data_by_metric[metric]), // sample names
+        xaxis: "x" + idx,
+        yaxis: "y" + idx,
+        range: [0, 100],
+        ...params,
+      };
+    });
   }
 }
--- multiqc/templates/plotly/plots/violin.py
+++ multiqc/templates/plotly/plots/violin.py
@@ -3,8 +3,10 @@ import logging
 from typing import Dict, List, Union, Optional
 
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 from multiqc.templates.plotly.plots.plot import Plot, PlotType, BaseDataset
+from multiqc.utils import mqc_colour
 
 logger = logging.getLogger(__name__)
 
@@ -26,7 +28,8 @@ def plot(data: List[Dict[str, MetricsT]], headers: List[Dict], pconfig: Dict) ->
 
 @dataclasses.dataclass
 class Dataset(BaseDataset):
-    data_by_sample: Dict[str, MetricsT]
+    data_by_metric: Dict[str, MetricsT]
+    header_by_metric: Dict[str, Dict[str, Union[str, int, float]]]
 
 
 class ViolinPlot(Plot):
@@ -34,79 +37,57 @@ class ViolinPlot(Plot):
         self,
         pconfig: Dict,
         list_of_data_by_sample: List[Dict[str, MetricsT]],
-        list_of_headers: List[Dict],
+        list_of_header_by_metric: List[Dict],
     ):
         super().__init__(PlotType.VIOLIN, pconfig, len(list_of_data_by_sample))
 
         # Extend each dataset object with a list of samples
-        self.datasets: List[Dataset] = [
-            Dataset(**d.__dict__, data_by_sample=self.prep_data(data_by_sample, headers))
-            for d, data_by_sample, headers in zip(self.datasets, list_of_data_by_sample, list_of_headers)
-        ]
+        for idx, (data_by_sample, header_by_metric) in enumerate(zip(list_of_data_by_sample, list_of_header_by_metric)):
+            header_by_metric = {
+                m: {k: v for k, v in header.items() if isinstance(v, (str, int, float))}
+                for m, header in header_by_metric.items()
+            }
+
+            data_by_metric = {}
+            for sample, metrics in data_by_sample.items():
+                for metric, value in list(metrics.items()):
+                    if metric not in header_by_metric:
+                        continue
+                    if metric not in data_by_metric:
+                        data_by_metric[metric] = {}
+                    data_by_metric[metric][sample] = value
+
+            for metric, data in data_by_metric.items():
+                xmin = header_by_metric[metric].get("min")
+                header_by_metric[metric]["tozero"] = xmin == 0
+                if xmin is None:
+                    xmin = min(data.values())
+                    xmin -= xmin * 0.05
+                header_by_metric[metric]["min"] = xmin
+                xmax = header_by_metric[metric].get("max")
+                if xmax is None:
+                    xmax = max(data.values())
+                    xmax += xmax * 0.05
+                header_by_metric[metric]["max"] = xmax
+
+            self.datasets[idx] = Dataset(
+                *self.datasets[idx].__dict__, data_by_metric=data_by_metric, header_by_metric=header_by_metric
+            )
 
         self.categories: List[str] = pconfig.get("categories", [])
 
         self.trace_params.update(
             orientation="h",
-            box={"visible": True},
+            box={"visible": False},
             meanline={"visible": True},
+            jitter=0.5,
             points="all",
-            jitter=0,
+            pointpos=0,
         )
 
-    @staticmethod
-    def tt_label() -> Optional[str]:
-        """Default tooltip label"""
-        return "%{x}: %{y}"
-
-    @staticmethod
-    def prep_data(data_by_sample: Dict[str, MetricsT], headers: Dict) -> Dict[str, MetricsT]:
-        # categories = []
-        # for k, header in headers.items():
-        #     categories.append(
-        #         {
-        #             "namespace": header["namespace"],
-        #             "title": header["title"],
-        #             "description": header["description"],
-        #             "max": header["dmax"],
-        #             "min": header["dmin"],
-        #             "suffix": header.get("suffix", ""),
-        #             "decimalPlaces": header.get("decimalPlaces", "2"),
-        #         }
-        #     )
-        #     # Add the data
-        #     thisdata = []
-        #     these_snames = []
-        #     for s_name, samp in data_by_sample.items():
-        #         if k in samp:
-        #             val = samp[k]
-        #             dt.raw_vals[s_name][k] = val
-        #
-        #             if "modify" in header and callable(header["modify"]):
-        #                 val = header["modify"](val)
-        #
-        #             thisdata.append(val)
-        #             these_snames.append(s_name)
-
-        # Reorder data to make it indexed by metric first, so each violin corresponds to
-        # one metric, and contains a dot for each sample that have a value for this metric.
-        # data_by_metric: Dict[str, MetricsT] = {}
-        # for sample, metrics in data_by_sample.items():
-        #     for metric, value in metrics.items():
-        #         if metric not in data_by_metric:
-        #             data_by_metric[metric] = {}
-        #         data_by_metric[metric][sample] = value
-
-        # replace metric key with header["title"]
-        prep_metric_by_sample = dict()
-        for sample, metrics_d in data_by_sample.items():
-            prep_metric_by_sample[sample] = dict()
-            for metric, value in metrics_d.items():
-                if metric in headers:
-                    title = headers[metric]["title"]
-                    prep_metric_by_sample[sample][title] = value
-
-        return prep_metric_by_sample
+        self.layout.height = 70 * max(len(ds.header_by_metric) for ds in self.datasets)
+        self.layout.margin = dict(pad=0)
+        self.layout.violingap = 0
 
     def dump_for_javascript(self):
         """Serialise the data to pick up in plotly-js"""
@@ -114,21 +95,42 @@ class ViolinPlot(Plot):
         d["categories"] = self.categories
         return d
 
+    @staticmethod
+    def tt_label() -> Optional[str]:
+        """Default tooltip label"""
+        return "%{x}: %{y}"
+
     def create_figure(self, layout: go.Layout, dataset: Dataset, is_log=False, is_pct=False):
         """
         Create a Plotly figure for a dataset
         """
+        c_scale = mqc_colour.mqc_colour_scale("plot_defaults")
         fig = go.Figure(layout=layout)
-
-        for name, metrics in dataset.data_by_sample.items():
+        fig = make_subplots(
+            len(dataset.data_by_metric),
+            1,
+            figure=fig,
+            vertical_spacing=0.5 / len(dataset.data_by_metric),
+        )
+        for idx, (metric, data) in enumerate(dataset.data_by_metric.items()):
             fig.add_trace(
                 go.Violin(
-                    name=name,
-                    x=list(metrics.values()),
-                    y=list(metrics.keys()),
+                    x=list(data.values()),
+                    name=dataset.header_by_metric[metric].get("title", metric),
+                    text=list(data.keys()),
+                    marker={"color": c_scale.get_colour(idx, lighten=1)},
                     **self.trace_params,
-                )
+                ),
+                row=idx + 1,
+                col=1,
             )
+            fig.update_xaxes(
+                range=[dataset.header_by_metric[metric]["min"], dataset.header_by_metric[metric]["max"]],
+                rangemode="tozero" if dataset.header_by_metric[metric]["tozero"] else "normal",
+                row=idx + 1,
+                col=1,
+            )
+
         return fig
 
     def save_data_file(self, data: BaseDataset) -> None:
