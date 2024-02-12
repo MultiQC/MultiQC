59f5884457a201199ac7297af9b533efe15c97bd
Vlad Savelyev <vladislav.sav@gmail.com>
2024-02-12 12:32:50 +0100

    Bump versions for v1.20 release [skip changelog] (#2315)
    * Bump versions for v1.20 release [skip changelog]
    
    * Changelog
    
    * Bit of changelog massaging prior to v1.20 release
    
    ---------
    
    Co-authored-by: Phil Ewels <phil.ewels@seqera.io>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ MultiQC was written by Phil Ewels (http://phil.ewels.co.uk) at Seqera Labs (http
 
 from setuptools import find_packages, setup
 
-version = "1.20dev"
+version = "1.20"
 dl_version = "main" if "dev" in version else f"v{version}"
 
 print(
