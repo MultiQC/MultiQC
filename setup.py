529161e42078a55c7906cac51f0181c26faf7d7b
Phil Ewels <phil.ewels@seqera.io>
2024-02-12 15:59:28 +0100

    Bump version to v1.21dev (#2319)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ MultiQC was written by Phil Ewels (http://phil.ewels.co.uk) at Seqera Labs (http
 
 from setuptools import find_packages, setup
 
-version = "1.20"
+version = "1.21dev"
 dl_version = "main" if "dev" in version else f"v{version}"
 
 print(
