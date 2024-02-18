0aae28bf08c6431f502cb58533dccac8b75193c1
Vlad Savelyev <vladislav.sav@gmail.com>
2024-02-18 17:56:08 +0100

    Fix ploidy estimation defaultdict

--- multiqc/modules/dragen/ploidy_estimation_metrics.py
+++ multiqc/modules/dragen/ploidy_estimation_metrics.py
@@ -1,5 +1,4 @@
 import logging
-from collections import defaultdict
 
 from multiqc.modules.base_module import BaseMultiqcModule
 
@@ -57,8 +56,7 @@ def parse_ploidy_estimation_metrics_file(f):
     PLOIDY ESTIMATION,,Ploidy estimation,X0
     """
 
-    data = defaultdict(dict)
-
+    data = {}
     for line in f["f"].splitlines():
         _, _, metric, stat = line.split(",")
         if stat.strip() == "":
