352d9e840cebe8d90ce9c96f62ae0cfad7126206
Vlad Savelyev <vladislav.sav@gmail.com>
2024-02-21 21:18:33 +0100

    Catch timeout error

--- multiqc/multiqc.py
+++ multiqc/multiqc.py
@@ -414,26 +414,35 @@ def run(
                 "is_docker": os.path.exists("/.dockerenv"),
                 "is_singularity": os.path.exists("/.singularity.d"),
                 "is_conda": os.path.exists(os.path.join(sys.prefix, "conda-meta")),
-                "ci_environment": strtobool(os.getenv("CI", False)),
+                "is_ci": strtobool(os.getenv("CI", False)),
             }
-            r = requests.get(config.version_check_url, params=meta)
-            release_info = r.json()
-            # Broadcast log messages if found
-            for msg in release_info.get("broadcast_messages", []):
-                if msg.get("message"):
-                    level = msg.get("level")
-                    if level not in ["debug", "info", "warning", "error", "critical"]:
-                        level = "info"
-                    getattr(logger, level)(msg["message"])
-            # Available update log if newer
-            remove_version = version.parse(re.sub(r"[^0-9.]", "", release_info["latest_release"]["version"]))
-            this_version = version.parse(re.sub(r"[^0-9.]", "", config.short_version))
-            if remove_version > this_version:
-                logger.warning(f"MultiQC Version {release_info['latest_release']['version']} now available!")
-            logger.debug(
-                f"Latest MultiQC version is {release_info['latest_release']['version']}, "
-                f"released {release_info['latest_release']['release_date']}"
-            )
+            wait_seconds = 2
+            try:
+                r = requests.get(config.version_check_url, params=meta, timeout=wait_seconds)
+            except requests.exceptions.Timeout as e:
+                logger.debug(
+                    f"Timed out after waiting for {wait_seconds}s for multiqc.info to check latest version: {e}"
+                )
+            except requests.exceptions.RequestException as e:
+                logger.debug(f"Could not connect to multiqc.info for version check: {e}")
+            else:
+                release_info = r.json()
+                # Broadcast log messages if found
+                for msg in release_info.get("broadcast_messages", []):
+                    if msg.get("message"):
+                        level = msg.get("level")
+                        if level not in ["debug", "info", "warning", "error", "critical"]:
+                            level = "info"
+                        getattr(logger, level)(msg["message"])
+                # Available update log if newer
+                remove_version = version.parse(re.sub(r"[^0-9.]", "", release_info["latest_release"]["version"]))
+                this_version = version.parse(re.sub(r"[^0-9.]", "", config.short_version))
+                if remove_version > this_version:
+                    logger.warning(f"MultiQC Version {release_info['latest_release']['version']} now available!")
+                logger.debug(
+                    f"Latest MultiQC version is {release_info['latest_release']['version']}, "
+                    f"released {release_info['latest_release']['release_date']}"
+                )
         except Exception as e:
             logger.debug(f"Could not connect to multiqc.info for version check: {e}")
 
