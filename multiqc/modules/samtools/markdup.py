b7fb46d770add0959675ad75b30af1563190da87
Matthias De Smet <11850640+matthdsm@users.noreply.github.com>
2024-03-05 13:52:38 +0100

    add general table

--- multiqc/modules/samtools/markdup.py
+++ multiqc/modules/samtools/markdup.py
@@ -4,7 +4,7 @@
 
 import logging
 import json
-
+from multiqc.plots import bargraph
 
 # Initialise the logger
 log = logging.getLogger(__name__)
@@ -16,8 +16,7 @@ class MarkdupReportMixin:
 
         self.samtools_markdup = dict()
         for f in self.find_log_files("samtools/markdup"):
-            parsed_data = parse_report(f["f"])
-            print(parsed_data)
+            parsed_data = self.parse_report(f["f"])
             if len(parsed_data) > 0:
                 if f["s_name"] in self.samtools_markdup:
                     log.debug(f"Duplicate sample name found! Overwriting: {f['s_name']}")
@@ -26,7 +25,6 @@ class MarkdupReportMixin:
 
         # Filter to strip out ignored sample names
         self.samtools_markdup = self.ignore_samples(self.samtools_markdup)
-
         if len(self.samtools_markdup) == 0:
             return 0
 
@@ -37,15 +35,51 @@ class MarkdupReportMixin:
         # Replace None with actual version if it is available
         self.add_software_version(None)
 
+        # Add to the general stats table
+        general_stats_headers = {
+            "PERCENT DUPLICATION": {
+                "title": "Duplicates",
+                "description": "markdup - Percent Duplication",
+                "max": 100,
+                "min": 0,
+                "suffix": "%",
+                "scale": "OrRd",
+            },
+        }
+        self.general_stats_addcols(self.samtools_markdup, general_stats_headers)
+
+        # Prep the data for the plots
+        # FEATURE PARITY with PICARD MarkDuplicates
+        # UNIQUE PAIRS, UNIQUE UNPAIRED, DUPLICATE PAIR,DUPLICATE PAIR OPTICAL, DUPLICATE UNPAIRED, UNMAPPED (EXCLUDED)
+
+        # Config for the plot
+        pconfig = {
+            "id": "samtools-markdup_deduplication",
+            "title": "Samtools markdup: duplication Stats",
+            "ylab": "# Reads",
+        }
+
+        self.add_section(
+            name="Mark Duplicates",
+            anchor="samtools-markdup",
+            description="Number of reads, categorised by duplication state using Samtools.",
+            plot=bargraph.plot(self.samtools_markdup, [], pconfig),
+        )
+
         return len(self.samtools_markdup)
 
+    def parse_report(self, f):
+        """Parse the samtools markdup output file and return a dictionary of the data."""
+        try:
+            # check if the file is a json file
+            parsed_data = json.loads(f)
+        except ValueError:
+            # if it is not a json file, then it is a text file
+            parsed_data = {line.split(":")[0].strip(): line.split(":")[1].strip() for line in f.splitlines() if line}
 
-def parse_report(f):
-    """Parse the samtools markdup output file and return a dictionary of the data."""
-    try:
-        # check if the file is a json file
-        parsed_data = json.loads(f)
-    except ValueError:
-        # if it is not a json file, then it is a text file
-        parsed_data = {line.split(":")[0].strip(): line.split(":")[1].strip() for line in f.splitlines() if line}
-    return parsed_data
+        parsed_data["PERCENT DUPLICATION"] = (
+            int(parsed_data.get("DUPLICATE TOTAL", 0)) / int(parsed_data.get("READ")) * 100
+        )
+
+        print(parsed_data)
+        return parsed_data
