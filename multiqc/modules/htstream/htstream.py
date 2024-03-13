37772fe1ace539d7c4c67f9f36e603b8102a508e
Bradley Jenner <bnjenner@ucdavis.edu>
2024-03-12 19:19:10 -0700

    ModuleNoSamplesFound.2

--- multiqc/modules/htstream/htstream.py
+++ multiqc/modules/htstream/htstream.py
@@ -75,7 +75,7 @@ class MultiqcModule(BaseMultiqcModule):
 
         # make sure samples are being processed
         if len(self.data) == 0:
-            raise UserWarning
+            raise ModuleNoSamplesFound
 
         # report number of files found
         log.info("Found " + str(len(self.data)) + " reports.")
