d214c3913b9c323e6693939e0c184aa66ab68e5e
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2024-04-23 16:41:06 +0200

    Add two scripts to allow benchmarking and testing lz compression

---
+++ scripts/lz-compress.py
@@ -0,0 +1,10 @@
+import sys
+
+from multiqc.utils import lzstring
+
+
+if __name__ == "__main__":
+    lz_string = lzstring.LZString()
+    with open(sys.argv[1], "rt") as f:
+        data = f.read()
+    print(lz_string.compressToBase64(data), end="")
