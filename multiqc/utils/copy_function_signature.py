4f3425657a3cbee60fd3c384ba984eae1692f757
Vlad Savelyev <vladislav.sav@gmail.com>
2024-04-25 21:39:34 +0200

    Fix copy_fucntion_signature wrappers for older python

--- multiqc/utils/copy_function_signature.py
+++ multiqc/utils/copy_function_signature.py
@@ -7,34 +7,44 @@ Solution is borrowed entirely from the comment at
 https://github.com/python/typing/issues/270#issuecomment-1346124813
 """
 
-import functools
-from collections.abc import Callable
-from typing import Any, Concatenate, ParamSpec, TypeVar
+import sys
 
+if sys.version_info < (3, 10):
+    # ParamSpec and Concatenate are new in Python 3.10, so defining
+    # no-op versions of decorators for earlier versions of Python.
 
-P = ParamSpec("P")
-T = TypeVar("T")
+    def copy_callable_signature(source):
+        return lambda target: target
 
+    def copy_method_signature(source):
+        return lambda target: target
 
-def copy_callable_signature(source: Callable[P, T]) -> Callable[[Callable[..., T]], Callable[P, T]]:
-    def wrapper(target: Callable[..., T]) -> Callable[P, T]:
-        @functools.wraps(source)
-        def wrapped(*args: P.args, **kwargs: P.kwargs) -> T:
-            return target(*args, **kwargs)
+else:
+    import functools
+    from collections.abc import Callable
+    from typing import Any, Concatenate, ParamSpec, TypeVar
 
-        return wrapped
+    P = ParamSpec("P")
+    T = TypeVar("T")
 
-    return wrapper
+    def copy_callable_signature(source: Callable[P, T]) -> Callable[[Callable[..., T]], Callable[P, T]]:
+        def wrapper(target: Callable[..., T]) -> Callable[P, T]:
+            @functools.wraps(source)
+            def wrapped(*args: P.args, **kwargs: P.kwargs) -> T:
+                return target(*args, **kwargs)
 
+            return wrapped
 
-def copy_method_signature(
-    source: Callable[Concatenate[Any, P], T],
-) -> Callable[[Callable[..., T]], Callable[Concatenate[Any, P], T]]:
-    def wrapper(target: Callable[..., T]) -> Callable[Concatenate[Any, P], T]:
-        @functools.wraps(source)
-        def wrapped(self: Any, /, *args: P.args, **kwargs: P.kwargs) -> T:
-            return target(self, *args, **kwargs)
+        return wrapper
 
-        return wrapped
+    def copy_method_signature(
+        source: Callable[Concatenate[Any, P], T],
+    ) -> Callable[[Callable[..., T]], Callable[Concatenate[Any, P], T]]:
+        def wrapper(target: Callable[..., T]) -> Callable[Concatenate[Any, P], T]:
+            @functools.wraps(source)
+            def wrapped(self: Any, /, *args: P.args, **kwargs: P.kwargs) -> T:
+                return target(self, *args, **kwargs)
 
-    return wrapper
+            return wrapped
+
+        return wrapper
