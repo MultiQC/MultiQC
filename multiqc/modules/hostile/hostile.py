c73d63e9305b1c5a17f407bf76c5d8d021ad38e9
Sumeet Tiwari <sumeet.kumartt@gmail.com>
2024-04-26 22:45:29 +0100

    review 1

--- multiqc/modules/hostile/hostile.py
+++ multiqc/modules/hostile/hostile.py
@@ -25,11 +25,20 @@ class MultiqcModule(BaseMultiqcModule):
         self.parse_data = dict()
         for f in self.find_log_files("hostile", filehandles=True):
             self.parse_logs(f)
+        
+        # add version
+        self.add_software_version(self.version,f)
+        
+        # Ignore samples
+        self.parse_data = self.ignore_samples(self.parse_data)
 
         # Check if no matching log files were found
         if len(self.parse_data) == 0:
             raise ModuleNoSamplesFound
-
+        
+        # Number of files found
+        log.info(f"Found {len(self.parse_data)} reports")
+        
         # Write hostile data.
         self.write_data_file(self.parse_data, "multiqc_hostile")
 
@@ -49,9 +58,15 @@ class MultiqcModule(BaseMultiqcModule):
 
         if len(parse_data) > 0:
             self.add_data_source(json_file)
-
+        
         s_name = self.clean_s_name(json_file["fn"])
         self.parse_data[s_name] = parse_data
+
+        #print(self.parse_data[s_name][0])
+        if "version" in self.parse_data[s_name][0]:
+            self.version = self.parse_data[s_name][0]['version']
+        else:
+            self.version = "0.4.0"
 
     def hostile_plot(self):
         """
@@ -59,28 +74,25 @@ class MultiqcModule(BaseMultiqcModule):
         Plot the barplot
         """
         data = {}
+
         for f_name, values in self.parse_data.items():
             s_name = values[0]["fastq1_in_name"].split(".")[0]
             database = os.path.basename(values[0]["index"])
             data[s_name] = {"Cleaned reads": values[0]["reads_out"], "Host reads": values[0]["reads_removed"]}
-
+        
         ## categories
-        cat = ["Cleaned reads", "Host reads"]
+        cats = ["Cleaned reads", "Host reads"]
 
         pconfig = {
             "title": "Hostile: Reads Filtered",
             "id": "he_reads_plots",
             "ylab": "# Reads",
-            "plot_type": "bargraph",
-            "stacked_data": True,
+            "plot_type": "bargraph"
         }
 
         self.add_section(
             name="Reads Filtering",
             anchor="hostile-reads",
             description=f"This plot shows the number of cleaned reads vs host-reads per sample (database index: {database}).",
-            plot=bargraph.plot(data, cat, pconfig),
+            plot=bargraph.plot(data, cats, pconfig),
         )
-
-
-### self.add_software_version cannot be used because its missing in the JSON reports produced by the tool itself.
