20dd6374145e0fb1330c7692d81f4050d159f197
Vlad Savelyev <vladislav.sav@gmail.com>
2024-05-07 19:49:45 +0200

    Remove report and config from RunResult

--- multiqc/core/exceptions.py
+++ multiqc/core/exceptions.py
@@ -1,5 +1,4 @@
 import logging
-from multiqc import config, report
 
 logger = logging.getLogger("multiqc")
 
@@ -10,16 +9,11 @@ class RunResult:
 
     * appropriate error code (e.g. 1 if a module broke, 0 on success)
     * error message if a module broke
-    * report instance
-    * config instance
-
     """
 
     def __init__(self, sys_exit_code: int = 0, message: str = ""):
         self.sys_exit_code = sys_exit_code
         self.message = message
-        self.report = report
-        self.config = config
 
 
 class RunError(Exception):
