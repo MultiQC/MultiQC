59b1623b336585bd0925caa6927ba10d6ea61456
Matthias Zepper <6963520+MatthiasZepper@users.noreply.github.com>
2024-06-09 14:14:49 +0200

    UMI-tools extract module: Improve plots (#2614)
    * Modify UMI-tools extract plots and data presentation.
    
    * Split umi-extract regex plot.
    
    * Polishing the UMI-tools extract plots.
    
    * Only plot regex plot, if we have the corresponding data in the first place.
    
    ---------
    
    Co-authored-by: Matthias Zepper <MatthiasZepper@users.noreply.github.com>
    Co-authored-by: Vlad Savelyev <vladislav.sav@gmail.com>

--- multiqc/modules/umitools/umitools.py
+++ multiqc/modules/umitools/umitools.py
@@ -74,6 +74,13 @@ class MultiqcModule(BaseMultiqcModule):
             self.extract_general_stats_table(extract_data_by_sample)
             self.umitools_extract_barplot(extract_data_by_sample)
 
+            # Only plot regex match rate if we have regex extraction logs:
+            if any(
+                any(key in v for key in ["read1_match", "read2_match", "read1_mismatch", "read2_mismatch"])
+                for v in extract_data_by_sample.values()
+            ):
+                self.umitools_extract_barplot_regex(extract_data_by_sample)
+
     def _parse_s_name(self, f) -> Optional[str]:
         # Get the s_name from the input file if possible
         # stdin : <_io.TextIOWrapper name='M18-39155_T1.Aligned.sortedByCoord.out.bam' mode='r' encoding='UTF-8'>
@@ -142,6 +149,18 @@ class MultiqcModule(BaseMultiqcModule):
                 else:
                     data[key] = type_(re_matches.group(1))
 
+            # Calculate a few simple supplementary stats
+            try:
+                data["extract_percent_passing"] = round(
+                    ((data["extract_output_reads"] / data["extract_input_reads"]) * 100.0), 2
+                )
+            except (KeyError, ZeroDivisionError):
+                pass
+            try:
+                data["extract_removed_reads"] = data["extract_input_reads"] - data["extract_output_reads"]
+            except KeyError:
+                pass
+
         return data
 
     def dedup_general_stats_table(self, data_by_sample):
@@ -179,6 +198,14 @@ class MultiqcModule(BaseMultiqcModule):
                 "shared_key": "read_count",
                 "scale": "PuRd",
             },
+            "extract_percent_passing": {
+                "title": "% UMI Extract",
+                "description": "% reads from which a UMI extraction succeeded.",
+                "max": 100,
+                "min": 0,
+                "suffix": "%",
+                "scale": "RdYlGn",
+            },
         }
         self.general_stats_addcols(data_by_sample, headers)
 
@@ -208,25 +235,63 @@ class MultiqcModule(BaseMultiqcModule):
 
     def umitools_extract_barplot(self, data_by_sample):
         keys = {
-            "read1_match": {"color": "#7fc9c7", "name": "Read1 Match"},
-            "read1_mismatch": {"color": "#fd9286", "name": "Read1 Mismatch"},
-            "read2_match": {"color": "#7f89c9", "name": "Read2 Match"},
-            "read2_mismatch": {"color": "#fd86aa", "name": "Read2 Mismatch"},
+            "extract_output_reads": {"color": "#7fc9c7", "name": "Extraction succeeded"},
+            "extract_removed_reads": {"color": "#fd9286", "name": "Extraction failed"},
         }
 
         # Add a section with a barplot plot of UMI stats to the report
         self.add_section(
-            name="Extract Stats",
-            anchor="umitools_extract",
-            description="Read stats from `umi_tools extract`",
+            name="UMI extraction rate",
+            anchor="umitools_extract_overall",
+            description="Success rate of `umi_tools extract`",
             plot=bargraph.plot(
                 data_by_sample,
                 keys,
                 {
-                    "id": "umitools_extract_barplot",
-                    "title": "UMI-tools: Extract Stats",
+                    "id": "umitools_extract_barplot_success",
+                    "title": "UMI-tools: Extraction rate",
                     "ylab": "# Reads",
                     "cpswitch_counts_label": "Number of Reads",
+                },
+            ),
+        )
+
+    def umitools_extract_barplot_regex(self, data_by_sample):
+        keys = [
+            {
+                "read1_match": {"color": "#7fc9c7", "name": "Match on read 1"},
+                "read1_mismatch": {"color": "#fd9286", "name": "Mismatch on read 1"},
+            },
+            {
+                "read2_match": {"color": "#7fc9c7", "name": "Match on read 2"},
+                "read2_mismatch": {"color": "#fd9286", "name": "Mismatch on read 2"},
+            },
+        ]
+
+        subset_read1 = {
+            sample: {key: metrics[key] for key in ("read1_match", "read1_mismatch") if key in metrics}
+            for sample, metrics in data_by_sample.items()
+        }
+
+        subset_read2 = {
+            sample: {key: metrics[key] for key in ("read2_match", "read2_mismatch") if key in metrics}
+            for sample, metrics in data_by_sample.items()
+        }
+
+        # Add a section with a barplot plot of the regex match rate to the report
+        self.add_section(
+            name="UMI extraction regex match rate",
+            anchor="umitools_extract_regex",
+            description="Regex match rate of `umi_tools extract`",
+            plot=bargraph.plot(
+                [subset_read1, subset_read2],
+                keys,
+                {
+                    "id": "umitools_extract_regex_barplot",
+                    "title": "UMI-tools: Extraction regex match rate",
+                    "ylab": "# Reads",
+                    "cpswitch_counts_label": "Number of Reads",
+                    "data_labels": ["Read 1", "Read 2"],
                 },
             ),
         )
