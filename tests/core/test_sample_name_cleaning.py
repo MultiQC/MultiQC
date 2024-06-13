343ea3d2a9a250d60ca242a27874574548189a2f
Vlad Savelyev <vladislav.sav@gmail.com>
2024-06-13 22:51:28 +0200

    Use pytest for all core unit tests (#2623)
    * Use pytest for core unit tests
    
    * Move conftest further

--- tests/core/test_sample_name_cleaning.py
+++ tests/core/test_sample_name_cleaning.py
@@ -1,63 +1,46 @@
-#!/usr/bin/python
-
 """Test that the sample cleaning logic works as expected."""
 
-import unittest
+import pytest
 from copy import copy
-
 from multiqc import config
 from multiqc.base_module import BaseMultiqcModule
 
-# Capture default fn_clean_exts, fn_clean_trim before we trash them.
-def_fn_clean_exts = copy(config.fn_clean_exts)
-def_fn_clean_trim = copy(config.fn_clean_trim)
+
+@pytest.fixture
+def base_module():
+    return BaseMultiqcModule()
 
 
-class T(unittest.TestCase):
-    def setUp(self):
-        self.bmm = BaseMultiqcModule()
-        self.scrub_defaults = False
-        self.config = {}
+@pytest.fixture
+def clean_config():
+    original_exts = copy(config.fn_clean_exts)
+    original_trim = copy(config.fn_clean_trim)
 
-    def test_noop(self):
-        self.scrub_defaults = True
+    # This will execute before each test
+    yield
 
-        self.assertClean("foo.bar.fastq.gz", "foo.bar.fastq.gz")
-
-    def test_default_trim(self):
-        self.assertClean("foo.bar.fastq.gz", "foo.bar")
-
-    def test_custom_clean_ext(self):
-        self.config = dict(extra_fn_clean_exts=["chop_this_off"])
-
-        self.assertClean("foo.chop_this_off", "foo")
-
-    def test_regex_keep(self):
-        self.config = dict(extra_fn_clean_exts=[{"type": "regex_keep", "pattern": "abc..X"}])
-
-        self.assertClean("foo_abc12X_bar", "abc12X")
-
-        self.assertClean("foo_abc123_bar", "foo_abc123_bar")
-
-    # Helper functions
-
-    def assertClean(self, unclean, clean):
-        self.assertEqual(self.clean(unclean), clean)
-
-    def clean(self, unclean, root=None):
-        """Refresh the configuration, then invoke the cleanup logic with
-        self.config upon the input sample name.
-        """
-        if self.scrub_defaults:
-            config.fn_clean_exts[:] = []
-            config.fn_clean_trim[:] = []
-        else:
-            config.fn_clean_exts[:] = def_fn_clean_exts
-            config.fn_clean_trim[:] = def_fn_clean_trim
-        config._add_config(self.config, None)
-
-        return self.bmm.clean_s_name(unclean, root)
+    # Restore configuration after each test
+    config.fn_clean_exts = original_exts
+    config.fn_clean_trim = original_trim
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_noop(base_module, clean_config):
+    # Assume no operation needed
+    config.fn_clean_exts[:] = []
+    config.fn_clean_trim[:] = []
+    assert base_module.clean_s_name("foo.bar.fastq.gz") == "foo.bar.fastq.gz"
+
+
+def test_default_trim(base_module, clean_config):
+    assert base_module.clean_s_name("foo.bar.fastq.gz") == "foo.bar"
+
+
+def test_custom_clean_ext(base_module, clean_config):
+    config.fn_clean_exts = ["chop_this_off"]
+    assert base_module.clean_s_name("foo.chop_this_off") == "foo"
+
+
+def test_regex_keep(base_module, clean_config):
+    config.fn_clean_exts = [{"type": "regex_keep", "pattern": "abc..X"}]
+    assert base_module.clean_s_name("foo_abc12X_bar") == "abc12X"
+    assert base_module.clean_s_name("foo_abc123_bar") == "foo_abc123_bar"
