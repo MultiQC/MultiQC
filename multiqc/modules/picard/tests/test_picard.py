cd4fdd2725bb941d50df39cc23b07dd43a256a37
Vlad Savelyev <vladislav.sav@gmail.com>
2024-06-14 15:12:50 +0200

    Make picard CollectMultipleMetrics pattern shared

--- multiqc/modules/picard/tests/test_picard.py
+++ multiqc/modules/picard/tests/test_picard.py
@@ -12,7 +12,11 @@ def data_dir():
 
 # Most files are expected to produce one sample, these are exceptions:
 NUM_SAMPLES_BY_FILE = {
-    "two_samples.txt": 2,
+    "two_samples.txt": 2,  # AlignmentSummaryMetrics
+    "single_sample.gvcf.variant_calling_summary_metrics": 0,  # VariantCallingMetrics
+    "overview_errors.csv": 0,  # ValidateSamFile
+    "overview_warnings.csv": 0,  # ValidateSamFile
+    "truncated.txt": 0,  # RnaSeqMetrics
 }
 
 
@@ -22,18 +26,45 @@ def test_picard_data_parsed(tool, data_dir):
     print(tool_subdir)
     assert tool_subdir.exists()
 
-    # Check that all test files are parsed
-    for path in tool_subdir.rglob("*"):
-        if path.parent.name == "multiqc_data" or path.name == "multiqc_report.html" or not path.is_file():
-            continue
+    report.reset()
+    report.analysis_files = [tool_subdir]
+    report.search_files(["picard"])
 
-        print(f"Scanning path {path}")
-        report.reset()
-        report.analysis_files = [path]
-        report.search_files(["picard"])
-        m = MultiqcModule(tools=[tool])
-        expected_num_samples = NUM_SAMPLES_BY_FILE.get(path.name, 1)
-        assert len(m.saved_raw_data) == 1
-        samples_parsed = list(m.saved_raw_data.values())[0].keys()
-        print(path, samples_parsed)
-        assert len(samples_parsed) == expected_num_samples
+    m = MultiqcModule(tools=[tool])
+    assert len(m.saved_raw_data) == 1
+
+    all_files = [
+        p
+        for p in tool_subdir.rglob("*")
+        if p.parent.name != "multiqc_data" and p.name != "multiqc_report.html" and p.is_file()
+    ]
+    expected_num_samples = sum(NUM_SAMPLES_BY_FILE.get(p.name, 1) for p in all_files)
+    samples_parsed = list(m.saved_raw_data.values())[0].keys()
+
+    print("Input files:", [p.name for p in all_files])
+    print("Parsed samples:", samples_parsed)
+
+    assert len(samples_parsed) == expected_num_samples
+
+
+# @pytest.mark.parametrize("tool", TOOLS)
+# def test_picard_data_parsed(tool, data_dir):
+#     tool_subdir = data_dir / "modules/picard" / tool
+#     print(tool_subdir)
+#     assert tool_subdir.exists()
+#
+#     # Check that all test files are parsed
+#     for path in tool_subdir.rglob("*"):
+#         if path.parent.name == "multiqc_data" or path.name == "multiqc_report.html" or not path.is_file():
+#             continue
+#
+#         print(f"Scanning path {path}")
+#         report.reset()
+#         report.analysis_files = [path]
+#         report.search_files(["picard"])
+#         m = MultiqcModule(tools=[tool])
+#         expected_num_samples = NUM_SAMPLES_BY_FILE.get(path.name, 1)
+#         assert len(m.saved_raw_data) == 1
+#         samples_parsed = list(m.saved_raw_data.values())[0].keys()
+#         print(path, samples_parsed)
+#         assert len(samples_parsed) == expected_num_samples
