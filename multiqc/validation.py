42bf278d84a07b8280a1afb685bfed2983f4fc7f
Vlad Savelyev <vladislav.sav@gmail.com>
2024-06-21 20:30:14 +0200

    Plot config validation: validate x_lines, x_bands, etc

--- multiqc/validation.py
+++ multiqc/validation.py
@@ -1,9 +1,10 @@
 import inspect
 import logging
+from collections import defaultdict
+from typing import List, Dict, Set
 
 from pydantic import BaseModel, ValidationError, model_validator
 from typeguard import check_type, TypeCheckError
-
 
 logger = logging.getLogger(__name__)
 
@@ -14,8 +15,18 @@ class ConfigValidationError(Exception):
         super().__init__()
 
 
-_validation_errors = []
-_validation_warnings = []
+_validation_errors_by_cls: Dict[str, Set[str]] = defaultdict(set)
+_validation_warnings_by_cls: Dict[str, Set[str]] = defaultdict(set)
+
+
+def add_validation_error(cls: type, error: str):
+    assert issubclass(cls, ValidatedConfig)
+    _validation_errors_by_cls[cls.__name__].add(error)
+
+
+def add_validation_warning(cls: type, warning: str):
+    assert issubclass(cls, ValidatedConfig)
+    _validation_warnings_by_cls[cls.__name__].add(warning)
 
 
 class ValidatedConfig(BaseModel):
@@ -23,13 +34,16 @@ class ValidatedConfig(BaseModel):
         try:
             super().__init__(**data)
         except ValidationError as e:
-            if not _validation_errors:
+            if not _validation_errors_by_cls.get(self.__class__.__name__):
                 raise
             else:
                 # errors are already added into plot.pconfig_validation_errors by a custom validator
                 logger.debug(e)
 
-        if _validation_errors or _validation_warnings:
+        errors = _validation_errors_by_cls.get(self.__class__.__name__)
+        warnings = _validation_warnings_by_cls.get(self.__class__.__name__)
+
+        if errors or warnings:
             modname = ""
             callstack = inspect.stack()
             for n in callstack:
@@ -39,23 +53,23 @@ class ValidatedConfig(BaseModel):
                     break
             plot_type = self.__class__.__name__.replace("Config", "")
 
-            if _validation_warnings:
+            if warnings:
                 logger.warning(f"{modname}Warnings in {plot_type} plot configuration {data}:")
-                for warning in _validation_warnings:
+                for warning in sorted(warnings):
                     logger.warning(f"• {warning}")
-                _validation_warnings.clear()
+                _validation_warnings_by_cls[self.__class__.__name__].clear()  # Reset for interactive usage
 
-            if _validation_errors:
+            if errors:
                 logger.error(f"{modname}Invalid {plot_type} plot configuration {data}:")
-                for error in _validation_errors:
+                for error in sorted(errors):
                     logger.error(f"• {error}")
-                _validation_errors.clear()  # Reset for interactive usage
+                _validation_errors_by_cls[self.__class__.__name__].clear()  # Reset for interactive usage
                 raise ConfigValidationError(module_name=modname)
 
     # noinspection PyNestedDecorators
     @model_validator(mode="before")
     @classmethod
-    def validate_fields(cls, values):
+    def validate_fields_before(cls, values):
         # Check unrecognized fields
         if not isinstance(values, dict):
             return values
@@ -64,8 +78,8 @@ class ValidatedConfig(BaseModel):
         filtered_values = {}
         for name, val in values.items():
             if name not in cls.model_fields:
-                _validation_errors.append(
-                    f"unrecognized field '{name}'. Available fields: {', '.join(cls.model_fields.keys())}"
+                add_validation_error(
+                    cls, f"unrecognized field '{name}'. Available fields: {', '.join(cls.model_fields.keys())}"
                 )
             else:
                 filtered_values[name] = val
@@ -77,7 +91,7 @@ class ValidatedConfig(BaseModel):
             if cls.model_fields[name].deprecated:
                 new_name = cls.model_fields[name].deprecated
                 msg = f"Deprecated field '{name}'. Use '{new_name}' instead"
-                _validation_warnings.append(msg)
+                add_validation_warning(cls, msg)
                 if new_name not in values:
                     values_without_deprecateds[new_name] = val
             else:
@@ -88,12 +102,25 @@ class ValidatedConfig(BaseModel):
         for name, field in cls.model_fields.items():
             if field.is_required():
                 if name not in values:
-                    _validation_errors.append(f"missing required field '{name}'")
+                    add_validation_error(cls, f"missing required field '{name}'")
 
-        # Check types
+        # Check types and validate specific fields
+        corrected_values = {}
         for name, val in values.items():
             field = cls.model_fields[name]
             expected_type = field.annotation
+
+            # Parse potential sub-models
+            parse_method = getattr(cls, f"parse_{name}", None)
+            if parse_method is not None:
+                try:
+                    val = parse_method(val)
+                except Exception as e:
+                    msg = f"'{name}': failed to parse value '{val}'"
+                    add_validation_error(cls, msg)
+                    logger.debug(f"{msg}: {e}")
+                    continue
+
             try:
                 check_type(val, expected_type)
             except TypeCheckError as e:
@@ -102,7 +129,10 @@ class ValidatedConfig(BaseModel):
                     v_str = v_str[:20] + "..."
                 expected_type_str = str(expected_type).replace("typing.", "")
                 msg = f"'{name}': expected type '{expected_type_str}', got '{type(val).__name__}' {v_str}"
-                _validation_errors.append(msg)
+                add_validation_error(cls, msg)
                 logger.debug(f"{msg}: {e}")
 
+            corrected_values[name] = val
+
+        values = corrected_values
         return values
