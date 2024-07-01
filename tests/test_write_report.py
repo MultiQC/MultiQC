956028f233a655d5f0661a9807b43b509e88224f
Vlad Savelyev <vladislav.sav@gmail.com>
2024-07-02 01:23:29 +0200

    Fix writing report to stdout, log everything to stderr

---
+++ tests/test_write_report.py
@@ -0,0 +1,17 @@
+import multiqc
+from multiqc import report
+from multiqc.core.update_config import ClConfig
+
+
+def test_write_stdout(data_dir, capsys):
+    """
+    Verify stdout option: stdout contains only HTML, nothing else is written to disk
+    """
+    report.reset()
+    mod_dir = data_dir / "modules" / "bismark"
+    assert mod_dir.exists() and mod_dir.is_dir()
+
+    multiqc.run(mod_dir, cfg=ClConfig(filename="stdout"))
+    captured = capsys.readouterr()
+
+    assert captured.out.startswith("""<!DOCTYPE html>\n<html lang="en">\n<head>""")
