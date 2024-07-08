c34c0b83c00ac87f5cf1b480868f9258f4beaa85
JosephLalli <lalli@wisc.edu>
2024-07-08 14:55:52 -0500

    Address end of file newline and include add_data_source function

--- multiqc/modules/vg/stats.py
+++ multiqc/modules/vg/stats.py
@@ -28,6 +28,7 @@ class MultiqcModule(BaseMultiqcModule):
             # Superfluous function call to confirm that it is used in this module
             # Replace None with actual version if it is available
             self.add_software_version(None, f["s_name"])
+            self.add_data_source(f)
 
         self.vg_stats_map = self.ignore_samples(self.vg_stats_map)
 
