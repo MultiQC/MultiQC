e733b02f8f71904cfdd7e500445af85f862b64aa
Vlad Savelyev <vladislav.sav@gmail.com>
2024-07-16 17:23:45 +0200

    Split up scripts/doc_md_to_docstrings.py scripts/generade_module_docs.py

--- scripts/generade_module_docs.py
+++ scripts/generade_module_docs.py
@@ -4,8 +4,7 @@ Generate docs/modules/<module.md> files for each module in MultiQC from the Mult
 Usage:
 
     python scripts/generate_module_docs.py
-    python scripts/generate_module_docs.py --output-dir docs/modules
-    python scripts/generate_module_docs.py --module samtools
+    python scripts/generate_module_docs.py samtools
 """
 
 import argparse
@@ -14,7 +13,7 @@ from textwrap import dedent
 from multiqc import config, report, BaseMultiqcModule
 
 parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument("--module", help="Generate docs for a specific module")
+parser.add_argument("module", help="Generate for a specific module", nargs="?")
 args = parser.parse_args()
 
 
@@ -22,34 +21,6 @@ for mod_id, entry_point in config.avail_modules.items():
     if args.module and args.module != mod_id:
         continue
     if mod_id == "custom_content":
-        continue
-
-    output_path = config.REPO_DIR / "docs/modules" / f"{mod_id}.md"
-    if output_path.exists():
-        print(
-            f"{output_path} already exists, so doing reverse: reading the md file contents, stripping the header, "
-            f"and adding the rest to the module docstring, then writing the module code back to the python file to disk"
-        )
-        with output_path.open("r") as fh:
-            text = fh.read()
-        docstring = text.split("---\n", maxsplit=2)[2].strip()
-        py_path = config.REPO_DIR / "multiqc" / "modules" / mod_id / f"{mod_id}.py"
-        with py_path.open() as fh:
-            module_code_lines = fh.readlines()
-        with py_path.open("w") as fh:
-            for line in module_code_lines:
-                if "# Initialise the parent object" in line or "# Initialise the logger" in line:
-                    continue
-
-                fh.write(line)
-                if line == "class MultiqcModule(BaseMultiqcModule):\n":
-                    fh.write('    """\n')
-                    for doc_line in docstring.split("\n"):
-                        fh.write(f"    {doc_line}\n")
-                    fh.write('    """\n')
-                    fh.write("\n")
-
-        print(f"Updated {py_path}")
         continue
 
     data_dir = config.REPO_DIR / "test-data" / "data"
@@ -75,6 +46,7 @@ description: {module.info}
 {dedent(docstring)}
 """
 
+    output_path = config.REPO_DIR / "docs/modules" / f"{mod_id}.md"
     with output_path.open("w") as fh:
         fh.write(text)
 
