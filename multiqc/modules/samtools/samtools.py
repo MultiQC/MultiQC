9699ff13a241f3446aad4ca12af536aabea69c0b
Vlad Savelyev <vladislav.sav@gmail.com>
2024-07-17 21:16:50 +0200

    Clean up

--- multiqc/modules/samtools/samtools.py
+++ multiqc/modules/samtools/samtools.py
@@ -56,7 +56,6 @@ class MultiqcModule(BaseMultiqcModule):
     """
 
     def __init__(self):
-        # Initialise the parent object
         super(MultiqcModule, self).__init__(
             name="Samtools",
             anchor="samtools",
