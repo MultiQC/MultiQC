060159a06aa4f04860fb8f764e980d36799c1d03
Vlad Savelyev <vladislav.sav@gmail.com>
2024-07-18 15:30:57 +0200

    Add reset fixture

--- tests/test_run.py
+++ tests/test_run.py
@@ -4,15 +4,13 @@ import tempfile
 import pytest
 
 import multiqc
-from multiqc import report
 from multiqc.core.update_config import ClConfig
 
 
-def test_write_default(data_dir, tmp_path):
+def test_write_default(reset, data_dir, tmp_path):
     """
     Verify HTML and data directory with default names are written to current dir
     """
-    report.reset()
     mod_dir = data_dir / "modules" / "kallisto"
     assert mod_dir.exists() and mod_dir.is_dir()
 
@@ -25,11 +23,10 @@ def test_write_default(data_dir, tmp_path):
     assert files_after - files_before == {"multiqc_report.html", "multiqc_data"}
 
 
-def test_write_stdout(data_dir, tmp_path, capsys):
+def test_write_stdout(reset, data_dir, tmp_path, capsys):
     """
     Verify stdout option: stdout contains only HTML, nothing else is written to disk
     """
-    report.reset()
     mod_dir = data_dir / "modules" / "kallisto"
     assert mod_dir.exists() and mod_dir.is_dir()
 
@@ -46,7 +43,7 @@ def test_write_stdout(data_dir, tmp_path, capsys):
 
 
 @pytest.mark.parametrize("clean_up", [True, False])
-def test_no_analysis_found(monkeypatch, tmp_path, clean_up):
+def test_no_analysis_found(reset, monkeypatch, tmp_path, clean_up):
     """
     Verify that an error is raised when a module is not found
     """
