6c6d37b51784918a83730210320705e18e9f2153
Vlad Savelyev <vladislav.sav@gmail.com>
2024-07-29 11:35:46 +0200

    Refactor. Drop stat_types. Add separate table. Use lineplots

--- multiqc/modules/nanoq/nanoq.py
+++ multiqc/modules/nanoq/nanoq.py
@@ -1,103 +1,66 @@
-"""MultiQC module to parse output from nanoq"""
-
 import re
 import logging
+from collections import defaultdict
+from copy import copy, deepcopy
 from typing import Dict, List, Any
 
-from multiqc import config
 from multiqc.base_module import BaseMultiqcModule, ModuleNoSamplesFound
-from multiqc.plots import bargraph
-from multiqc.utils import mqc_colour
+from multiqc.plots import bargraph, table, linegraph
+from multiqc.plots.table_object import TableConfig
 
-# Initialise the logger
 log = logging.getLogger(__name__)
 
 
 class MultiqcModule(BaseMultiqcModule):
-    """Nanoq module"""
-
-    _KEYS_MAPPING = {
-        "Number of reads": "Number of reads",
-        "Number of bases": "Number of bases",
-        "N50 read length": "N50 read length",
-        "Longest read": "Longest read",
-        "Shortest read": "Shortest read",
-        "Mean read length": "Mean read length",
-        "Median read length": "Median read length",
-        "Mean read quality": "Mean read quality",
-        "Median read quality": "Median read quality",
-    }
-
     _stat_types = ("summary", "quality")
 
     def __init__(self):
-        # Initialise the parent object
         super(MultiqcModule, self).__init__(
             name="nanoq",
             anchor="nanoq",
             href="https://github.com/nerdna/nanoq/",
-            info="reports read quality and length from nanopore sequencing data in fast{a,q}.{gz,bz2,xz} format.",
+            info="Reports read quality and length from nanopore sequencing data",
             doi="10.21105/joss.02991",
         )
 
         # Find and load any nanoq reports
-        self.nanoq_summary: Dict[str, Dict[str, float]] = {}
-        self.nanoq_data: Dict[str, Dict[str, float]] = {}
+        data_by_sample: Dict[str, Dict[str, float]] = {}
         for f in self.find_log_files("nanoq", filehandles=True):
-            self.parse_nanoq_log(f)
+            sample_data = self.parse_nanoq_log(f)
+            if sample_data:
+                data_by_sample[f["s_name"]] = sample_data
+
+                if f["s_name"] in data_by_sample:
+                    log.debug(f"Duplicate sample data found! Overwriting: {f['s_name']}")
+
+                self.add_data_source(f)
 
             # Superfluous function call to confirm that it is used in this module
             # Replace None with actual version if it is available
             self.add_software_version(None, f["s_name"])
 
         # Filter to strip out ignored sample names
-        self.nanoq_data = self.ignore_samples(self.nanoq_data)
-
-        if len(self.nanoq_data) == 0:
+        data_by_sample = self.ignore_samples(data_by_sample)
+        if len(data_by_sample) == 0:
             raise ModuleNoSamplesFound
 
-        log.info(f"Found {len(self.nanoq_data)} reports")
+        log.info(f"Found {len(data_by_sample)} reports")
 
         # Write parsed report data to a file
-        self.write_data_file(self.nanoq_data, "multiqc_nanoq")
+        self.write_data_file(data_by_sample, "multiqc_nanoq")
 
         # Add Nanoq summary to the general stats table
-        self.nanoq_general_stats()
+        self.add_table(data_by_sample)
 
         # Quality distribution Plot
-        self.reads_by_quality_plot()
+        self.reads_by_quality_plot(data_by_sample)
 
         # Read length distribution Plot
-        self.reads_by_length_plot()
+        self.reads_by_length_plot(data_by_sample)
 
-    def parse_nanoq_log(self, f) -> None:
+    def parse_nanoq_log(self, f) -> Dict[str, float]:
         """Parse output from nanoq"""
-        # Initialize dictionaries to store the parsed data
-        nanoq_summary: Dict[str, List[Any]] = {}
-        read_length_thresholds: Dict[str, List[Any]] = {}
-        read_quality_thresholds: Dict[str, List[Any]] = {}
-
-        # Helper function to parse summary part
-        def parse_summary(lines: List[str]) -> Dict[str, List[Any]]:
-            summary: Dict[str, List[Any]] = {"Metric": [], "Value": []}
-            for line in lines:
-                if ":" in line:
-                    metric, value = line.split(":", 1)
-                    summary["Metric"].append(metric.strip())
-                    summary["Value"].append(float(value.strip()))
-            return summary
-
-        # Helper function to parse thresholds part
-        def parse_thresholds(lines: List[str], threshold_type: str) -> Dict[str, List[Any]]:
-            thresholds: Dict[str, List[Any]] = {"Threshold": [], "Number of Reads": [], "Percentage": []}
-            for line in lines:
-                match = re.match(r">\s*(\d+)\s+(\d+)\s+(\d+\.\d+)%", line)
-                if match:
-                    threshold, num_reads, percentage = match.groups()
-                    thresholds["Threshold"].append(threshold + (threshold_type if threshold_type == "bp" else ""))
-                    thresholds["Number of Reads"].append(int(num_reads))
-                    thresholds["Percentage"].append(float(percentage))
-            return thresholds
+        stats: Dict[str, float] = dict()
 
         # Parse the file content
         segment = None
@@ -124,73 +87,83 @@ class MultiqcModule(BaseMultiqcModule):
             elif segment == "quality_thresholds":
                 quality_threshold_lines.append(line)
 
-        # Parse each part
-        nanoq_summary = parse_summary(summary_lines)
+        for line in summary_lines:
+            if ":" in line:
+                metric, value = line.split(":", 1)
+                stats[metric.strip()] = float(value.strip())
+
+        # Helper function to parse thresholds part
+        def parse_thresholds(lines: List[str], threshold_type: str) -> Dict[str, List[Any]]:
+            _thresholds: Dict[str, List[Any]] = {"Threshold": [], "Number of Reads": [], "Percentage": []}
+            for _line in lines:
+                match = re.match(r">\s*(\d+)\s+(\d+)\s+(\d+\.\d+)%", _line)
+                if match:
+                    _threshold, _num_reads, _percentage = match.groups()
+                    _thresholds["Threshold"].append(_threshold + (threshold_type if threshold_type == "bp" else ""))
+                    _thresholds["Number of Reads"].append(int(_num_reads))
+                    _thresholds["Percentage"].append(float(_percentage))
+            return _thresholds
+
         read_length_thresholds = parse_thresholds(length_threshold_lines, "bp")
         read_quality_thresholds = parse_thresholds(quality_threshold_lines, "")
 
-        # Prepare the final nanoq stats dictionary
-        nanoq_stats: Dict[str, float] = {}
-        for metric, value in zip(nanoq_summary["Metric"], nanoq_summary["Value"]):
-            if metric in self._KEYS_MAPPING:
-                nanoq_stats[self._KEYS_MAPPING[metric]] = value
-
         for threshold, num_reads in zip(read_length_thresholds["Threshold"], read_length_thresholds["Number of Reads"]):
-            nanoq_stats[f"Reads > {threshold}"] = num_reads
+            stats[f"Reads > {threshold}"] = num_reads
 
         for threshold, num_reads in zip(
             read_quality_thresholds["Threshold"], read_quality_thresholds["Number of Reads"]
         ):
-            nanoq_stats[f"Reads > Q{threshold}"] = num_reads
+            stats[f"Reads > Q{threshold}"] = num_reads
 
-        # Store nanoq_summary for general stats
-        self.nanoq_summary[f["s_name"]] = {k: v for k, v in zip(nanoq_summary["Metric"], nanoq_summary["Value"])}
+        return stats
 
-        self.save_data(f, nanoq_stats)
-
-    def save_data(self, f, nanoq_stats: Dict[str, float]) -> None:
-        """
-        Normalise fields and save parsed data.
-        """
-        if any(k.startswith("Reads > Q") for k in nanoq_stats):
-            stat_type = "quality"
-        else:
-            stat_type = "summary"
-
-        out_d = {f"{k}_{stat_type}": v for k, v in nanoq_stats.items()}
-
-        # Warn if we find overlapping data for the same sample
-        if f["s_name"] in self.nanoq_data:
-            if not set(self.nanoq_data[f["s_name"]].keys()).isdisjoint(out_d.keys()):
-                log.debug(f"Duplicate sample data found! Overwriting: {f['s_name']}")
-
-        self.nanoq_data.setdefault(f["s_name"], {}).update(out_d)
-
-        self.add_data_source(f)
-
-    def nanoq_general_stats(self) -> None:
+    def add_table(self, data_by_sample: Dict[str, Dict[str, float]]) -> None:
         """Nanoq General Stats Table"""
-        headers = {
+        headers: Dict[str, Dict] = {
             "Number of reads": {
-                "title": f"# Reads ({config.long_read_count_prefix})",
-                "description": f"Number of reads ({config.long_read_count_desc})",
+                "title": "Reads",
+                "description": "Number of reads",
                 "scale": "Greens",
-                "modify": lambda x: x * config.long_read_count_multiplier,
+                "shared_key": "read_count",
             },
             "Number of bases": {
-                "title": f"Total Bases ({config.base_count_prefix})",
-                "description": f"Total bases ({config.base_count_desc})",
+                "title": "Bases",
+                "description": "Total bases sequenced",
                 "scale": "Purples",
-                "modify": lambda x: x * config.base_count_multiplier,
+                "shared_key": "base_count",
             },
-            "N50 read length": {"title": "Read N50", "description": "N50 read length", "scale": "Blues"},
-            "Longest read": {"title": "Longest Read", "description": "Longest read length (bp)", "scale": "Oranges"},
-            "Shortest read": {"title": "Shortest Read", "description": "Shortest read length (bp)", "scale": "YlOrRd"},
-            "Mean read length": {"title": "Mean Length", "description": "Mean read length (bp)", "scale": "PuBuGn"},
+            "N50 read length": {
+                "title": "Read N50",
+                "description": "N50 read length",
+                "scale": "Blues",
+                "suffix": "bp",
+                "format": "{:,.0f}",
+            },
+            "Longest read": {
+                "title": "Longest Read",
+                "description": "Longest read length",
+                "suffix": "bp",
+                "scale": "Oranges",
+                "format": "{:,.0f}",
+            },
+            "Shortest read": {
+                "title": "Shortest Read",
+                "description": "Shortest read length",
+                "suffix": "bp",
+                "scale": "YlOrRd",
+                "format": "{:,.0f}",
+            },
+            "Mean read length": {
+                "title": "Mean Length",
+                "description": "Mean read length",
+                "suffix": "bp",
+                "scale": "PuBuGn",
+            },
             "Median read length": {
                 "title": "Median Length",
                 "description": "Median read length (bp)",
                 "scale": "RdYlBu",
+                "format": "{:,.0f}",
             },
             "Mean read quality": {
                 "title": "Mean Qual",
@@ -204,49 +177,46 @@ class MultiqcModule(BaseMultiqcModule):
             },
         }
 
+        self.add_section(
+            name="Nanoq Summary",
+            anchor="nanoq-summary",
+            description="Statistics from Nanoq reports",
+            plot=table.plot(
+                data_by_sample,
+                headers,
+                pconfig=TableConfig(
+                    id="nanoq_table",
+                    title="Nanoq Summary",
+                ),
+            ),
+        )
+
+        general_stats_headers = deepcopy(headers)
+        for k, h in general_stats_headers.items():
+            h["hidden"] = True
+        general_stats_headers["Number of reads"]["hidden"] = False
+        general_stats_headers["N50 read length"]["hidden"] = False
+
         # Add columns to the general stats table
-        self.general_stats_addcols(self.nanoq_summary, headers)
+        self.general_stats_addcols(data_by_sample, general_stats_headers)
 
-    def reads_by_quality_plot(self) -> None:
-        def _get_total_reads(_data_dict: Dict[str, Any]) -> Any:
-            for _stat_type in self._stat_types:
-                total_key = f"Number of reads_{_stat_type}"
-                if total_key in _data_dict:
-                    return _data_dict[total_key]
-            return None
-
+    def reads_by_quality_plot(self, data_by_sample: Dict[str, Dict[str, float]]) -> None:
         # Get data for plot
-        data: Dict[str, Dict[str, float]] = {}
-        cats: List[str] = []
-        for name, d in self.nanoq_data.items():
-            # Iterate over available types and keep categories in same plot
-            for stat_type in self._stat_types:
-                reads_by_q = {k: v for k, v in d.items() if re.match(r"Reads > Q\d+_" + stat_type, k)}
-                if len(reads_by_q) == 0:
-                    continue
+        lineplot_data: Dict[str, Dict[int, float]] = defaultdict(dict)
+        for name, d in data_by_sample.items():
+            reads_by_q = {k: v for k, v in d.items() if k.startswith("Reads > Q")}
+            if len(reads_by_q) == 0:
+                continue
 
-                data.setdefault(name, {})
-
-                total_reads = _get_total_reads(d)
-                for k, v in reads_by_q.items():
-                    threshold = k.split("_")[0]
-                    data[name][threshold] = v / total_reads * 100
-                    cats.append(threshold)
-
-        if len(data) == 0:
-            return None
+            total_reads = d["Number of reads"]
+            for k, v in reads_by_q.items():
+                threshold = int(k.split("> Q")[1])
+                lineplot_data[name][threshold] = v / total_reads * 100
 
         # Plot
-        cats = sorted(cats, key=lambda x: int(re.sub(r"[^\d.]", "", x)))
-        pconfig = {
-            "id": "nanoq_plot_quality",
-            "title": "Read Quality",
-            "ylab": "# Reads",
-            "cpswitch_counts_label": "Number of Reads",
-        }
         self.add_section(
-            name="Reads Quality",
-            anchor="nanoq-plot-quality",
+            name="Read quality",
+            anchor="nanoq_plot_quality",
             description="Read counts categorised by read quality (Phred score).",
             helptext="""
             Sequencing machines assign each generated read a quality score using the
@@ -254,50 +224,37 @@ class MultiqcModule(BaseMultiqcModule):
             The phred score represents the liklelyhood that a given read contains errors.
             High quality reads have a high score.
             """,
-            plot=bargraph.plot(data, cats, pconfig),
+            plot=linegraph.plot(
+                lineplot_data,
+                linegraph.LinePlotConfig(
+                    id="nanoq_plot_quality_plot",
+                    title="Nanoq: read qualities",
+                ),
+            ),
         )
 
-    def reads_by_length_plot(self) -> None:
-        def _get_total_reads(_data_dict: Dict[str, Any]) -> Any:
-            for _stat_type in self._stat_types:
-                total_key = f"Number of reads_{_stat_type}"
-                if total_key in _data_dict:
-                    return _data_dict[total_key]
-            return None
-
+    def reads_by_length_plot(self, data_by_sample: Dict[str, Dict[str, float]]) -> None:
         # Get data for plot
-        data: Dict[str, Dict[str, float]] = {}
-        cats: List[str] = []
-        for name, d in self.nanoq_data.items():
-            # Iterate over available types and keep categories in same plot
-            for stat_type in self._stat_types:
-                reads_by_l = {k: v for k, v in d.items() if re.match(r"Reads > \d+bp_" + stat_type, k)}
-                if len(reads_by_l) == 0:
-                    continue
+        linegraph_data: Dict[str, Dict[int, float]] = defaultdict(dict)
+        for name, d in data_by_sample.items():
+            reads_by_l = {k: v for k, v in d.items() if re.match(r"Reads > \d+bp", k)}
+            if len(reads_by_l) == 0:
+                continue
 
-                data.setdefault(name, {})
-
-                total_reads = _get_total_reads(d)
-                for k, v in reads_by_l.items():
-                    threshold = k.split("_")[0]
-                    data[name][threshold] = v / total_reads * 100
-                    cats.append(threshold)
-
-        if len(data) == 0:
-            return None
-
-        # Plot
-        cats = sorted(cats, key=lambda x: int(re.sub(r"[^\d.]", "", x)))
-        pconfig = {
-            "id": "nanoq_plot_length",
-            "title": "Read Length",
-            "ylab": "# Reads",
-            "cpswitch_counts_label": "Number of Reads",
-        }
+            for k, v in reads_by_l.items():
+                threshold = int(k.split(" > ")[1].strip("bp"))
+                linegraph_data[name][threshold] = v
 
         self.add_section(
-            name="Reads Length",
-            anchor="nanoq-plot-length",
+            name="Read lengths",
+            anchor="nanoq_plot_length",
             description="Read counts categorised by read length.",
-            plot=bargraph.plot(data, cats, pconfig),
+            plot=linegraph.plot(
+                linegraph_data,
+                linegraph.LinePlotConfig(
+                    id="nanoq_plot_length_plot",
+                    title="Nanoq: read lengths",
+                    xlog=True,
+                ),
+            ),
         )
