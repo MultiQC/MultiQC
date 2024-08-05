0b7f04f3d33778743dad9ae775787bb238309392
Ezra Greenberg <120955867+egreenberg7@users.noreply.github.com>
2024-08-05 15:23:48 +0300

    Bracken: fix bug when direct reads not classified (#2738)
    * Fix Bracken bug when direct reads not classified
    
    * Prevent error when no samples are found
    
    * Fix kraken vs Bracken for logger
    
    * Fix formatting
    
    * Revert "Fix formatting"
    
    This reverts commit 403f59bfe1b58432810519c50c429bf1b78294aa.
    
    * Revert "Fix kraken vs Bracken for logger"
    
    This reverts commit 995fdaa53fee0ca2aeca1bf069122a53c4b9808b.
    
    * Fix
    
    ---------
    
    Co-authored-by: Vlad Savelyev <vladislav.sav@gmail.com>

--- multiqc/modules/bracken/bracken.py
+++ multiqc/modules/bracken/bracken.py
@@ -1,6 +1,7 @@
 import logging
 
 from multiqc.modules.kraken import MultiqcModule as KrakenModule
+from multiqc.base_module import ModuleNoSamplesFound
 
 log = logging.getLogger(__name__)
 
@@ -34,3 +35,17 @@ class MultiqcModule(KrakenModule):
             doi="10.7717/peerj-cs.104",
             sp_key="bracken",
         )
+
+    def sample_total_readcounts(self):
+        """
+        Get the total read counts for each sample, using the fact that all reads are assigned to root
+        """
+        total_all_samples = 0
+        for s_name, data in self.kraken_raw_data.items():
+            self.kraken_sample_total_readcounts[s_name] = data[0]["counts_rooted"]
+            total_all_samples += self.kraken_sample_total_readcounts[s_name]
+
+        # Check that we had some counts for some samples, exit if not
+        if total_all_samples == 0:
+            log.warning("No samples had any reads")
+            raise ModuleNoSamplesFound
