fa3b88425513015a621d70252dbbce6b8aa598da
Vlad Savelyev <vladislav.sav@gmail.com>
2024-10-10 00:07:23 +0200

    Add anthropic

--- multiqc/core/ai.py
+++ multiqc/core/ai.py
@@ -12,7 +12,7 @@ class LLMClient:
     def __init__(self, model: str):
         self.model = model
 
-    def chat(self, message: List[str]) -> str:
+    def chat(self, data_items: List[str]) -> str:
         raise NotImplementedError("Not implemented")
 
 
@@ -22,10 +22,10 @@ You are given quality control data from a bioinformatics workflow.
 
 Your task is to eyeball the data and give a very short and helpful summary of the results \
 to the customer. The summary should only warn about what a human would otherwise miss or leave unnoticed. \
-Be specific: it's okay to just say that the data is good, if no items stand out as problematic.
+Be specific: it's okay to simply say that all data is good, if no items are found to stand out as problematic.
 
 Please use HTML formatting to make the summary visually attractive. Use colors to bring user's attention \
-to really important pieces, but do not make them very bright.
+to really important pieces, but do not use a very bright color palette. Make highlighting very subtle.
 
 Do no add a header or any other formatting apart from simple HTML.
 
@@ -49,12 +49,40 @@ class OpenAIClient(LLMClient):
         return ai_message.content
 
 
+class AnthropicClient(LLMClient):
+    def __init__(self, model: str, token: str):
+        try:
+            import anthropic  # type: ignore
+        except ImportError:
+            raise ImportError(
+                "anthropic package is not installed, make sure to install MultiQC with `pip install multiqc[anthropic]`"
+            )
+
+        super().__init__(model)
+        self.client = anthropic.Anthropic(api_key=token)
+        self.history: List = []
+
+    def chat(self, data_items: List[str]) -> Optional[str]:
+        self.history += [{"role": "user", "content": data_item} for data_item in data_items]
+
+        response = self.client.messages.create(model=self.model, messages=self.history)
+        ai_message = response.content[0]
+        self.history.append(ai_message)
+        return ai_message.text
+
+
 def _get_llm_client() -> Optional[LLMClient]:
     openai_token = os.environ.get("OPENAI_API_KEY")
     if openai_token:
         return OpenAIClient(
             model=os.environ.get("OPENAI_MODEL", "gpt-4o"),
             token=openai_token,
+        )
+    anthropic_token = os.environ.get("ANTHROPIC_API_KEY")
+    if anthropic_token:
+        return AnthropicClient(
+            model=os.environ.get("ANTHROPIC_MODEL", "claude-3-5-sonnet-20240620"),
+            token=anthropic_token,
         )
 
     return None
