09290250a59c3be08ff700fbb72cd34d9e32887e
Vlad Savelyev <vladislav.sav@gmail.com>
2024-12-13 13:34:04 +0100

    User features as bars, colors as groups

--- multiqc/modules/percolator/percolator.py
+++ multiqc/modules/percolator/percolator.py
@@ -1,12 +1,14 @@
+from collections import defaultdict
 import logging
 from statistics import median
 
 from multiqc.base_module import BaseMultiqcModule, ModuleNoSamplesFound
 from multiqc.plots import bargraph
+from multiqc.utils import mqc_colour
 
 log = logging.getLogger(__name__)
 
-FEATURE_NAME_DICT = {
+GROUP_TO_FEATURE = {
     "psm_file_combined": [
         "MS:1002255",
         "MS:1002252",
@@ -133,6 +135,14 @@ FEATURE_NAME_DICT = {
     ],
 }
 
+FEATURE_TO_GROUP = {feature: group for group, features in GROUP_TO_FEATURE.items() for feature in features}
+
+GROUP_NAME = {
+    "psm_file_combined": "PSM File",
+    "ms2pip": "MS2PIP",
+    "deeplc": "DeepLC",
+}
+
 
 class MultiqcModule(BaseMultiqcModule):
     """
@@ -150,76 +160,73 @@ class MultiqcModule(BaseMultiqcModule):
         )
 
         # Parse logs
-        self.percolator = dict()
+        self.data_by_sample = dict()
         for f in self.find_log_files("percolator", filehandles=True):
             self.parse_percolator(f)
 
         # Filter to strip out ignored sample names
-        self.percolator = self.ignore_samples(self.percolator)
+        self.data_by_sample = self.ignore_samples(self.data_by_sample)
 
-        if len(self.percolator) == 0:
+        if len(self.data_by_sample) == 0:
             raise ModuleNoSamplesFound
-        log.info(f"Found {len(self.percolator)} logs")
-
-        # Summarize the data from all samples by computing the median across all samples for each feature
-        self.summarized_data = {group: dict() for group in FEATURE_NAME_DICT.keys()}
-        self.compute_median()
-        self.write_data_file(self.summarized_data, "multiqc_percolator_summarized")
+        log.info(f"Found {len(self.data_by_sample)} logs")
 
         # Superfluous function call to confirm that it is used in this module
         # Replace None with an actual version if it is available
         self.add_software_version(None)
 
-        descr_plot = """The bar plot illustrates the median weights for each feature, 
-        which have been assigned to one of three categories: psm_file_combined, ms2pip and deeplc."""
-        helptext = """For each feature, the associated weight is the median value calculated over all input samples. 
-        Prior to computing the median, the absolute value was calculated, 
-        as only the magnitude is of interest in this context."""
-        self.add_section(plot=self.percolator_barplot(), helptext=helptext, description=descr_plot)
+        # Summarize the data from all samples by computing the median across all samples for each feature
+        values_by_feature = defaultdict(list)
+        for sample_dict in self.data_by_sample.values():
+            for feature, value in sample_dict.items():
+                values_by_feature[feature].extend(value)
+        # Since our bars are features and not samples, computing the median of each feature across samples
+        median_weights_by_feature = dict()
+        for feature, values in values_by_feature.items():
+            median_value = median(values)
+            median_weights_by_feature[feature] = median_value
+        # Sort the features by the median weight
+        median_weights_by_feature = dict(sorted(median_weights_by_feature.items(), key=lambda x: -x[1], reverse=True))
+        barplot_data = {
+            feature: {f"{GROUP_NAME[FEATURE_TO_GROUP[feature]]}": median_value}
+            for feature, median_value in median_weights_by_feature.items()
+        }
 
-        self.write_data_file(self.percolator, "multiqc_percolator_full")
+        """Plot the bar plot of the median weight for each feature."""
+        self.add_section(
+            anchor="percolator_median_feature_weights",
+            helptext="""The bar plot illustrates the median weights for each feature, 
+        which have been assigned to one of three categories: psm_file_combined, ms2pip and deeplc.""",
+            description="""For each feature, the associated weight is the median value calculated over all input samples.""",
+            plot=bargraph.plot(
+                barplot_data,
+                # cats=cats,
+                pconfig={
+                    "id": "percolator_plot",
+                    "title": "Percolator: Median Feature Weights",
+                    "sort_samples": False,
+                    "hide_zero_cats": False,
+                },
+            ),
+        )
+
+        self.write_data_file(self.data_by_sample, "multiqc_percolator_barplot")
 
     def parse_percolator(self, file):
         """Extract the normalized weights for each feature from the percolator logs."""
         s_name = file["s_name"]
         lines = file["f"].readlines()
         header = lines[0].strip().split("\t")
-        if s_name in self.percolator:
+        if s_name in self.data_by_sample:
             log.debug(f"Duplicate sample name found! Overwriting: {s_name}")
-        self.percolator[s_name] = {feature: list() for feature in header}
+        self.data_by_sample[s_name] = {feature: list() for feature in header}
         # Select every third line starting from the first data line after the header (normalized values)
         for i in range(1, len(lines), 3):
             line = lines[i].strip().split("\t")
             # Take the absolute value of each element
-            line = [abs(float(x)) for x in line]
+            line = [float(x) for x in line]
             assert len(line) == len(header)
             # For each sample, collect all values for each feature in a list
             for elem in range(len(line)):
-                self.percolator[s_name][header[elem]].append(line[elem])
+                self.data_by_sample[s_name][header[elem]].append(line[elem])
         self.add_data_source(file, s_name)
-
-    def compute_median(self):
-        """Compute the median weight for each feature across all samples."""
-        # Collect the values for each feature across all samples
-        for sample_dict in self.percolator.values():
-            for feature, value in sample_dict.items():
-                group = [group for group, features in FEATURE_NAME_DICT.items() if feature in features]
-                assert len(group) == 1
-                group = group[0]
-                if feature not in self.summarized_data[group]:
-                    self.summarized_data[group][feature] = list()
-                self.summarized_data[group][feature].extend(value)
-        # Compute the median for each feature
-        for group in self.summarized_data.keys():
-            for feature, values in self.summarized_data[group].items():
-                median_value = median(values)
-                self.summarized_data[group][feature] = median_value
-
-    def percolator_barplot(self):
-        """Plot the bar plot of the median weight for each feature."""
-        plot_config = {
-            "id": "percolator_plot",
-            "title": "Percolator: Median Feature Weights",
-        }
-
-        return bargraph.plot(self.summarized_data, pconfig=plot_config)
