ecd1b108a4971684958ea68f4fb14d16796d5ece
Vlad Savelyev <vladislav.sav@gmail.com>
2024-12-16 23:01:44 +0100

    Clean up

--- multiqc/modules/mgikit/mgikit.py
+++ multiqc/modules/mgikit/mgikit.py
@@ -1,34 +1,21 @@
-#!/usr/bin/env python
-
-"""MultiQC example plugin module"""
-
-from __future__ import print_function
-from collections import OrderedDict
 from multiqc import config
 from multiqc.plots import table, bargraph
 from multiqc.base_module import BaseMultiqcModule
 from multiqc.base_module import ModuleNoSamplesFound
 import logging
 
-# Initialise the main MultiQC logger
-log = logging.getLogger("multiqc")
+log = logging.getLogger(__name__)
 
 
 class MultiqcModule(BaseMultiqcModule):
     def __init__(self):
-        # Initialise the parent module Class object
         super(MultiqcModule, self).__init__(
-            # name="MGIKIT",
-            target="mgikit",
+            name="mgikit",
             anchor="mgikit",
             href="https://github.com/sagc-bioinformatics/mgikit",
-            info="can be used to demultiplex demultiplex FASTQ files from an MGI sequencing instrument for downstream analysis.",
+            info="Demultiplexes FASTQ files from an MGI sequencing instrument",
             doi="https://doi.org/10.1101/2024.01.09.574938",
         )
-
-        # Halt execution if we've disabled the plugin
-        if config.kwargs.get("mgikit_disable_plugin", True):
-            return None
 
         for f in self.find_log_files("mgikit"):
             self.add_data_source(f)
@@ -65,15 +52,10 @@ class MultiqcModule(BaseMultiqcModule):
         show_all_samples = not config.kwargs.get("keep_core_samples", False)
         if not show_all_samples:
             log.warning("Undetermined and Ambiguous cases will be eliminated in this report!")
-        # Find and load any input files for this module
 
-        decimal_positions = config.kwargs.get("decimal_positions", 2)
-        decimal_positions = str(decimal_positions)
-        ################
-        # General statistics
-        general_info_logs = self.find_log_files("mgikit/mgi_general_info")
+        decimal_positions = str(config.kwargs.get("decimal_positions", 2))
         mgi_general_statistics = {}
-        columns_headers = OrderedDict()
+        columns_headers = {}
         columns_headers["M Clusters"] = {
             "namespace": "mgikit",
             "title": "M Total Clusters",  # Short title, table column title
@@ -130,13 +112,14 @@ class MultiqcModule(BaseMultiqcModule):
             "suffix": "%",  # Suffix for value (eg. '%')
             "format": "{:,." + decimal_positions + "f}",
         }
+
         file_cnt = 0
-        for f in general_info_logs:
+        for f in self.find_log_files("mgikit/mgi_general_info"):
             file_cnt += 1
             collect_data = False
             lines = f["f"].splitlines()
             line_itr = 0
-            header = None
+            header = []
             while line_itr < len(lines):
                 line = lines[line_itr]
                 if collect_data:
@@ -154,21 +137,17 @@ class MultiqcModule(BaseMultiqcModule):
                     header = lines[line_itr].split("\t")
                 line_itr += 1
 
-        # print(mgi_general_statistics)
-        if len(mgi_general_statistics.keys()) > 0:
-            self.general_stats_addcols(mgi_general_statistics, columns_headers)
-            self.write_data_file(mgi_general_statistics, "multiqc_mgikit_general")
-        else:
+        if len(mgi_general_statistics.keys()) == 0:
             raise ModuleNoSamplesFound
-
         if file_cnt > 0:
-            log.info("{} general information log files (*.mgikit.general) were loaded!".format(file_cnt))
+            log.info(f"{file_cnt} general information log files (*.mgikit.general) were loaded")
 
-        # lane statistics
-        general_info_logs = self.find_log_files("mgikit/mgi_general_info")
+        self.general_stats_addcols(mgi_general_statistics, columns_headers)
+        self.write_data_file(mgi_general_statistics, "multiqc_mgikit_general")
+
+        # Lane statistics
         mgi_lane_statistics = {}
-        columns_headers = OrderedDict()
-
+        columns_headers = {}
         columns_headers["Mb Total Yield"] = {
             "namespace": "mgikit",
             "title": "Mb Total Yield",  # Short title, table column title
@@ -219,7 +198,7 @@ class MultiqcModule(BaseMultiqcModule):
             "format": "{:,." + decimal_positions + "f}",
         }
 
-        for f in general_info_logs:
+        for f in self.find_log_files("mgikit/mgi_general_info"):
             lines = f["f"].splitlines()
             for line_itr in range(len(lines)):
                 line = lines[line_itr]
@@ -249,13 +228,12 @@ class MultiqcModule(BaseMultiqcModule):
                 ),
             )
 
-        # statistics about matching reads
+        # Statistics about matching reads
         mgi_sample_reads_data = dict()
         mgi_lane_read_data = {}
         mgi_lane_sample_read_data = {}
-        sample_read_logs = self.find_log_files("mgikit/mgi_sample_reads")
         file_cnt = 0
-        for f in sample_read_logs:
+        for f in self.find_log_files("mgikit/mgi_sample_reads"):
             file_cnt += 1
             log_details = f["fn"].split(".")
             if len(log_details[-4]) == 0:
@@ -263,7 +241,6 @@ class MultiqcModule(BaseMultiqcModule):
             else:
                 curr_label = log_details[-4] + "-" + log_details[-3]
 
-            # print(log_details)
             mgi_lane_read_data[curr_label] = {}
             mgi_lane_sample_read_data[curr_label] = {}
             lines = f["f"].splitlines()
@@ -281,7 +258,6 @@ class MultiqcModule(BaseMultiqcModule):
 
                 if not show_all_samples and vals[0] in [undetermined_label, ambiguous_label]:
                     continue
-                # print(vals[0], mgi_lane_read_data)
                 if vals[0] in mgi_lane_sample_read_data[curr_label].keys():
                     log.warning("Same sample ({}) appears twice in the same log file!".format(vals[0]))
 
@@ -299,24 +275,21 @@ class MultiqcModule(BaseMultiqcModule):
                     if vals[0] not in [undetermined_label, ambiguous_label]:
                         mgi_lane_read_data[curr_label][header[i]] += int(vals[i])
                     mgi_lane_sample_read_data[curr_label][vals[0]][header[i]] += int(vals[i])
-                # print(mgi_sample_reads_data)
         if len(mgi_sample_reads_data) == 0:
             raise ModuleNoSamplesFound
 
         mgi_sample_reads_data_filtered = None
         if file_cnt > 0:
-            log.info("{} Read information log files (*.mgikit.info) were loaded!".format(file_cnt))
+            log.info(f"{file_cnt} read information log files (*.mgikit.info) were loaded")
 
             # Filter out samples matching ignored sample names
             mgi_sample_reads_data_filtered = self.ignore_samples(mgi_sample_reads_data)
-            cats = OrderedDict()
+            cats = {}
             for i in range(1, len(header)):
                 cats[header[i]] = {"name": header[i], "color": mgikit_colors[i - 1]}
 
             cats[undetermined_label] = {"name": undetermined_label, "color": mgikit_colors[len(header) - 1]}
             cats[ambiguous_label] = {"name": ambiguous_label, "color": mgikit_colors[len(header)]}
-            # print(cats)
-            # print(mgi_lane_read_data)
             pconfig = {
                 "id": "mgi_lane_read_plot",
                 "title": module_name + ": Clusters by lane",
@@ -325,8 +298,6 @@ class MultiqcModule(BaseMultiqcModule):
                 "hide_zero_cats": True,
             }
 
-            # print(mgi_lane_read_data)
-            # print(cats)
             lane_read_plt = bargraph.plot(mgi_lane_read_data, cats, pconfig=pconfig)
 
             # Add a report section with the line plot
@@ -341,59 +312,57 @@ class MultiqcModule(BaseMultiqcModule):
             # Nothing found - raise a UserWarning to tell MultiQC
             if not mgi_sample_reads_data_filtered or len(mgi_sample_reads_data_filtered.keys()) == 0:
                 log.info("No sample left after filteration!")
-                raise UserWarning
-            else:
-                pconfig = {
-                    "id": "mgi_sample_read_plot",
-                    "title": module_name + ": Clusters by sample",
-                    "xlab": "Sample",
-                    "ylab": "Matching reads",
-                    "hide_zero_cats": True,
-                }
+                raise ModuleNoSamplesFound
 
-                sample_read_plt = bargraph.plot(mgi_sample_reads_data_filtered, cats, pconfig=pconfig)
+            pconfig = {
+                "id": "mgi_sample_read_plot",
+                "title": module_name + ": Clusters by sample",
+                "xlab": "Sample",
+                "ylab": "Matching reads",
+                "hide_zero_cats": True,
+            }
 
-                # Add a report section with the line plot
-                self.add_section(
-                    name="Clusters by sample",
-                    anchor="mgikit-second",
-                    description="Number of reads per sample.",
-                    helptext="Perfect index reads are those that do not have a single mismatch. All samples are aggregated across lanes combinned. Undetermined reads are treated as a separate sample.",
-                    plot=sample_read_plt,
-                )
+            sample_read_plt = bargraph.plot(mgi_sample_reads_data_filtered, cats, pconfig=pconfig)
 
-                if not brief_report:
-                    cntr = 0
-                    for lane in sorted(mgi_lane_read_data.keys()):
-                        cntr += 1
-                        pconfig = {
-                            "id": "mgi_sample_read_plot" + str(cntr),
-                            "title": module_name + ": Clusters by sample for Lane (" + lane + ")",
-                            "xlab": "Sample",
-                            "ylab": "Matching reads",
-                            "hide_zero_cats": True,
-                        }
+            # Add a report section with the line plot
+            self.add_section(
+                name="Clusters by sample",
+                anchor="mgikit-second",
+                description="Number of reads per sample.",
+                helptext="Perfect index reads are those that do not have a single mismatch. All samples are aggregated across lanes combinned. Undetermined reads are treated as a separate sample.",
+                plot=sample_read_plt,
+            )
 
-                        sample_read_plt = bargraph.plot(mgi_lane_sample_read_data[lane], cats, pconfig=pconfig)
+            if not brief_report:
+                cntr = 0
+                for lane in sorted(mgi_lane_read_data.keys()):
+                    cntr += 1
+                    pconfig = {
+                        "id": "mgi_sample_read_plot" + str(cntr),
+                        "title": module_name + ": Clusters by sample for Lane (" + lane + ")",
+                        "xlab": "Sample",
+                        "ylab": "Matching reads",
+                        "hide_zero_cats": True,
+                    }
 
-                        # Add a report section with the line plot
-                        self.add_section(
-                            name="Clusters by sample for Lane: " + lane,
-                            anchor="mgikit-lane-sample_reads-" + str(cntr),
-                            description="This plot shows the number of reads per sample for Lane: " + lane,
-                            helptext="This longer string (can be **markdown**) helps explain how to interpret the plot",
-                            plot=sample_read_plt,
-                        )
+                    sample_read_plt = bargraph.plot(mgi_lane_sample_read_data[lane], cats, pconfig=pconfig)
+
+                    # Add a report section with the line plot
+                    self.add_section(
+                        name="Clusters by sample for Lane: " + lane,
+                        anchor="mgikit-lane-sample_reads-" + str(cntr),
+                        description="This plot shows the number of reads per sample for Lane: " + lane,
+                        helptext="This longer string (can be **markdown**) helps explain how to interpret the plot",
+                        plot=sample_read_plt,
+                    )
 
         # Check undetermined barcodes
-        undetermined_barcode_logs = self.find_log_files("mgikit/mgi_undetermined_barcode")
         mgi_undetermined_barcode_data = {}
         mgi_undetermined_barcode_cnt = {}
 
         file_cnt = 0
         cat_lane = {}
-        for f in undetermined_barcode_logs:
-            # print(f['fn'])
+        for f in self.find_log_files("mgikit/mgi_undetermined_barcode"):
             file_cnt += 1
             log_details = f["fn"].split(".")
             if len(log_details[-4]) == 0:
@@ -420,7 +389,7 @@ class MultiqcModule(BaseMultiqcModule):
                     mgi_undetermined_barcode_cnt[vals[0]] += int(vals[1])
 
         if file_cnt > 0:
-            log.info("{} Undetermined barcode log files (*.mgikit.undetermined_barcode) were loaded!".format(file_cnt))
+            log.info(f"{file_cnt} undetermined barcode log files (*.mgikit.undetermined_barcode) were loaded")
 
             pconfig = {
                 "id": "mgi_lane_undetermined_plot",
@@ -430,16 +399,15 @@ class MultiqcModule(BaseMultiqcModule):
                 "hide_zero_cats": True,
             }
 
-            sorted_dic = OrderedDict(
-                [
-                    (k[0], mgi_undetermined_barcode_data[k[0]])
-                    for k in sorted(
+            sorted_dic = {
+                k[0]: mgi_undetermined_barcode_data[k[0]]
+                for k in list(
+                    sorted(
                         mgi_undetermined_barcode_cnt.items(),
                         key=lambda item: -int(item[1]),
-                    )[:visualisation_threshold]
-                ]
-            )
-
+                    )
+                )[:visualisation_threshold]
+            }
             lane_barcode_plt = bargraph.plot(sorted_dic, cat_lane, pconfig=pconfig)
 
             # Add a report section with the line plot
@@ -449,16 +417,12 @@ class MultiqcModule(BaseMultiqcModule):
                 description="Count of the top twenty five most abundant undetermined barcodes by lanes.",
                 plot=lane_barcode_plt,
             )
-        # else:
-        #    log.warning("No log files (*.mgikit.undetermined_barcode) was found!")
 
         # Check ambiguous barcodes
-        ambiguous_barcode_logs = self.find_log_files("mgikit/mgi_ambiguous_barcode")
         mgi_ambiguous_barcode_data = {}
         file_cnt = 0
         cat_lane = {}
-        for f in ambiguous_barcode_logs:
-            # print(f['fn'])
+        for f in self.find_log_files("mgikit/mgi_ambiguous_barcode"):
             file_cnt += 1
             log_details = f["fn"].split(".")
             if len(log_details[-4]) == 0:
@@ -472,20 +436,19 @@ class MultiqcModule(BaseMultiqcModule):
                     continue
                 mgi_ambiguous_barcode_data[curr_label + ":" + vals[0]] = {
                     "Frequency": int(vals[1]),
-                    # "Samples": vals[2],
                 }
 
         if file_cnt > 0:
-            log.info("{} Ambiguous barcode log files (*.mgikit.ambiguous_barcode) were loaded!".format(file_cnt))
-            sorted_dic = OrderedDict(
-                [
-                    (k, v)
-                    for k, v in sorted(
+            log.info(f"{file_cnt} ambiguous barcode log files (*.mgikit.ambiguous_barcode) were loaded")
+            sorted_dic = {
+                k: v
+                for k, v in list(
+                    sorted(
                         mgi_ambiguous_barcode_data.items(),
                         key=lambda item: -int(item[1]["Frequency"]),
                     )
-                ][:visualisation_threshold]
-            )
+                )[:visualisation_threshold]
+            }
 
             # Add a report section with the line plot
             columns_headers = {}
@@ -518,5 +481,3 @@ class MultiqcModule(BaseMultiqcModule):
                     },
                 ),
             )
-        # else:
-        #    log.info("No log files (*.mgikit.ambiguous_barcode) was found!")
