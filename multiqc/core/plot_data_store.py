630f891e5b9d20e1f0232319fb160feb81f7475a
Vlad Savelyev <vladislav.sav@gmail.com>
2025-04-30 14:52:30 +0200

    Also make sure the file log is complete

--- multiqc/core/plot_data_store.py
+++ multiqc/core/plot_data_store.py
@@ -229,7 +229,7 @@ def save_report_metadata() -> None:
         # Write to file
         metadata_df.to_parquet(parquet_file, compression="gzip")
 
-    logger.debug("Saved report metadata to parquet file")
+    logger.debug(f"Saved parquet file {parquet_file}")
 
 
 def _update_parquet(df: pd.DataFrame, anchor: Anchor) -> None:
