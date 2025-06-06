91a3be87085c98bb2b5e239554b9a4c722f6fe8e
Vlad Savelyev <vladislav.sav@gmail.com>
2025-06-06 16:49:01 +0200

    Fix typing

--- multiqc/core/special_case_modules/custom_content.py
+++ multiqc/core/special_case_modules/custom_content.py
@@ -340,8 +340,8 @@ def custom_module_classes() -> List[BaseMultiqcModule]:
         # Initialise this new module class and append to list
         else:
             # Is this file asking to be a sub-section under a parent section?
-            mod_id = ModuleId(ccdict.config.get("parent_id", ccdict.config.get("id", mod_id)))
-            section_id: SectionId = SectionId(ccdict.config.get("section_id", mod_id))
+            mod_id = cast(ModuleId, ccdict.config.get("parent_id", ccdict.config.get("id", mod_id)))
+            section_id: SectionId = cast(SectionId, ccdict.config.get("section_id", mod_id))
 
             mod_anchor: Optional[Anchor] = None
             if "parent_anchor" in ccdict.config:
