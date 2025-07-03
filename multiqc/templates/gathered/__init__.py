0f883f16a335fcd72d52109c688b587f94edc5b3
Phil Ewels <phil.ewels@seqera.io>
2025-07-03 14:23:45 +0200

    Updates for other templates that use default as a base

--- multiqc/templates/gathered/__init__.py
+++ multiqc/templates/gathered/__init__.py
@@ -9,7 +9,16 @@ module.
 """
 
 import os
+import importlib
 
 template_dir = os.path.dirname(__file__)
 template_parent = "default"
 base_fn = "base.html"
+
+# Import template functions from parent
+try:
+    parent_mod = importlib.import_module(f"multiqc.templates.{template_parent}")
+    if hasattr(parent_mod, "template_functions"):
+        template_functions = parent_mod.template_functions
+except ImportError:
+    pass
--- multiqc/templates/geo/__init__.py
+++ multiqc/templates/geo/__init__.py
@@ -11,8 +11,17 @@ http://code.divshot.com/geo-bootstrap/
 """
 
 import os
+import importlib
 
 template_parent = "default"
 
 template_dir = os.path.dirname(__file__)
 base_fn = "base.html"
+
+# Import template functions from parent
+try:
+    parent_mod = importlib.import_module(f"multiqc.templates.{template_parent}")
+    if hasattr(parent_mod, "template_functions"):
+        template_functions = parent_mod.template_functions
+except ImportError:
+    pass
