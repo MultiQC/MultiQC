3ad9b1f551351621c2aceb57adf35acb318892b3
Arthur Schönfeld <arthurschoenfeld@laborberlin.com>
2025-09-01 12:47:54 +0200

    deacon

--- multiqc/modules/deacon/deacon.py
+++ multiqc/modules/deacon/deacon.py
@@ -2,10 +2,10 @@ import logging
 import json 
 
 from multiqc.base_module import BaseMultiqcModule
-from multiqc.plots import bargraph #für Balkendiagramm
+from multiqc.plots import bargraph 
 
 log = logging.getLogger(__name__) 
-#Logger
+#logger
 
 class MultiqcModule(BaseMultiqcModule):
     def __init__(self):
@@ -18,41 +18,45 @@ class MultiqcModule(BaseMultiqcModule):
     )
         
         self.deacon_data = dict() 
-        #self. verhindert lokale Variable -> für betrachtetes Objekt gültig, nicht für def __init__
-        for f in self.find_log_files("deacon", filehandles = True): #filhandles = True sagt, dass Multiqc Dateien öffnen soll
-            sample_name = self.clean_s_name(f["s_name"], f) #f["s_name"] -> Samplename; self.clean_s_name -> normiert den Namen(Dateipfade entfernen etc.), verhindert, dass zwei Dateien gleichen Namen haben
+        #self. makes deacon.data an instance variable, not a local for __init__
+
+        #iterate through all detected Deacon log files
+        for f in self.find_log_files("deacon", filehandles = True): #filehandles = opens the files
+            sample_name = self.clean_s_name(f["s_name"], f) #f["s_name"] -> samplename; self.clean_s_name -> normalizes the name to avoid duplicates
             try:
-                data = json.load(f["f"]) #json-Inhalt der Datei wird gelesen und soll geparsed werden; f["f"] -> offenes file
-            except Exception as e: #Fehler werden abgefangen in e
-                log.error("failed to parse data from {f['f']} : {e}") #{e} -> liefert Fehlermeldung
+                data = json.load(f["f"]) #load JSON data from filehandles
+            except Exception as e: #catch parsing-errors in e
+                log.error(f"failed to parse data from {f['f']} : {e}") #{e} -> error message
                 continue
 
-            #Metriken extrahieren 
+            #extract metrics for each sample
             self.deacon_data[sample_name] = {
-                "k" : data.get("k"), #k-mer Länge?
-                "w" : data.get("w"), #Window Größe?
-                "abs_threshold" : data.get("abs_threshold"),
+                "k" : data.get("k"), #k-mere length
+                "w" : data.get("w"), #wondow size
+                "abs_threshold" : data.get("abs_threshold"), #number of k-mers that count as hit
                 "rel_threshold" : data.get("rel_threshold"),
-                #"prefix_length" : data.get("prefix_length"),
-                #"deplete" : data.get("deplete"),
-                #"rename" : data.get("rename"),
-                "seqs_in" : data.get("seqs_in"),
-                "seqs_out" : data.get("seqs_out"),
-                "seqs_out_proportion" : data.get("seqs_out_proportion"),
-                "seqs_removed" : data.get("seqs_removed"),
+                #"prefix_length" : data.get("prefix_length"), #prefix length used for hashing/filtering
+                #"deplete" : data.get("deplete"), #boolean, depleted reads (True/False)
+                #"rename" : data.get("rename"), #boolean, renamed reads after export (True/False)
+                "seqs_in" : data.get("seqs_in"), #number of input reads
+                "seqs_out" : data.get("seqs_out"), #number of output reads
+                "seqs_out_proportion" : data.get("seqs_out_proportion"), 
+                "seqs_removed" : data.get("seqs_removed"), #number of removed reads
                 "seqs_removed_proportion" : data.get("seqs_removed_proportion"),
-                "bp_in" : data.get("bp_in"),
-                "bp_out" : data.get("bp_out"),
+                "bp_in" : data.get("bp_in"), #number of input base-pairs
+                "bp_out" : data.get("bp_out"), #number of output base-pairs
                 "bp_out_proportion" : data.get("bp_out_proportion"),
-                "time" : data.get("time"),
+                "bp_removed" : data.get("bp_removed"), #number of removed base-pairs
+                "bp_removed_proportion" : data.get("bp_removed_proportion"),
+                "time" : data.get("time"), #runtime
                 "seqs_per_second" : data.get("seqs_per_second"),
-                "bp_per_second" : data.get("bp_per_second")
+                "bp_per_second" : data.get("bp_per_second") 
             }
         
         if len(self.deacon_data) == 0:
             raise UserWarning("no deacon reports found")
         
-        #Tabelle als Bericht mit Metriken
+        #create table in the report
         self.add_data_table(
             self.deacon_data,
             headers = {
@@ -60,14 +64,18 @@ class MultiqcModule(BaseMultiqcModule):
                 "w" : {"title" : "window size"},
                 "abs_threshold" : {"title" : "absolute threshold"},
                 "rel_threshold" : {"title" : "relative threshold"},
+                #"prefix_length" : {"title" : "prefix length"},
+                #"deplete" : {"title" : "title"},
+                #"rename" : {"title" : "rename"},
+                #-> optionale headers?
                 "seqs_in" : {"title" : "reads in"},
                 "seqs_out" : {"title" : "reads out"},
                 "seqs_removed" : {"title" : "reads removed"},
-                "seqs_removed_proportion" : {"title" : "reads removed (%)", "formate" : "{:%}"}, #Angabe in Prozent
+                "seqs_removed_proportion" : {"title" : "reads removed (%)", "format" : "{:%}"}, #Angabe in Prozent
                 "bp_in" : {"title" : "bp in"},
                 "bp_out" : {"title" : "bp out"},
                 "bp_removed" : {"title" : "bp removed"},
-                "bp_removed_proportion" : {"title" : "bp removed (%)", "formate" : "{:%}"},
+                "bp_removed_proportion" : {"title" : "bp removed (%)", "format" : "{:%}"},
                 "time": {"title" : "time (in s)"},
                 "seqs_per_second" : {"title" : "reads/s"},
                 "bp_per_second" : {"title" : "bp/s"}
@@ -76,23 +84,23 @@ class MultiqcModule(BaseMultiqcModule):
             description = "statistics parsed from deacon reports"
         )
 
-        #Plots hinzufügen -> Reads entfernt
+        #add plot in report for: reads removed
         plot_data = {}
-        for sample, stats in self.deacon_data.items(): #für alle samples und deren Statistiken
-            if stats.get("seqs_removed_proprtion") is not None: #überprüfen, ob der Report "seqs_removed_proportion" vorhanden ist
-                plot_data[sample] = stats["seqs_removed_proportion"] * 100 #Umwandlung in %
+        for sample, stats in self.deacon_data.items(): #iterate through all samples and their statistics
+            if stats.get("seqs_removed_proportion") is not None: #check, if report contains "seqs_removed_proportion"
+                plot_data[sample] = stats["seqs_removed_proportion"] * 100 #convert to %
 
-        if len(plot_data) > 0: #wenn samples mit Daten enthalten sind
-            pconfig = { #Plot-Konfiguration, Dictionary; Report-Konfiguration nicht nach default (ohne pconfig wäre default)
+        if len(plot_data) > 0: #create if there is data
+            pconfig = { #plot-configuration, dictionary; no default configuration
                 "id" : "deacon_removed_reads",
                 "title" : "% Reads removed (Deacon)",
                 "ylab" : "% removed"
             }
 
-            self.add_section( #neue report-Sektion für MultiQC
+            self.add_section( #new section in MultiQC report
                 name = "Reads removed",
                 anchor = "deacon_removed_reads",
                 description = "percentage of removed reads per sample",
-                plot = bargraph.plot(plot_data, pconfig) #erzeugt das Balkendiagramm
+                plot = bargraph.plot(plot_data, pconfig) #generates a barplot
             )
 
