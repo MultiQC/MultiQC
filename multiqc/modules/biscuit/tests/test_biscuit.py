bb024a1d3da303dac99ff7efc2ed37ae374d6cfc
Jacob Morrison <Jacob.Morrison@vai.org>
2025-09-12 11:57:55 -0400

    Refactor BISCUIT module
    The BISCUIT module hasn't been updated in quite a while and with the
    number of changes made to MultiQC since then, it was starting to not
    behave as intended.
    
    Some (though not all) of the changes made:
    - Improve consistency in how to handle missing data for maintenance
      purposes.
    - Add file tags to config_defaults for removal. This will allow MultiQC
      to handle all of the sample name cleaning and allow user options that
      affect the sample name to work.
    - Return average cytosine retention plots to grouped bar charts, rather
      than stacked bar charts. This shows each of the cytosine contexts as
      their own bar, rather than stacking them, which doesn't make sense in
      this particular context.
    - Reduce bloat of functions that exist, but only have pass in them. This
      was due to how the module was previously written. Now, only meaningful
      functions are included in the module.
    - Improve some of the help messages for clarity.
    - Change some of the color schemes.
    - Change Watson/Crick strand verbiage to OT/CTOT and OB/CTOB.

---
+++ multiqc/modules/biscuit/tests/test_biscuit.py
@@ -0,0 +1,50 @@
+import pytest
+
+from multiqc import config, report
+from multiqc.modules.biscuit import MultiqcModule
+from multiqc.utils import testing
+
+# NOTE: These tests could be fleshed out more (more inputs, more error cases, etc.)
+
+EPSILON = 0.0001
+
+
+@pytest.fixture
+def data_dir():
+    return testing.data_dir()
+
+
+def read_file(data_dir, fname):
+    with (data_dir / "modules/biscuit/v0.3.16.20200420" / fname).open() as fh:
+        return fh.read()
+
+
+@pytest.fixture
+def mapq_table(data_dir):
+    return read_file(data_dir, "tcga_lusc_normal_subsampled_mapq_table.txt")
+
+
+def test_mapq_table(mapq_table):
+    """Test that parsing the MAPQ table works as expected"""
+    from multiqc.modules.biscuit import parse_align_mapq
+
+    parsed = parse_align_mapq(mapq_table)
+
+    # Four keys are expected (frac_align, opt_align, sub_align, mapqs)
+    assert len(parsed) == 4
+
+    # mapqs key should have 61 keys
+    assert len(parsed["mapqs"]) == 61
+
+    # Check correctly parsed values
+    assert parsed["frac_align"] - 98.81 < EPSILON
+    assert parsed["opt_align"] == 56535091
+    assert parsed["sub_align"] == 9287487
+    assert parsed["not_align"] == 794210
+
+    # Spot check MAPQ values
+    assert parsed["mapqs"][0] - 9.41 < EPSILON
+    assert parsed["mapqs"][14] - 0.05 < EPSILON
+    assert parsed["mapqs"][26] - 0.04 < EPSILON
+    assert parsed["mapqs"][40] - 3.83 < EPSILON
+    assert parsed["mapqs"][60] - 78.25 < EPSILON
