fee051ef5e282b8a32bd07a787ff16a83a1e964e
Phil Ewels <phil.ewels@seqera.io>
2026-04-21 08:59:37 +0200

    Use safe alternative to eval()

--- multiqc/modules/freyja/freyja.py
+++ multiqc/modules/freyja/freyja.py
@@ -1,4 +1,5 @@
 import logging
+from ast import literal_eval
 from typing import Dict
 
 from multiqc.base_module import BaseMultiqcModule, ModuleNoSamplesFound
@@ -39,8 +40,8 @@ class MultiqcModule(BaseMultiqcModule):
             summarized_line = next(line for line in f["f"] if line.startswith("summarized\t"))
             dict_str = summarized_line.split("\t")[1].strip()
             try:
-                sample_dict: Dict[str, float] = dict(eval(dict_str))
-            except ValueError:
+                sample_dict: Dict[str, float] = dict(literal_eval(dict_str))
+            except (ValueError, SyntaxError):
                 log.error(f"Error parsing 'summarized' line for '{s_name}': {dict_str}, skipping sample")
                 continue
             if not sample_dict:
