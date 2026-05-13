e27a8c2247cff61e65b4e4fc2d400a4fd5d2b499
Phil Ewels <phil.ewels@seqera.io>
2026-05-13 12:33:22 +0200

    Fix linting error

--- multiqc/modules/ribodetector/ribodetector.py
+++ multiqc/modules/ribodetector/ribodetector.py
@@ -23,7 +23,7 @@ class MultiqcModule(BaseMultiqcModule):
     """
 
     def __init__(self):
-        super(MultiqcModule, self).__init__(
+        super().__init__(
             name="RiboDetector",
             anchor="ribodetector",
             href="https://github.com/hzi-bifo/RiboDetector",
