10c95c5811202c04ba77c7f30309dbc76d3f40a9
Josh Chorlton <josh@bugseq.com>
2026-07-01 20:25:58 +0000

    handle thinking responses from bedrock AI

--- multiqc/core/ai.py
+++ multiqc/core/ai.py
@@ -517,14 +517,14 @@ class AWSBedrockClient(Client):
         )
 
         response_body = json.loads(response["body"].read())
-        if (
-            "content" not in response_body
-            or len(response_body["content"]) != 1
-            or "text" not in response_body["content"][0]
-        ):
-            logger.error(f"bedrock response content: {response_body}")
+        if "content" not in response_body:
+            logger.error(f"bedrock response does not have 'content': {response_body}")
             raise ValueError("Unexpected bedrock response body")
-        content = response_body["content"][0]["text"]  # Extract the assistant's response
+        text_blocks = [b for b in response_body["content"] if b["type"] == "text"]
+        if len(text_blocks) != 1:
+            logger.error(f"bedrock response does not contain 1 text block: {response_body}")
+            raise ValueError("Unexpected bedrock response body")
+        content = text_blocks[0]["text"]  # Extract the assistant's response
         return AWSBedrockClient.ApiResponse(content=content, model=self.model)
 
 
